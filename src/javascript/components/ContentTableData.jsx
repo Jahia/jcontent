437824481c9dd740d39772e8ea22ec1d2bbf540f
Yousria <yousria.benchadi@gmail.com>
2018-06-27 10:34:41 +0200

    BACKLOG-8097 : fix

--- src/javascript/components/ContentTableData.jsx
+++ src/javascript/components/ContentTableData.jsx
@@ -10,28 +10,6 @@ class ContentTableData extends React.Component {
         super(props);
     }
 
-    childrenRender(results){
-        let rows = [];
-        if(results.data.jcr && results.data.jcr.nodesByCriteria){
-            rows = _.map(results.data.jcr.nodesByCriteria.nodes, contentNode => {
-                let contents = _.values(contentNode);
-
-                return {
-                    path: contentNode.path,
-                    uuid: contentNode.uuid,
-                    displayName: contentNode.displayName,
-                    contents: contents
-                }
-            });
-        }
-
-        return <List>
-            {rows.map((node, index) => {
-                return <h3 key={index}>{node.displayName}</h3>;
-            })}
-        </List>
-    }
-
     render() {
         return <Query fetchPolicy={'network-only'} query={allContentQuery}>
             { ({loading, error, data}) => {

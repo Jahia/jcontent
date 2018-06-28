706d34f072947d73a41afa6d9de0d73a7fb44ebe
David Griffon <dgriffon@jahia.com>
2018-06-28 08:37:28 +0200

    BACKLOG-8090 : update breadcrumb

--- src/javascript/components/ContentBreadcrumbs.jsx
+++ src/javascript/components/ContentBreadcrumbs.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import * as _ from 'lodash';
-import {Typography} from "@material-ui/core";
 
 
 class ContentBreadcrumbs extends React.Component {
@@ -10,7 +9,14 @@ class ContentBreadcrumbs extends React.Component {
         let pathElements = _.reduce(_.split(path.substring(1), '/'), (result, value) => _.concat(result, result[result.length - 1] + '/' + value), ['']);
 
         return (
-            <span>{_.range(pathElements.length).map(i => (<span key={i}><Link to={pathElements[i]}>{i > 0 ? _.replace(pathElements[i], pathElements[i - 1], '') : '/'}</Link>   -    </span>))}</span>
+            <span>{_.range(pathElements.length).map(i => {
+                const link = i > 0 ? _.replace(pathElements[i], pathElements[i - 1], '') : "/";
+                return (
+                    <span key={i}>
+                        {i < pathElements.length - 1 ? <span><Link to={pathElements[i]}>{link}</Link> - </span> : <span>{link}</span>}
+                    </span>
+            )})}
+            </span>
         )
     }
 }

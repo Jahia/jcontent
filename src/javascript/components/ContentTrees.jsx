06c98298bddde5c01820f0a74b5e15bbf89cfad6
David Griffon <dgriffon@jahia.com>
2018-07-13 17:17:38 +0200

    BACKLOG-8208 : fix params encoding

--- src/javascript/components/ContentTrees.jsx
+++ src/javascript/components/ContentTrees.jsx
@@ -41,7 +41,7 @@ let ContentTrees = (props) => {
                         rootPath={rootPath + "/contents"}
                         selectableTypes={['jmix:list']}
                         lang={lang}
-                        handleSelect={ path => goto( path, {recursionTypesFilter:'jnt:contentFolder', typeFilter:'jmix:editorialContent'}) }
+                        handleSelect={ path => goto( path, {recursionTypesFilter:'[jnt:contentFolder]', typeFilter:'[jmix:editorialContent]'}) }
                         openableTypes={['jmix:list', 'jnt:contentFolder']}
                         rootLabel={t("label.contentManager.browseFolders")}
                     />
@@ -52,7 +52,7 @@ let ContentTrees = (props) => {
                         rootPath={rootPath}
                         selectableTypes={['jnt:page']}
                         lang={lang}
-                        handleSelect={ path => goto( path, {recursionTypesFilter:'jnt:page', typeFilter:'jmix:editorialContent'} ) }
+                        handleSelect={ path => goto( path, {recursionTypesFilter:'[jnt:page]', typeFilter:'[jmix:editorialContent]'} ) }
                         openableTypes={['jnt:page', 'jnt:virtualsite', 'jnt:navMenuText']}
                         rootLabel={t("label.contentManager.browsePages")}
                     />

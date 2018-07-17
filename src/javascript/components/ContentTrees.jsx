68c835aed90e011e69d0b2b690b9833a23635bfd
David Griffon <dgriffon@jahia.com>
2018-07-17 17:38:17 +0200

    BACKLOG-8167 : make the tree open on the current selection / allow to close it without losing the selection / add a button to open to the current location

--- src/javascript/components/ContentTrees.jsx
+++ src/javascript/components/ContentTrees.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import {Picker} from "@jahia/react-apollo";
 import {PickerViewMaterial} from '@jahia/react-material';
-import {List, ListItem} from "@material-ui/core";
+import {List, ListItem, Button} from "@material-ui/core";
 import CmRouter from "./CmRouter";
 import gql from "graphql-tag";
 import {translate} from 'react-i18next';
 import * as _ from 'lodash';
 
 class ContentTree extends React.Component {
+    constructor(props) {
+        super(props);
+        this.picker = React.createRef();
+    }
 
+    openPath(path, rootPath) {
+        this.picker.current.openPath(path, rootPath);
+    }
     render() {
         let {rootPath, path, handleSelect, lang, openableTypes, selectableTypes, rootLabel, filterTypes, recurTypes} = this.props;
         return (
@@ -16,6 +23,7 @@ class ContentTree extends React.Component {
                 applyFor: "node",
                 gql: gql`fragment PrimaryNodeTypeName on JCRNode { primaryNodeType { name } }`
             }]}
+                    ref={this.picker}
                     rootPaths={[rootPath]}
                     defaultOpenPaths={[path]}
                     openableTypes={openableTypes}
@@ -32,35 +40,53 @@ class ContentTree extends React.Component {
     }
 }
 
-let ContentTrees = (props) => {
-    const {lang, rootPath, path, t} = props;
-    return (<CmRouter render={({goto}) => (
-            <List>
-                <ListItem>
-                    <ContentTree
-                        path={path}
-                        rootPath={rootPath + "/contents"}
-                        selectableTypes={['jmix:list']}
-                        lang={lang}
-                        handleSelect={ path => goto( path, {type: "contents"}) }
-                        openableTypes={['jmix:list', 'jnt:contentFolder']}
-                        rootLabel={t("label.contentManager.browseFolders")}
-                    />
-                </ListItem>
-                <ListItem>
-                    <ContentTree
-                        path={path}
-                        rootPath={rootPath}
-                        selectableTypes={['jnt:page']}
-                        lang={lang}
-                        handleSelect={ path => goto( path, {type: "pages"} ) }
-                        openableTypes={['jnt:page', 'jnt:virtualsite', 'jnt:navMenuText']}
-                        rootLabel={t("label.contentManager.browsePages")}
-                    />
-                </ListItem>
-            </List>
-        )} />
-    )
-};
+class ContentTrees extends React.Component {
+    constructor(props) {
+        super(props);
+        this.contentTree = React.createRef();
+        this.pageTree= React.createRef();
+    }
+
+    openTrees(path) {
+        this.contentTree.current.openPath(path);
+        this.pageTree.current.openPath(path);
+    }
+
+    render() {
+        const {lang, rootPath, path, t} = this.props;
+        return (<CmRouter render={({goto}) => (
+                <List>
+                    <ListItem>
+                        <Button onClick={() => this.openTrees(path)}>{t("label.contentmanager.showCurrentPath")}</Button>
+                    </ListItem>
+                    <ListItem>
+                        <ContentTree
+                            ref={this.contentTree}
+                            path={path}
+                            rootPath={rootPath + "/contents"}
+                            selectableTypes={['jmix:list']}
+                            lang={lang}
+                            handleSelect={path => goto(path, {type: "contents"})}
+                            openableTypes={['jmix:list', 'jnt:contentFolder']}
+                            rootLabel={t("label.contentManager.browseFolders")}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ContentTree
+                            ref={this.pageTree}
+                            path={path}
+                            rootPath={rootPath}
+                            selectableTypes={['jnt:page']}
+                            lang={lang}
+                            handleSelect={path => goto(path, {type: "pages"})}
+                            openableTypes={['jnt:page', 'jnt:virtualsite', 'jnt:navMenuText']}
+                            rootLabel={t("label.contentManager.browsePages")}
+                        />
+                    </ListItem>
+                </List>
+            )}/>
+        )
+    }
+}
 
 export default translate()(ContentTrees);

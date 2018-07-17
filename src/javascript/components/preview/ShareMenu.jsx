e8f0321fc6a87115a7d9736dd75ed0c674f29a19
akarmanov <alexandrk_@hotmail.com>
2018-07-17 14:50:37 -0400

    Add copy to clipboard functionality

--- src/javascript/components/preview/ShareMenu.jsx
+++ src/javascript/components/preview/ShareMenu.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { translate } from "react-i18next";
 import { withStyles, IconButton, MenuItem, Menu } from "@material-ui/core";
 import { Share } from "@material-ui/icons";
+import copy from 'copy-to-clipboard';
 
 const styles = theme => ({
     button: {
@@ -12,47 +13,52 @@ const styles = theme => ({
 
 class ShareMenu extends Component {
 
-    handleMenuClick(event, anchorType) {
-        this.setState({ [anchorType]: event.currentTarget });
-    };
-
-    handleMenuClose(anchorType) {
-        this.setState({ [anchorType]: null });
-    };
-
     constructor(props) {
         super(props);
         this.state = {
-            shareMenuAnchor : null,
+            shareMenuAnchor: null,
         };
+    }
+
+    copy(value) {
+        this.handleMenuClose("shareMenuAnchor");
+        copy(value);
     }
 
 
     render() {
-        const { t, classes } = this.props;
-        const { shareMenuAnchor } = this.state;
+        const {t, classes, selection} = this.props;
+        const {shareMenuAnchor} = this.state;
 
         return <span>
             <IconButton
-                aria-owns={ shareMenuAnchor ? 'share-menu' : null}
+                aria-owns={shareMenuAnchor ? 'share-menu' : null}
                 aria-haspopup="true"
-                onClick={ ( event) => this.handleMenuClick(event, "shareMenuAnchor") }>
-                <Share />
+                onClick={(event) => this.handleMenuClick(event, "shareMenuAnchor")}>
+                <Share/>
             </IconButton>
             <Menu
                 id="share-menu"
-                anchorEl={ shareMenuAnchor }
-                open={Boolean( shareMenuAnchor )}
-                onClose={ () => this.handleMenuClose("shareMenuAnchor") }>
-                <MenuItem onClick={ () => this.handleMenuClose("shareMenuAnchor") }>
-                    { t('label.contentManager.contentPreview.copyPathToClipboard') }
+                anchorEl={shareMenuAnchor}
+                open={Boolean(shareMenuAnchor)}
+                onClose={() => this.handleMenuClose("shareMenuAnchor")}>
+                <MenuItem onClick={() => this.copy(selection.path)}>
+                    {t('label.contentManager.contentPreview.copyPathToClipboard')}
                 </MenuItem>
-                <MenuItem onClick={ () => this.handleMenuClose("shareMenuAnchor") }>
-                    { t('label.contentManager.contentPreview.copyUUIDToClipboard') }
+                <MenuItem onClick={() => this.copy(selection.uuid)}>
+                    {t('label.contentManager.contentPreview.copyUUIDToClipboard')}
                 </MenuItem>
             </Menu>
         </span>
     }
+
+    handleMenuClick(event, anchorType) {
+        this.setState({[anchorType]: event.currentTarget});
+    };
+
+    handleMenuClose(anchorType) {
+        this.setState({[anchorType]: null});
+    };
 }
 
 export default translate()(withStyles(styles)(ShareMenu));

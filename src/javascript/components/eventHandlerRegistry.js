0e9dd5125a2b1f05a38b55a8128e56d5eba44b79
David Griffon <dgriffon@jahia.com>
2018-08-08 22:36:51 +0200

    BACKLOG-8301 : add missing file

---
+++ src/javascript/components/eventHandlerRegistry.js
@@ -0,0 +1,13 @@
+import * as _ from "lodash";
+
+
+const context = {
+}
+
+let register = (eventHandlers) => {;
+    // register listeners
+    eventHandlers = _.mapValues(eventHandlers, handlers => _.map(handlers, handler => handler.bind(this, context)));
+    _.mergeWith(window.parent, eventHandlers);
+}
+
+export {register, context}

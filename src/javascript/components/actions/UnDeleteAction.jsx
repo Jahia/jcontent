20d0f009bdeff92b03badc1b73ec244e5502a9c2
Yousria <yousria.benchadi@gmail.com>
2018-09-17 11:53:00 +0200

    BACKLOG-8499 : cleanup

--- src/javascript/components/actions/UnDeleteAction.jsx
+++ src/javascript/components/actions/UnDeleteAction.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {ContentTypeNamesQuery, GetNodeAndChildrenByPathQuery} from "../gqlQueries";
 import {translate} from "react-i18next";
 import * as _ from "lodash";
 import {withApollo} from 'react-apollo';
@@ -8,26 +7,7 @@ import {withNotifications} from "@jahia/react-material/index";
 class UnDeleteAction extends React.Component {
 
     constructor(props) {
-
         super(props);
-
-        this.onGwtContentUndelete = this.onGwtContentUndelete.bind(this);
-    }
-
-    onGwtContentUndelete(enginePath, engineNodeName){
-        const path = enginePath.substring(0, enginePath.lastIndexOf("/") + 1) + engineNodeName;
-        console.log(enginePath);
-        console.log(path);
-        this.props.client.query({
-            query: GetNodeAndChildrenByPathQuery,
-            fetchPolicy: "network-only",
-            variables: {
-                "path": path.substring(0, path.lastIndexOf("/")),
-                "language": "en",
-                "displayLanguage": "en",
-            }
-        });
-        console.log("executed");
     }
 
     render() {
@@ -36,7 +16,6 @@ class UnDeleteAction extends React.Component {
         let ctx = _.cloneDeep(context);
 
         ctx.displayName = ctx.displayName.substring(0, 100)+"...";
-        ctx.onGwtContentUndelete = this.onGwtContentUndelete;
 
         let mixinTypesProperty = null;
         if (context.node.properties != null) {

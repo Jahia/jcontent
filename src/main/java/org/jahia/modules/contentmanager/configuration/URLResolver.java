af00e82b357766ab7a4e8c54989a53c315f1210a
Sergiy Shyrkov <sshyrkov@jahia.com>
2018-09-19 12:15:40 +0200

    Cosmetics: removed unused import statements

--- src/main/java/org/jahia/modules/contentmanager/configuration/URLResolver.java
+++ src/main/java/org/jahia/modules/contentmanager/configuration/URLResolver.java
@@ -1,18 +1,10 @@
 package org.jahia.modules.contentmanager.configuration;
 
-import net.sf.ehcache.Ehcache;
-import org.apache.commons.lang.StringUtils;
-import org.jahia.api.Constants;
-import org.jahia.services.content.JCRNodeWrapper;
-import org.jahia.services.content.JCRSessionFactory;
-import org.jahia.services.render.RenderContext;
-import org.jahia.services.render.Resource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.jcr.RepositoryException;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Locale;
+
+import org.jahia.services.render.RenderContext;
+
+import net.sf.ehcache.Ehcache;
 
 /**
  * URL Resolver Wrapper to access protected method from the constructor ..

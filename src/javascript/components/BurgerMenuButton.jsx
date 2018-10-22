e16c744defd11175a9a8fbed37e26bb679cfe88f
stefan <ssavu@jahia.com>
2018-10-22 16:11:47 -0400

    BACKLOG-8838 - Update to use black/white DX icon.

--- src/javascript/components/BurgerMenuButton.jsx
+++ src/javascript/components/BurgerMenuButton.jsx
@@ -4,7 +4,7 @@ import {withStyles} from '@material-ui/core';
 
 const styles = theme => ({
     menuButton: {
-        background: "url(" + contextJsParameters.contextPath + "/engines/jahia-anthracite/images/dx_logo_solid.png) center center no-repeat",
+        background: "url(" + contextJsParameters.contextPath + "/engines/jahia-anthracite/images/logos/dx_logo_solid-white.png) center center no-repeat",
         marginLeft: -12,
         marginRight: 6,
         width: "3.5em",

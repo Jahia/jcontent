9e00bc0e6ec70625c43cdfd39d42c9e2506aba6e
oyevstafyev <oievstafiev@jahia.com>
2018-10-29 17:20:02 +0100

    [BACKLOG-8916] Delay opening upload panel added

--- src/javascript/components/actions/FileUploadAction.jsx
+++ src/javascript/components/actions/FileUploadAction.jsx
@@ -3,23 +3,28 @@ import {translate} from "react-i18next";
 import * as _ from "lodash";
 import {withNotifications} from "@jahia/react-material/index";
 import {connect} from "react-redux";
-import { setPanelState, setPath } from '../fileupload/redux/actions';
-import { panelStates } from '../fileupload/constatnts';
+import {setPanelState, setPath} from '../fileupload/redux/actions';
+import {panelStates} from '../fileupload/constatnts';
 import {batchActions} from 'redux-batched-actions';
 
 class FileUploadAction extends React.Component {
 
     render() {
-        let {children, context, t, labelKey, dispatchBatch, ...rest} = this.props;
-        return children({
-                        ...rest,
-                        labelKey: labelKey,
-                        onClick: () => dispatchBatch([
-                            setPath(context.path),
-                            setPanelState(panelStates.VISIBLE)
-                        ])
-                    });
 
+        let {children, context, t, labelKey, dispatchBatch, ...rest} = this.props;
+
+        return children({
+
+            ...rest,
+            labelKey: labelKey,
+
+            // Defer opening the upload panel for a fraction of second to avoid closing the menu that contains the action and opening the panel simultaneously
+            // which causes a Material UI ModalManager error otherwise.
+            onClick: () => setTimeout(() => dispatchBatch([
+                setPath(context.path),
+                setPanelState(panelStates.VISIBLE)
+            ]), 100)
+        });
     }
 }
 

107e7745bd6c3e113f1634541fb45e3ed917de3c
dgaillard <dgaillard@jahia.com>
2018-11-13 19:35:52 +0100

    BACKLOG-8318 remove console.log

--- src/javascript/components/actions/openInEditModeAction.jsx
+++ src/javascript/components/actions/openInEditModeAction.jsx
@@ -6,13 +6,11 @@ import {withDxContextAction} from "./withDxContextAction";
 
 export default composeActions(requirementsAction, withDxContextAction, {
     init: (context) => {
-        console.log(context);
         const siteContentPath = '/sites/' + context.dxContext.siteKey + '/contents';
         context.initRequirements({
             getDisplayableNodePath: true,
             enabled: context => {
                 return context.node.pipe(map(node => {
-                    console.log(node);
                     return !_.isEmpty(node.displayableNode) && node.displayableNode.path.indexOf(siteContentPath) === -1;
                 }))
             }

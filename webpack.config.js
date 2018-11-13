776f2aa1ae23c7fd1a45a1bf013a39041da4e3df
dgaillard <dgaillard@jahia.com>
2018-11-13 18:28:31 +0100

    BACKLOG-8318 add files to ignore (intellij graphql plugin) add yarn script to ease dev and update the webpack config to fasten webpack watch

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,58 @@ const path = require('path');
 const webpack = require('webpack');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+module.exports = (env, argv) => {
 
-module.exports = {
-    entry: {
-        main: [ '@babel/polyfill', 'whatwg-fetch', path.resolve(__dirname, 'src/javascript/publicPath'), path.resolve(__dirname, 'src/javascript/ContentManagerApp.jsx')]
-    },
-    output: {
-        path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
-        filename: 'content-manager.js'
-    },
-    resolve: {
-        mainFields: ['module', 'main'],
-        extensions: ['.mjs', '.js', '.jsx', 'json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.mjs$/,
-                include: /node_modules/,
-                type: "javascript/auto",
-            },
-            {
-                test: /\.jsx?$/,
-                include: [path.join(__dirname, "src")],
-                loader: 'babel-loader',
-
-                query: {
-                    presets: [['env', {modules: false}], 'react', 'stage-2'],
-                    plugins: [
-                        "lodash"
-                    ]
-                }
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
+    let config = {
+        entry: {
+            main: ['@babel/polyfill', 'whatwg-fetch', path.resolve(__dirname, 'src/javascript/publicPath'), path.resolve(__dirname, 'src/javascript/ContentManagerApp.jsx')]
+        },
+        output: {
+            path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
+            filename: 'content-manager.js'
+        },
+        resolve: {
+            mainFields: ['module', 'main'],
+            extensions: ['.mjs', '.js', '.jsx', 'json']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.mjs$/,
+                    include: /node_modules/,
+                    type: "javascript/auto"
+                },
+                {
+                    test: /\.jsx?$/,
+                    include: [path.join(__dirname, "src")],
+                    loader: 'babel-loader',
+                    query: {
+                        presets: [['env', {modules: false}], 'react', 'stage-2'],
+                        plugins: [
+                            "lodash"
+                        ]
                     }
-                }]
-            },
+                },
+                {
+                    test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }]
+                }
+            ]
+        },
+        plugins: [
+            //new BundleAnalyzerPlugin({analyzerMode: "static"}),
+            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|fr|de/)
         ],
-    },
-    plugins: [
-        //new BundleAnalyzerPlugin({analyzerMode: "static"}),
-        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /en|fr|de/),
-    ],
-    mode: 'development',
-    devtool: 'source-map'
+        mode: 'development'
+    };
+
+    config.devtool = (argv.mode === 'production') ? 'source-map' : 'eval-source-map';
+
+    return config;
 };

ae2329ad5cb54e120d01e7166c5eabc8ea697c27
tdraier <tdraier@jahia.com>
2018-11-14 18:19:32 +0100

    BACKLOG-8968 : upload action

--- src/javascript/components/actions/fileUploadAction.jsx
+++ src/javascript/components/actions/fileUploadAction.jsx
@@ -1,54 +1,24 @@
 import React from "react";
-import {translate} from "react-i18next";
-import * as _ from "lodash";
-import {withNotifications} from "@jahia/react-material/index";
-import {connect} from "react-redux";
 import {setPanelState, setPath} from '../fileupload/redux/actions';
 import {panelStates} from '../fileupload/constatnts';
 import {batchActions} from 'redux-batched-actions';
 import {composeActions} from "@jahia/react-material";
 import requirementsAction from "./requirementsAction";
+import {reduxAction} from "./reduxAction";
 
-// class FileUploadAction extends React.Component {
-//
-//     render() {
-//
-//         let {children, context, t, labelKey, dispatchBatch, ...rest} = this.props;
-//
-//         return children({
-//
-//             ...rest,
-//             labelKey: labelKey,
-//
-//             // Defer opening the upload panel for a fraction of second to avoid closing the menu that contains the action and opening the panel simultaneously
-//             // which causes a Material UI ModalManager error otherwise.
-//             onClick: () => setTimeout(() => dispatchBatch([
-//                 setPath(context.path),
-//                 setPanelState(panelStates.VISIBLE)
-//             ]), 100)
-//         });
-//     }
-// }
-//
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         dispatch: dispatch,
-//         dispatchBatch: (actions) => dispatch(batchActions(actions))
-//     }
-// };
-//
-// FileUploadAction = _.flowRight(
-//     withNotifications(),
-//     translate(),
-//     connect(null, mapDispatchToProps)
-// )(FileUploadAction);
-
-export default composeActions(requirementsAction, {
+export default composeActions(requirementsAction, reduxAction(null, (dispatch)=>({dispatchBatch: (actions)=> dispatch(batchActions(actions))})), {
     init:(context) => {
         context.initRequirements({
             requiredPermission: "jcr:addChildNodes",
             showOnNodeTypes: ["jnt:folder"]
         });
+    },
+
+    onClick:(context) => {
+        context.dispatchBatch([
+            setPath(context.path),
+            setPanelState(panelStates.VISIBLE)
+        ]);
     }
 
 });

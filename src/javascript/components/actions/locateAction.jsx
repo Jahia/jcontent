5da7aff803be3ba8e541b17662dbd7c9eb074575
stefan <ssavu@jahia.com>
2018-11-15 16:25:42 -0500

    BACKLOG-9013 - Created a locate action that navigates to a given path in content manager.

---
+++ src/javascript/components/actions/locateAction.jsx
@@ -0,0 +1,34 @@
+import {composeActions} from "@jahia/react-material";
+import requirementsAction from "./requirementsAction";
+import {cmSetMode, cmSetPath, cmOpenPaths, cmSetSelection} from "../redux/actions";
+import {reduxAction} from "./reduxAction";
+import Constants from "../constants";
+
+export default composeActions(requirementsAction, reduxAction(null,(dispatch) => ({
+    setMode: (state) => dispatch(cmSetMode(state)),
+    setPath: (state)=> dispatch(cmSetPath(state)),
+    setOpenPaths: (state)=> dispatch(cmOpenPaths(state)),
+    setSelection: (state)=> dispatch(cmSetSelection(state)),
+})), {
+    init: (context) => {
+      context.initRequirements();
+    },
+    onClick: (context) => {
+        let {setMode, setPath, setOpenPaths, setSelection} = context;
+        switch(context.locate.type) {
+            case "jnt:page":
+                let base = context.locate.paths[0].split("/");
+                base.pop();
+                context.locate.paths.splice(0,0, base.join("/"));
+            case "jnt:contentFolder":
+                setMode(Constants.mode.BROWSE);
+                break;
+            case "jnt:folder":
+                setMode(Constants.mode.FILES);
+                break;
+        }
+        setPath(context.locate.navigateToPath);
+        setOpenPaths(context.locate.paths);
+        setSelection([context.locate.node]);
+    }
+});

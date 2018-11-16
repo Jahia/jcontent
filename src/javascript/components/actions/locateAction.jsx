5e9c3c5cf6aabc18e8d98f1f8fba955510b0bd7b
stefan <ssavu@jahia.com>
2018-11-16 10:58:42 -0500

    BACKLOG-9032 - Add possibility to hide locate action in certain areas

--- src/javascript/components/actions/locateAction.jsx
+++ src/javascript/components/actions/locateAction.jsx
@@ -3,6 +3,8 @@ import requirementsAction from "./requirementsAction";
 import {cmSetMode, cmSetPath, cmOpenPaths, cmSetSelection} from "../redux/actions";
 import {reduxAction} from "./reduxAction";
 import Constants from "../constants";
+import {of} from "rxjs";
+import * as _ from "lodash";
 
 export default composeActions(requirementsAction, reduxAction(null,(dispatch) => ({
     setMode: (state) => dispatch(cmSetMode(state)),
@@ -11,7 +13,9 @@ export default composeActions(requirementsAction, reduxAction(null,(dispatch) =>
     setSelection: (state)=> dispatch(cmSetSelection(state)),
 })), {
     init: (context) => {
-      context.initRequirements();
+      context.initRequirements({
+          enabled: (context) => {return of(!_.isEmpty(context.locate))}
+      });
     },
     onClick: (context) => {
         let {setMode, setPath, setOpenPaths, setSelection} = context;

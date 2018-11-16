0771d4e5227d415c0c41b28172d7254cdea626bc
stefan <ssavu@jahia.com>
2018-11-16 15:47:37 -0500

    BACKLOG-9032 - Added ancestors nodes to sql2 search query for locate action functionality

--- src/javascript/components/gqlQueries.js
+++ src/javascript/components/gqlQueries.js
@@ -283,6 +283,13 @@ const sql2SearchContentQuery = gql`
                     totalCount
                 }
                 nodes {
+                    parents:ancestors(fieldFilter: {filters: {fieldName: "type", evaluation: AMONG, values:["jnt:page", "jnt:folder", "jnt:contentFolder"]}}) {
+                        type:property(name: "jcr:primaryType") {
+                            value
+                        }
+                        name
+                        path
+                    }
                     ...NodeFields
                 }
             }

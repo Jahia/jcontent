1072d0e53d3fd4ac58525d1a3787cce54913b3ec
Yousria <yousria.benchadi@gmail.com>
2018-12-28 10:49:26 +0100

    BACKLOG-9262 : fixed selection when clicking on preview without seleting a content

--- src/javascript/components/actions/previewAction.jsx
+++ src/javascript/components/actions/previewAction.jsx
@@ -1,14 +1,15 @@
 import {composeActions} from '@jahia/react-material';
 import requirementsAction from './requirementsAction';
-import {CM_DRAWER_STATES, cmSetPreviewState} from '../redux/actions';
+import {CM_DRAWER_STATES, cmSetPreviewState, cmSetSelection} from '../redux/actions';
 import {reduxAction} from './reduxAction';
 
-export default composeActions(requirementsAction, reduxAction(() => ({}), dispatch => ({setPreviewState: state => dispatch(cmSetPreviewState(state))})), {
+export default composeActions(requirementsAction, reduxAction(() => ({}), dispatch => ({setPreviewState: state => dispatch(cmSetPreviewState(state)), setSelection: selection => dispatch(cmSetSelection(selection))})), {
     init: context => {
         context.initRequirements({hideOnNodeTypes: ['jnt:page', 'jnt:folder', 'jnt:contentFolder']});
     },
     onClick: context => {
-        let {setPreviewState} = context;
+        let {setPreviewState, setSelection} = context;
+        setSelection(context.path);
         setPreviewState(CM_DRAWER_STATES.SHOW);
     }
 });

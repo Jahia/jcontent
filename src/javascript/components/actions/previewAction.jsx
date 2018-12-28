3cccd1da16953289038cd43efeae703fd4fb36de
oyevstafyev <oievstafiev@jahia.com>
2018-12-28 16:53:42 +0100

    [BACKLOG-9262] Minor improvements

--- src/javascript/components/actions/previewAction.jsx
+++ src/javascript/components/actions/previewAction.jsx
@@ -3,7 +3,10 @@ import requirementsAction from './requirementsAction';
 import {CM_DRAWER_STATES, cmSetPreviewState, cmSetSelection} from '../redux/actions';
 import {reduxAction} from './reduxAction';
 
-export default composeActions(requirementsAction, reduxAction(() => ({}), dispatch => ({setPreviewState: state => dispatch(cmSetPreviewState(state)), setSelection: selection => dispatch(cmSetSelection(selection))})), {
+export default composeActions(requirementsAction, reduxAction(() => ({}), dispatch => ({
+    setPreviewState: state => dispatch(cmSetPreviewState(state)),
+    setSelection: selection => dispatch(cmSetSelection(selection))
+})), {
     init: context => {
         context.initRequirements({hideOnNodeTypes: ['jnt:page', 'jnt:folder', 'jnt:contentFolder']});
     },

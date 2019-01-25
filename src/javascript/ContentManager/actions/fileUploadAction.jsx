754bed973d48e89bf8882122fb8c33a5cc2dd704
Yousria <yousria.benchadi@gmail.com>
2019-01-25 13:57:59 +0100

    BACKLOG-9437 : fix fileUploadAction since the Drawer doesn't exist anymore

--- src/javascript/ContentManager/actions/fileUploadAction.jsx
+++ src/javascript/ContentManager/actions/fileUploadAction.jsx
@@ -1,9 +1,9 @@
-import {setPanelState, setPath} from '../ContentLayout/Upload/Upload.redux-actions';
-import {panelStates} from '../ContentLayout/Upload/Upload.constants';
 import {batchActions} from 'redux-batched-actions';
 import {composeActions} from '@jahia/react-material';
 import requirementsAction from './requirementsAction';
 import {reduxAction} from './reduxAction';
+import {onFilesSelected} from '../ContentLayout/Upload/Upload.utils';
+import {setPath} from '../ContentLayout/Upload/Upload.redux-actions';
 
 export default composeActions(requirementsAction, reduxAction(null, dispatch => ({dispatchBatch: actions => dispatch(batchActions(actions))})), {
     init: context => {
@@ -14,10 +14,21 @@ export default composeActions(requirementsAction, reduxAction(null, dispatch => 
     },
 
     onClick: context => {
-        context.dispatchBatch([
-            setPath(context.path),
-            setPanelState(panelStates.VISIBLE)
-        ]);
+        let input = document.createElement('input');
+        let files = [];
+        input.setAttribute('type', 'file');
+        input.setAttribute('multiple', 'true');
+        input.click();
+        input.addEventListener('change', () => {
+            Array.from(input.files).forEach(file => {
+                files.push(file);
+            });
+            setPath(context.path);
+            onFilesSelected(
+                files,
+                context.dispatchBatch,
+                {path: context.path}
+            );
+        });
     }
-
 });

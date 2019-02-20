7d54831f4427a46ba8980d1db479d2a49f709b28
David Griffon <dgriffon@jahia.com>
2019-02-20 16:29:58 +0100

    BACKLOG-9600 : fix lint format errors

--- src/main/resources/javascript/activate-loader.js
+++ src/main/resources/javascript/activate-loader.js
@@ -1,5 +1,4 @@
 (function () {
-
     if (window.location.href.indexOf('/cms/contentmanager') === -1) {
         return;
     }
@@ -7,16 +6,15 @@
     function waitForFunction() {
         if (window.displayDXLoadingScreen) {
             window.displayDXLoadingScreen({
-                "en" : "Loading Content and Media Manager...",
-                "fr" : "Chargement du gestionnaire de contenu et média...",
-                "de" : "Content- und Medien-Manager wird geladen...",
+                en: 'Loading Content and Media Manager...',
+                fr: 'Chargement du gestionnaire de contenu et média...',
+                de: 'Content- und Medien-Manager wird geladen...'
             });
         } else {
-            console.log("Waiting for 100ms for function window.displayDXLoadingScreen to become available");
+            console.log('Waiting for 100ms for function window.displayDXLoadingScreen to become available');
             setTimeout(waitForFunction, 100);
         }
     }
 
     waitForFunction();
-
 })();

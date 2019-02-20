a83b5b2b5fc620517b3997401fd71949a5f92f98
Serge Huber <shuber@jahia.com>
2019-02-20 15:36:15 +0100

    BACKLOG-9600 Move loading screen to DX-Commons-Webpack module

---
+++ src/main/resources/javascript/activate-loader.js
@@ -0,0 +1,22 @@
+(function () {
+
+    if (window.location.href.indexOf('/cms/contentmanager') === -1) {
+        return;
+    }
+
+    function waitForFunction() {
+        if (window.displayDXLoadingScreen) {
+            window.displayDXLoadingScreen({
+                "en" : "Loading Content and Media Manager...",
+                "fr" : "Chargement du gestionnaire de contenu et média...",
+                "de" : "Content- und Medien-Manager wird geladen...",
+            });
+        } else {
+            console.log("Waiting for 100ms for function window.displayDXLoadingScreen to become available");
+            setTimeout(waitForFunction, 100);
+        }
+    }
+
+    waitForFunction();
+
+})();

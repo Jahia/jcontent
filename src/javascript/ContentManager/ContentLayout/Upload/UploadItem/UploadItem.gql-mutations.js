55ebecfe0debea3ebebb208379ef0f592fcfd2c1
oyevstafyev <oievstafiev@jahia.com>
2019-02-26 13:34:26 +0100

    [BACKLOG-9671] Minor improvements

--- src/javascript/ContentManager/ContentLayout/Upload/UploadItem/UploadItem.gql-mutations.js
+++ src/javascript/ContentManager/ContentLayout/Upload/UploadItem/UploadItem.gql-mutations.js
@@ -2,13 +2,13 @@ import gql from 'graphql-tag';
 
 const uploadFile = gql`mutation uploadFile($nameInJCR: String!, $path: String!, $mimeType: String!, $fileHandle: String!) {
     jcr {
-        addNode(name:$nameInJCR, parentPathOrId:$path, primaryNodeType:"jnt:file") {
-            addChild(name:"jcr:content", primaryNodeType:"jnt:resource") {
-                content: mutateProperty(name:"jcr:data") {
-                    setValue(type:BINARY, value:$fileHandle)
+        addNode(name: $nameInJCR, parentPathOrId: $path, primaryNodeType: "jnt:file") {
+            addChild(name: "jcr:content", primaryNodeType: "jnt:resource") {
+                content: mutateProperty(name: "jcr:data") {
+                    setValue(type: BINARY, value: $fileHandle)
                 }
-                contentType: mutateProperty(name:"jcr:mimeType") {
-                    setValue(value:$mimeType)
+                contentType: mutateProperty(name: "jcr:mimeType") {
+                    setValue(value: $mimeType)
                 }
             }
         }
@@ -17,22 +17,22 @@ const uploadFile = gql`mutation uploadFile($nameInJCR: String!, $path: String!, 
 
 const updateFileContent = gql`mutation updateFileContent($path: String!, $mimeType: String!, $fileHandle: String!) {
     jcr {
-        mutateNode(pathOrId:$path) {
-            mutateChildren(names:["jcr:content"]) {
-                content: mutateProperty(name:"jcr:data") {
-                    setValue(type:BINARY, value:$fileHandle)
+        mutateNode(pathOrId: $path) {
+            mutateChildren(names: ["jcr:content"]) {
+                content: mutateProperty(name: "jcr:data") {
+                    setValue(type: BINARY, value: $fileHandle)
                 }
-                contentType: mutateProperty(name:"jcr:mimeType") {
-                    setValue(value:$mimeType)
+                contentType: mutateProperty(name: "jcr:mimeType") {
+                    setValue(value: $mimeType)
                 }
             }
         }
     }
 }`;
 
-const importContent = gql`mutation importContent($path: String!, $fileHandle: String!){
+const importContent = gql`mutation importContent($path: String!, $fileHandle: String!) {
     jcr {
-        importNode(parentPathOrId: $path, file: $fileHandle)
+        importContent(parentPathOrId: $path, file: $fileHandle)
     }
 }`;
 

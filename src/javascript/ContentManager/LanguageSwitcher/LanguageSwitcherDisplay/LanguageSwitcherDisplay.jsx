f2beb11bd26e635a6c955720c571aea5f7c796af
Wladimir Delenclos <wdelenclos@gmail.com>
2019-03-04 17:19:50 +0100

    [BACKLOG-9643] Add conditional rendering for language

--- src/javascript/ContentManager/LanguageSwitcher/LanguageSwitcherDisplay/LanguageSwitcherDisplay.jsx
+++ src/javascript/ContentManager/LanguageSwitcher/LanguageSwitcherDisplay/LanguageSwitcherDisplay.jsx
@@ -29,36 +29,57 @@ export default class LanguageSwitcherDisplay extends React.Component {
     render() {
         let {lang, languages, onSelectLanguage} = this.props;
         let {anchorEl} = this.state;
-
         return (
             <React.Fragment>
-                <Button aria-owns={anchorEl ? 'language-switcher' : null}
-                        aria-haspopup="true"
-                        data-cm-role="language-switcher"
-                        color="inherit"
-                        onClick={this.handleClick}
-                >
-                    <Typography noWrap variant="body1" color="inherit">
-                        {this.uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
-                    &nbsp;
-                    </Typography>
-                    <ChevronDown fontSize="small" color="inherit"/>
-                </Button>
-                <Menu id="language-switcher" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
-                    {languages.map(lang => {
-                    return (
-                        <MenuItem
-                            key={lang.language}
-                            onClick={() => {
-                            onSelectLanguage(lang.language);
-                            this.handleClose();
-                        }}
+                {languages.length > 1 ? (
+                    <React.Fragment>
+                        <Button aria-owns={anchorEl ? 'language-switcher' : null}
+                                aria-haspopup="true"
+                                data-cm-role="language-switcher"
+                                color="inherit"
+                                onClick={this.handleClick}
                         >
-                            {this.uppercaseFirst(lang.displayName)}
-                        </MenuItem>
-                    );
-                })}
-                </Menu>
+                            <Typography noWrap variant="body1" color="inherit">
+                                {this.uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
+                                &nbsp;
+                            </Typography>
+                            <ChevronDown fontSize="small" color="inherit"/>
+                        </Button>
+                        <Menu id="language-switcher"
+                              anchorEl={anchorEl}
+                              open={Boolean(anchorEl)}
+                              onClose={this.handleClose}
+                        >
+                            {languages.map(lang => {
+                                return (
+                                    <MenuItem
+                                        key={lang.language}
+                                        onClick={() => {
+                                            onSelectLanguage(lang.language);
+                                            this.handleClose();
+                                        }}
+                                    >
+                                        {this.uppercaseFirst(lang.displayName)}
+                                    </MenuItem>
+                                );
+                            })}
+                        </Menu>
+                    </React.Fragment>
+                ) : (
+                    <React.Fragment>
+                        <Button aria-owns={anchorEl ? 'language-switcher' : null}
+                                aria-haspopup="true"
+                                data-cm-role="language-switcher"
+                                color="inherit"
+                                onClick={this.handleClick}
+                        >
+                            <Typography noWrap variant="body1" color="inherit">
+                                {this.uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
+                                &nbsp;
+                            </Typography>
+                        </Button>
+                    </React.Fragment>
+                )}
             </React.Fragment>
         );
     }

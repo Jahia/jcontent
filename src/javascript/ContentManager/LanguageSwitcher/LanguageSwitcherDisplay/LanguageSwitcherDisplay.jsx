e50482ff9c1a376c39bb3ab59776227df4b87f82
tdraier <tdraier@jahia.com>
2019-03-12 15:10:38 +0100

    BACKLOG-9800 : replaced buttons in language/site switcher

--- src/javascript/ContentManager/LanguageSwitcher/LanguageSwitcherDisplay/LanguageSwitcherDisplay.jsx
+++ src/javascript/ContentManager/LanguageSwitcher/LanguageSwitcherDisplay/LanguageSwitcherDisplay.jsx
@@ -1,87 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {lodash as _} from 'lodash';
-import {Button, Menu, MenuItem} from '@material-ui/core';
-import {Typography} from '@jahia/ds-mui-theme';
+import {Menu, MenuItem} from '@material-ui/core';
+import {Button, Typography} from '@jahia/ds-mui-theme';
 import {ChevronDown} from 'mdi-material-ui';
 
-export default class LanguageSwitcherDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            anchorEl: null
-        };
-        this.handleClick = this.handleClick.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-        this.uppercaseFirst = this.uppercaseFirst.bind(this);
-    }
+export const LanguageSwitcherDisplay = ({lang, languages, onSelectLanguage}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
 
-    handleClick(event) {
-        this.setState({anchorEl: event.currentTarget});
-    }
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
 
-    handleClose() {
-        this.setState({anchorEl: null});
-    }
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-    uppercaseFirst(string) {
+    const uppercaseFirst = string => {
         return string.charAt(0).toUpperCase() + string.substr(1);
-    }
+    };
 
-    render() {
-        let {lang, languages, onSelectLanguage} = this.props;
-        let {anchorEl} = this.state;
-        return (
-            <React.Fragment>
-                {languages.length > 1 ? (
-                    <React.Fragment>
-                        <Button aria-owns={anchorEl ? 'language-switcher' : null}
-                                aria-haspopup="true"
-                                data-cm-role="language-switcher"
-                                color="inherit"
-                                onClick={this.handleClick}
-                        >
-                            <Typography noWrap variant="zeta" color="inherit">
-                                {this.uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
-                                &nbsp;
-                            </Typography>
-                            <ChevronDown fontSize="small" color="inherit"/>
-                        </Button>
-                        <Menu id="language-switcher"
-                              anchorEl={anchorEl}
-                              open={Boolean(anchorEl)}
-                              onClose={this.handleClose}
-                        >
-                            {languages.map(lang => {
-                                return (
-                                    <MenuItem
-                                        key={lang.language}
-                                        onClick={() => {
-                                            onSelectLanguage(lang.language);
-                                            this.handleClose();
-                                        }}
-                                    >
-                                        {this.uppercaseFirst(lang.displayName)}
-                                    </MenuItem>
-                                );
-                            })}
-                        </Menu>
-                    </React.Fragment>
-                ) : (
-                    <React.Fragment>
-                        <Button aria-owns={anchorEl ? 'language-switcher' : null}
-                                aria-haspopup="true"
-                                data-cm-role="language-switcher"
-                                color="inherit"
-                                onClick={this.handleClick}
-                        >
-                            <Typography noWrap variant="zeta" color="inherit">
-                                {this.uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
-                                &nbsp;
-                            </Typography>
-                        </Button>
-                    </React.Fragment>
-                )}
-            </React.Fragment>
-        );
-    }
-}
+    return (
+        <React.Fragment>
+            {languages.length > 1 ? (
+                <React.Fragment>
+                    <Button aria-owns={anchorEl ? 'language-switcher' : null}
+                            aria-haspopup="true"
+                            data-cm-role="language-switcher"
+                            size="compact"
+                            color="inverted"
+                            onClick={handleClick}
+                    >
+                        <Typography noWrap variant="zeta" color="inherit">
+                            {uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
+                            &nbsp;
+                        </Typography>
+                        <ChevronDown fontSize="small" color="inherit"/>
+                    </Button>
+                    <Menu id="language-switcher"
+                          anchorEl={anchorEl}
+                          open={Boolean(anchorEl)}
+                          onClose={handleClose}
+                    >
+                        {languages.map(lang => {
+                            return (
+                                <MenuItem
+                                    key={lang.language}
+                                    onClick={() => {
+                                        onSelectLanguage(lang.language);
+                                        handleClose();
+                                    }}
+                                >
+                                    {uppercaseFirst(lang.displayName)}
+                                </MenuItem>
+                            );
+                        })}
+                    </Menu>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <Button aria-owns={anchorEl ? 'language-switcher' : null}
+                            aria-haspopup="true"
+                            data-cm-role="language-switcher"
+                            size="compact"
+                            color="inverted"
+                            onClick={handleClick}
+                    >
+                        <Typography noWrap variant="zeta" color="inherit">
+                            {uppercaseFirst(_.find(languages, language => language.language === lang).displayName)}
+                            &nbsp;
+                        </Typography>
+                    </Button>
+                </React.Fragment>
+            )}
+        </React.Fragment>
+    );
+};
+
+export default LanguageSwitcherDisplay;
--- src/javascript/ContentManager/SiteSwitcher/SiteSwitcherDisplay/SiteSwitcherDisplay.jsx
+++ src/javascript/ContentManager/SiteSwitcher/SiteSwitcherDisplay/SiteSwitcherDisplay.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {Button, Menu, MenuItem, withStyles} from '@material-ui/core';
-import {Typography} from '@jahia/ds-mui-theme';
+import React, {useState} from 'react';
+import {Menu, MenuItem, withStyles} from '@material-ui/core';
+import {Button, Typography} from '@jahia/ds-mui-theme';
 import {ChevronDown} from 'mdi-material-ui';
 import {lodash as _} from 'lodash';
 import {compose} from 'react-apollo';
@@ -11,66 +11,56 @@ const styles = () => ({
     }
 });
 
-class SiteSwitcherDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            anchorEl: null
-        };
-        this.handleClick = this.handleClick.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-    }
+export const SiteSwitcherDisplay = ({siteKey, siteNodes, loading, onSelectSite, classes, currentLang}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
 
-    handleClick(event) {
-        this.setState({anchorEl: event.currentTarget});
-    }
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
 
-    handleClose() {
-        this.setState({anchorEl: null});
-    }
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
 
-    render() {
-        let {siteKey, siteNodes, loading, onSelectSite, classes, currentLang} = this.props;
-        let {anchorEl} = this.state;
-
-        if (loading) {
-            return <span>Loading...</span>;
-        }
-        const siteNode = _.find(siteNodes, siteNode => siteNode.name === siteKey);
-        return (
-            <React.Fragment>
-                <Button aria-owns={anchorEl ? 'site-switcher' : null}
-                        color="inherit"
-                        aria-haspopup="true"
-                        data-cm-role="site-switcher"
-                        className={classes.siteSwitcher}
-                        onClick={this.handleClick}
-                >
-                    <Typography noWrap variant="zeta" color="inherit">
-                        {siteNode.displayName}
-                    </Typography>
-                    &nbsp;
-                    <ChevronDown fontSize="small" color="inherit"/>
-                </Button>
-                <Menu id="site-switcher" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
-                    {siteNodes.map(siteNode => {
-                        return (
-                            <MenuItem
-                                key={siteNode.uuid}
-                                onClick={() => {
-                                    onSelectSite(siteNode, currentLang);
-                                    this.handleClose();
-                                }}
-                            >
-                                {siteNode.displayName}
-                            </MenuItem>
-                        );
-                    })}
-                </Menu>
-            </React.Fragment>
-        );
+    if (loading) {
+        return <span>Loading...</span>;
     }
-}
+    const siteNode = _.find(siteNodes, siteNode => siteNode.name === siteKey);
+    return (
+        <React.Fragment>
+            <Button aria-owns={anchorEl ? 'site-switcher' : null}
+                    size="compact"
+                    color="inverted"
+                    aria-haspopup="true"
+                    data-cm-role="site-switcher"
+                    className={classes.siteSwitcher}
+                    onClick={handleClick}
+            >
+                <Typography noWrap variant="zeta" color="inherit">
+                    {siteNode.displayName}
+                </Typography>
+                &nbsp;
+                <ChevronDown fontSize="small" color="inherit"/>
+            </Button>
+            <Menu id="site-switcher" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+                {siteNodes.map(siteNode => {
+                    return (
+                        <MenuItem
+                            key={siteNode.uuid}
+                            onClick={() => {
+                                onSelectSite(siteNode, currentLang);
+                                handleClose();
+                            }}
+                        >
+                            {siteNode.displayName}
+                        </MenuItem>
+                    );
+                })}
+            </Menu>
+        </React.Fragment>
+    );
+};
+
 export default compose(
     withStyles(styles),
 )(SiteSwitcherDisplay);

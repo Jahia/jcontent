4ce58a1ef92b5fcf525ccc430168ab08de84a1a9
tdraier <tdraier@jahia.com>
2019-03-13 15:45:03 +0100

    BACKLOG-9800 : Added help message

--- src/javascript/ContentManager/routes/initRoutes.jsx
+++ src/javascript/ContentManager/routes/initRoutes.jsx
@@ -7,19 +7,34 @@ import SearchBar from '../ContentLayout/SearchBar';
 import ContentLayout from '../ContentLayout';
 import ImageEditor from '../ImageEditor';
 import {actionsRegistry} from '@jahia/react-material';
+import {Trans} from 'react-i18next';
 
 function initRoutes(registry) {
+    let help = (
+        <Trans i18nKey="label.contentManager.link.academy"
+               components={[
+                   <a key="academyLink"
+                      href={contextJsParameters.config.academyLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                   >.
+                   </a>
+               ]}/>
+    );
+
     registry.add('app-route', {
         type: 'route',
         target: ['cmm:50'],
         path: '/:siteKey/:lang/apps/:menu/:entry',
         render: (props, {dxContext, t}) => (
-            <MainLayout topBarProps={{
-                path: t('label.contentManager.appTitle', {path: ''}),
-                title: t([actionsRegistry.get(props.match.params.menu).buttonLabel, 'label.contentManager.leftMenu.manage.title']),
-                contextModifiers: <React.Fragment><SiteSwitcher/> <LanguageSwitcher/></React.Fragment>,
-                actions: <React.Fragment></React.Fragment>
-            }}
+            <MainLayout
+                topBarProps={{
+                    path: t('label.contentManager.appTitle', {path: ''}),
+                    title: t([actionsRegistry.get(props.match.params.menu).buttonLabel, 'label.contentManager.leftMenu.manage.title']),
+                    contextModifiers: <React.Fragment><SiteSwitcher/> <LanguageSwitcher/></React.Fragment>,
+                    actions: <React.Fragment></React.Fragment>
+                }}
+                help={help}
             >
                 <FullWidthContent>
                     <IFrameLayout
@@ -44,12 +59,14 @@ function initRoutes(registry) {
         target: ['cmm:99'],
         path: '/:siteKey/:lang/:mode',
         render: (props, {t}) => (
-            <MainLayout topBarProps={{
-                path: t('label.contentManager.appTitle', {path: ''}),
-                title: t('label.contentManager.title.' + props.match.params.mode),
-                contextModifiers: <React.Fragment><SiteSwitcher/><LanguageSwitcher/></React.Fragment>,
-                actions: <React.Fragment><SearchBar/></React.Fragment>
-            }}
+            <MainLayout
+                topBarProps={{
+                    path: t('label.contentManager.appTitle', {path: ''}),
+                    title: t('label.contentManager.title.' + props.match.params.mode),
+                    contextModifiers: <React.Fragment><SiteSwitcher/><LanguageSwitcher/></React.Fragment>,
+                    actions: <React.Fragment><SearchBar/></React.Fragment>
+                }}
+                help={help}
             >
                 <FullWidthContent>
                     <ContentLayout/>

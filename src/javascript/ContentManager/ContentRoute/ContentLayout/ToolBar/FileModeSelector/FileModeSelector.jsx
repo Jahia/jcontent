87e5e3abb826c7d6100171d6e13b66b95ea629f9
Yousria <yousria.benchadi@gmail.com>
2019-04-01 16:36:12 +0200

    BACKLOG-9997 : add missing proptypes

--- src/javascript/ContentManager/ContentRoute/ContentLayout/ToolBar/FileModeSelector/FileModeSelector.jsx
+++ src/javascript/ContentManager/ContentRoute/ContentLayout/ToolBar/FileModeSelector/FileModeSelector.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {FormControl, MenuItem} from '@material-ui/core';
+import {MenuItem} from '@material-ui/core';
 import {Select} from '@jahia/ds-mui-theme';
 import {compose} from 'react-apollo';
 import {connect} from 'react-redux';
 import {translate} from 'react-i18next';
 import {setMode, setSize} from '../../FilesGrid/FilesGrid.redux-actions';
+import PropTypes from "prop-types";
 
 export class FileModeSelector extends React.Component {
     constructor(props) {
@@ -61,6 +62,14 @@ export class FileModeSelector extends React.Component {
         );
     }
 }
+
+FileModeSelector.propTypes = {
+    t: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    setSize: PropTypes.func.isRequired,
+    mode: PropTypes.string.isRequired,
+    size: PropTypes.number.isRequired
+};
 
 export default compose(
     connect(state => ({mode: state.filesGrid.mode, size: state.filesGrid.size}), dispatch => ({

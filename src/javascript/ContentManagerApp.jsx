43d51b0dff0d81031a59bf6ec1eeaab6840a3df8
Thomas Draier <tdraier@jahia.com>
2019-04-08 15:04:31 +0200

    QA-11620 : catch link event to redirect to preview

--- src/javascript/ContentManagerApp.jsx
+++ src/javascript/ContentManagerApp.jsx
@@ -4,6 +4,12 @@ import ContentManager from './ContentManager';
 import {CssBaseline} from '@material-ui/core';
 
 function renderContentManager(target, id, dxContext) {
+    window.parent.addEventListener('click', e => {
+        if (Array.prototype.slice.call(window.parent.document.querySelectorAll('#JahiaGxtEditEnginePanel-usages a[target=\'_blank\'')).indexOf(e.target) > -1) {
+            e.target.href = e.target.href.replace('contentmanager', 'preview');
+        }
+    });
+
     ReactDOM.render(
         <React.Fragment>
             <CssBaseline/>

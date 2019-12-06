ae29a5b53e6541a1db7b50d5af03f8bd61cb5b14
Yousria <ybenchadi@jahia.com>
2019-12-06 14:40:01 +0100

    BACKLOG-11688 : fix the unzip not appearing issue on windows (#68)

--- src/javascript/ContentManager/actions/zipUnzip/unzipAction.jsx
+++ src/javascript/ContentManager/actions/zipUnzip/unzipAction.jsx
@@ -13,7 +13,7 @@ export default composeActions(requirementsAction, withNotificationContextAction,
             retrieveProperties: {retrievePropertiesNames: ['jcr:mixinTypes']},
             requiredPermission: 'jcr:addChildNodes',
             retrieveMimeType: true,
-            enabled: context => context.node.pipe(map(node => node.children.nodes.length !== 0 && node.children.nodes[0].mimeType.value === 'application/zip' && !isMarkedForDeletion(node)))
+            enabled: context => context.node.pipe(map(node => node.children.nodes.length !== 0 && (node.children.nodes[0].mimeType.value === 'application/zip' || node.children.nodes[0].mimeType.value === 'application/x-zip-compressed') && !isMarkedForDeletion(node)))
         });
     },
     onClick: context => {

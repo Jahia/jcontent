183a6477d9b1a9acb9e863750ebb905c4d7dd729
Cedric Moitrier <cmoitrier@jahia.com>
2020-01-31 13:52:14 +0100

    BACKLOG-12226: simplify impl of AppLayoutContainer

--- src/javascript/JContent/AppLayout/AppLayout.container.jsx
+++ src/javascript/JContent/AppLayout/AppLayout.container.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {compose} from 'react-apollo';
-import {withTranslation} from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import {LayoutModule, SecondaryNav} from '@jahia/moonstone';
 import ContentNavigation from '../ContentNavigation';
-import {Route, Switch, withRouter} from 'react-router';
+import {Route, Switch, useParams} from 'react-router';
 import {registry} from '@jahia/ui-extender';
 import JContentNavigationHeader from './NavigationHeader';
 
-export class AppLayoutContainer extends React.Component {
-    render() {
-        let routes = registry.find({type: 'route', target: 'jcontent'});
-        const {dxContext, t, match} = this.props;
-
-        return (
-            <LayoutModule navigation={
+const AppLayoutContainer = ({dxContext}) => {
+    const routes = registry.find({type: 'route', target: 'jcontent'});
+    const {t} = useTranslation('jcontent');
+    const {mode} = useParams();
+    return (
+        <LayoutModule
+            navigation={
                 <SecondaryNav header={<JContentNavigationHeader/>}>
-                    <ContentNavigation mode={match.params.mode}/>
+                    <ContentNavigation mode={mode}/>
                 </SecondaryNav>
             }
-                          content={
-                              <Switch>
-                                  {routes.map(r =>
-                                      <Route key={r.key} path={r.path} render={props => r.render(props, {dxContext, t})}/>
-                                  )}
-                              </Switch>
-                          }/>
-        );
-    }
-}
-
-AppLayoutContainer.propTypes = {
-    dxContext: PropTypes.object.isRequired,
-    match: PropTypes.object.isRequired,
-    t: PropTypes.func.isRequired
+            content={
+                <Switch>
+                    {routes.map(r =>
+                        <Route key={r.key} path={r.path} render={props => r.render(props, {dxContext, t})}/>
+                        )}
+                </Switch>
+            }
+        />
+    );
 };
 
-export default compose(
-    withRouter,
-    withTranslation()
-)(AppLayoutContainer);
+AppLayoutContainer.propTypes = {
+    dxContext: PropTypes.object.isRequired
+};
+
+export default AppLayoutContainer;

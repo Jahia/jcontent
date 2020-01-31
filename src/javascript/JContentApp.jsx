ac0e63e7e665bf098a0dfefc97bab100af1e3b47
Thomas Draier <tdraier@jahia.com>
2020-01-31 13:37:55 +0100

    BACKLOG-12159 : use context attributes from app shell

--- src/javascript/JContentApp.jsx
+++ src/javascript/JContentApp.jsx
@@ -11,33 +11,11 @@ const JContentApp = () => {
         return '';
     }
 
-    // TODO there parameters need to come from jahia-ui-root context which should become accessible once dx-commons and jur is merged
-    const ctx = {
-        urlbase: '/modules/moonstone/jcontent', // This one goes right after context path
-        langName: window.contextJsParameters.locale,
-        uilang: window.contextJsParameters.locale,
-        userName: window.contextJsParameters.user.fullname,
-        workspace: window.contextJsParameters.workspace,
-        contextPath: window.contextJsParameters.contextPath,
-        siteKey: window.contextJsParameters.siteKey
-    };
-
     // No that by default site is always set to system site in ContentManage.jsx getHistory method
     return (
         <>
             <CssBaseline/>
-            <JContent dxContext={{
-                siteDisplayableName: '',
-                maxUploadSize: 10000000,
-                displayWorkflowCounter: true,
-                config: {
-                    sql2CheatSheetUrl: '',
-                    actions: [],
-                    importAcademyLink: 'https://academy.jahia.com/documentation/enduser/dx/7.3/using-jahia/using-content-and-media-manager#exporting_importing_contents',
-                    ...window.contextJsParameters.config
-                },
-                ...ctx
-            }}/>
+            <JContent dxContext={window.contextJsParameters}/>
         </>
     );
 };

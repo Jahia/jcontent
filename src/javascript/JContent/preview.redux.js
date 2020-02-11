2a7d0768c632510ac023e40d5d9aa416b8a23f49
Thomas Draier <tdraier@jahia.com>
2020-02-11 17:58:46 +0100

    BACKLOG-12335: fixed preview (#115)

--- src/javascript/JContent/preview.redux.js
+++ src/javascript/JContent/preview.redux.js
@@ -4,7 +4,7 @@ import {createActions, handleAction, handleActions} from 'redux-actions';
 export const {cmSetPreviewMode, cmSetPreviewState} = createActions('CM_SET_PREVIEW_MODE', 'CM_SET_PREVIEW_STATE');
 
 export const cmSetPreviewSelection = previewSelection => (dispatch, getState) => {
-    if (!previewSelection || getState().selection.length === 0) {
+    if (!previewSelection || getState().jcontent.selection.length === 0) {
         dispatch({
             type: 'CM_SET_PREVIEW_SELECTION',
             payload: previewSelection

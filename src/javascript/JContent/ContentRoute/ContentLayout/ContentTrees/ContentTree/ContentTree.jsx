d85398f96e5c38eb7c621062a3b66bec413c3ec4
Yousria <ybenchadi@jahia.com>
2020-02-13 11:50:09 +0100

    BACKLOG-12343 : use the new TreeView component (#117)

--- src/javascript/JContent/ContentRoute/ContentLayout/ContentTrees/ContentTree/ContentTree.jsx
+++ src/javascript/JContent/ContentRoute/ContentLayout/ContentTrees/ContentTree/ContentTree.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Picker} from '@jahia/react-apollo';
 import {PredefinedFragments} from '@jahia/apollo-dx';
-import PickerViewMaterial from './PickerViewMaterial';
+import {TreeView} from '@jahia/moonstone';
 import gql from 'graphql-tag';
+import JContentConstants from '../../../../JContent.constants';
+import {Collections, File, FolderSpecial, Setting} from '@jahia/moonstone/dist/icons';
 
 const PickerItemsFragment = {
     mixinTypes: {
@@ -38,6 +40,72 @@ const PickerItemsFragment = {
     }
 };
 
+function getIcon(mode) {
+    switch (mode) {
+        case JContentConstants.mode.PAGES:
+            return <File size="small"/>;
+        case JContentConstants.mode.CONTENT_FOLDERS:
+            return <FolderSpecial size="small"/>;
+        case JContentConstants.mode.MEDIA:
+            return <Collections size="small"/>;
+        case JContentConstants.mode.APPS:
+            return <Setting size="small"/>;
+        default:
+            return <File size="small"/>;
+    }
+}
+
+function getParentPath(path) {
+    return path.substr(0, path.lastIndexOf('/'));
+}
+
+function findInTree(tree, id) {
+    for (var i = 0; i < tree.length; i++) {
+        if (tree[i].id === id) {
+            return tree[i];
+        }
+
+        let result = findInTree(tree[i].children, id);
+        if (result) {
+            return result;
+        }
+    }
+}
+
+function convertPathsToTree(pickerEntries, mode) {
+    let tree = [];
+    if (pickerEntries.length === 0) {
+        return tree;
+    }
+
+    let rootElement = {
+        id: pickerEntries[0].path,
+        label: pickerEntries[0].node.displayName,
+        hasChildren: pickerEntries[0].hasChildren,
+        parent: getParentPath(pickerEntries[0].path),
+        iconStart: getIcon(mode),
+        children: []
+    };
+    tree.push(rootElement);
+    for (let i = 1; i < pickerEntries.length; i++) {
+        let parentPath = getParentPath(pickerEntries[i].path);
+        let element = {
+            id: pickerEntries[i].path,
+            label: pickerEntries[i].node.displayName,
+            hasChildren: pickerEntries[i].hasChildren,
+            parent: parentPath,
+            iconStart: getIcon(mode),
+            children: []
+        };
+        let parent = findInTree(tree, parentPath);
+        if (parent !== undefined) {
+            parent.children.push(element);
+        }
+    }
+
+    return tree;
+}
+
 class ContentTree extends React.Component {
     constructor(props) {
         super(props);
@@ -45,33 +113,40 @@ class ContentTree extends React.Component {
     }
 
     render() {
-        let {rootPath, path, openPaths, handleOpen, handleSelect, lang, openableTypes, selectableTypes, rootLabel, setRefetch, dataCmRole, container, mode} = this.props;
+        let {rootPath, openPaths, path, handleOpen, handleSelect, lang, openableTypes, selectableTypes, setRefetch, mode} = this.props;
         return (
             <Picker
                 ref={this.picker}
+                openSelection
                 rootPaths={[rootPath]}
                 openPaths={openPaths}
                 openableTypes={openableTypes}
                 selectableTypes={selectableTypes}
                 queryVariables={{lang: lang}}
                 selectedPaths={[path]}
-                openSelection={false}
                 setRefetch={setRefetch}
                 fragments={[PickerItemsFragment.mixinTypes, PickerItemsFragment.primaryNodeType, PickerItemsFragment.isPublished, PredefinedFragments.displayName]}
                 onOpenItem={(path, open) => handleOpen(path, open)}
                 onSelectItem={path => handleSelect(path)}
             >
-                {({handleSelect, ...others}) => (
-                    <PickerViewMaterial {...others} dataCmRole={dataCmRole} rootLabel={rootLabel} container={container} mode={mode}/>
-                )}
+                {({pickerEntries}) => {
+                    return (
+                        <TreeView isReversed
+                                  data={convertPathsToTree(pickerEntries, mode)}
+                                  openedItems={openPaths}
+                                  selectedItems={[path]}
+                                  onClickItem={object => handleSelect(object.id)}
+                                  onOpenItem={object => handleOpen(object.id, true)}
+                                  onCloseItem={object => handleOpen(object.id, false)}
+                        />
+                    );
+                }}
             </Picker>
         );
     }
 }
 
 ContentTree.propTypes = {
-    container: PropTypes.object.isRequired,
-    dataCmRole: PropTypes.string.isRequired,
     handleOpen: PropTypes.func.isRequired,
     handleSelect: PropTypes.func.isRequired,
     lang: PropTypes.string.isRequired,
@@ -79,7 +154,6 @@ ContentTree.propTypes = {
     openPaths: PropTypes.arrayOf(PropTypes.string).isRequired,
     openableTypes: PropTypes.array.isRequired,
     path: PropTypes.string.isRequired,
-    rootLabel: PropTypes.string.isRequired,
     rootPath: PropTypes.string.isRequired,
     selectableTypes: PropTypes.array.isRequired,
     setRefetch: PropTypes.func.isRequired

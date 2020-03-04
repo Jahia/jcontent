6cbbb675eb83c2a5efe05647fdf4edcd4c00db80
Yousria <yousria.benchadi@gmail.com>
2020-03-04 13:52:17 +0100

    BACKLOG-12608 : use selection to handle one item selection with the edit action

--- src/javascript/JContent/ContentRoute/MainActionBar/MainActionBar.jsx
+++ src/javascript/JContent/ContentRoute/MainActionBar/MainActionBar.jsx
@@ -10,13 +10,14 @@ const ButtonRendererShortLabel = getButtonRenderer({labelStyle: 'short'});
 const ButtonRendererNoLabel = getButtonRenderer({labelStyle: 'none'});
 
 export const MainActionBar = () => {
-    const {path} = useSelector(state => ({path: state.jcontent.path}));
-
+    const {path, selection} = useSelector(state => ({path: state.jcontent.path, selection: state.jcontent.selection}));
+    let editPath = (selection && selection.length === 0) ? path : selection[0];
+    let enabled = selection.length < 2;
     return (
         <div className={styles.root}>
             <Separator variant="vertical"/>
             <DisplayAction actionKey="pageComposer" context={{path}} render={ButtonRenderer} size="big" variant="ghost" color="accent" className={styles.item}/>
-            <DisplayAction actionKey="edit" context={{path}} render={ButtonRenderer} size="big" variant="outlined" className={styles.item}/>
+            <DisplayAction actionKey="edit" context={{path: editPath, enabled: enabled}} render={ButtonRenderer} size="big" variant="outlined" className={styles.item}/>
             <ButtonGroup size="big" variant="default" color="accent" className={styles.item}>
                 <DisplayAction actionKey="publish" context={{path}} render={ButtonRendererShortLabel}/>
                 <DisplayAction actionKey="publishMenu" context={{path, menuFilter: ctx => ctx.key !== 'publish'}} render={ButtonRendererNoLabel}/>

41a14effbb82740ac785f2950f39af8997ff2173
Thomas Draier <tdraier@jahia.com>
2020-03-06 14:52:59 +0100

    BACKLOG-12425 : Added ContentTitle and test

--- src/javascript/JContent/ContentRoute/ContentTitle/ContentTitle.jsx
+++ src/javascript/JContent/ContentRoute/ContentTitle/ContentTitle.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import classnames from 'clsx';
-import PropTypes from 'prop-types';
 import {Typography} from '@jahia/moonstone';
 import styles from './ContentTitle.scss';
+import {useSelector} from 'react-redux';
+import {useNodeInfo} from '@jahia/data-helper';
 
-const ContentTitle = ({title}) => (
-    <div className={classnames(styles.root, 'alignCenter')}>
-        <Typography variant="title">
-            {title}
-        </Typography>
-    </div>
-);
+const ContentTitle = () => {
+    const {path, language} = useSelector(state => ({
+        path: state.jcontent.path,
+        language: state.language
+    }));
 
-ContentTitle.propTypes = {
-    title: PropTypes.string.isRequired
+    const {loading, node} = useNodeInfo({path: path, language: language}, {getDisplayName: true});
+
+    return (
+        <div className={classnames(styles.root, 'alignCenter')}>
+            <Typography variant="title" style={{opacity: loading ? 0 : 1}}>
+                {(!loading && node && node.displayName) || 'Loading ...'}
+            </Typography>
+        </div>
+    );
 };
 
 export default ContentTitle;
--- src/javascript/JContent/ContentRoute/ContentTitle/ContentTitle.test.jsx
+++ src/javascript/JContent/ContentRoute/ContentTitle/ContentTitle.test.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import {shallow} from '@jahia/test-framework';
 import ContentTitle from './ContentTitle';
+import {useSelector} from 'react-redux';
+import {useNodeInfo} from '@jahia/data-helper';
+
+jest.mock('react-redux', () => ({
+    useSelector: jest.fn()
+}));
+
+jest.mock('@jahia/data-helper', () => ({
+    useNodeInfo: jest.fn()
+}));
 
 describe('Content title', () => {
     it('Should render', async () => {
         const title = 'Hello Content Title!';
-        const contentTitle = shallow(<ContentTitle title={title}/>);
+        useSelector.mockImplementation(() => ({
+            path: 'testPath',
+            language: 'en'
+        }));
+
+        useNodeInfo.mockImplementation(() => ({
+            node: {
+                displayName: title
+            },
+            loading: false
+        }));
+
+        const contentTitle = shallow(<ContentTitle/>);
         expect(contentTitle.contains(title)).toBeTruthy();
+    });
+
+    it('Should render empty title while loading', async () => {
+        useSelector.mockImplementation(() => ({
+            path: 'testPath',
+            language: 'en'
+        }));
+
+        useNodeInfo.mockImplementation(() => ({
+            loading: true
+        }));
+
+        const contentTitle = shallow(<ContentTitle/>);
+        expect(contentTitle.find('Typography').props().style.opacity).toBe(0);
     });
 });

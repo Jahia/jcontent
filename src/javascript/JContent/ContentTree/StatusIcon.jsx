003b6abecb144ef0744c0d72521cce17e28751ff
Thomas Draier <tdraier@jahia.com>
2020-03-12 14:22:24 +0100

    BACKLOG-12278 : Added context menu in tree (#190)

---
+++ src/javascript/JContent/ContentTree/StatusIcon.jsx
@@ -0,0 +1,27 @@
+import {Lock, NoCloud} from '@jahia/moonstone/dist/icons';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContentTree.scss';
+import {DisplayAction} from '@jahia/ui-extender';
+import {getButtonRenderer} from '../../utils/getButtonRenderer';
+
+export const StatusIcon = ({isLocked, isNotPublished, path, ...props}) => {
+    return (
+        <>
+            <span className={styles.ContentTree_ItemStatusIcon}>
+                {(isLocked && <Lock {...props}/>) || (isNotPublished && <NoCloud {...props}/>)}
+            </span>
+            <span className={styles.ContentTree_ItemMenuIcon}>
+                <DisplayAction actionKey="contentMenu" context={{path}} render={getButtonRenderer({labelStyle: 'none', variant: 'ghost', isReversed: true, size: 'small'})} {...props}/>
+            </span>
+        </>
+    );
+};
+
+StatusIcon.propTypes = {
+    path: PropTypes.string.isRequired,
+
+    isLocked: PropTypes.bool.isRequired,
+
+    isNotPublished: PropTypes.bool.isRequired
+};

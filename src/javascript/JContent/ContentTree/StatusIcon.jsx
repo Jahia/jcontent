46123a7289e14dbe162b7fbc7a6d71e0d938a855
Thomas Draier <tdraier@jahia.com>
2020-03-12 14:17:54 +0100

    BACKLOG-12278 : rollbacked css changes

--- src/javascript/JContent/ContentTree/StatusIcon.jsx
+++ src/javascript/JContent/ContentTree/StatusIcon.jsx
@@ -7,14 +7,14 @@ import {getButtonRenderer} from '../../utils/getButtonRenderer';
 
 export const StatusIcon = ({isLocked, isNotPublished, path, ...props}) => {
     return (
-        <span className={styles.ContentTree_ItemIcon}>
+        <>
             <span className={styles.ContentTree_ItemStatusIcon}>
                 {(isLocked && <Lock {...props}/>) || (isNotPublished && <NoCloud {...props}/>)}
             </span>
             <span className={styles.ContentTree_ItemMenuIcon}>
                 <DisplayAction actionKey="contentMenu" context={{path}} render={getButtonRenderer({labelStyle: 'none', variant: 'ghost', isReversed: true, size: 'small'})} {...props}/>
             </span>
-        </span>
+        </>
     );
 };
 

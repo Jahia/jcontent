788d24f1c676176224cb90c2ef6e3cd848896046
Thomas Draier <tdraier@jahia.com>
2020-03-23 23:19:55 +0100

    Refactored zip with components

--- src/javascript/JContent/actions/zipUnzip/zipAction.jsx
+++ src/javascript/JContent/actions/zipUnzip/zipAction.jsx
@@ -1,62 +1,100 @@
-import React from 'react';
-import {componentRendererAction, composeActions, ProgressOverlay} from '@jahia/react-material';
-import requirementsAction from '../requirementsAction';
-import {withNotificationContextAction} from '../withNotificationContextAction';
+import React, {useContext} from 'react';
+import {ProgressOverlay, withNotifications} from '@jahia/react-material';
 import {triggerRefetchAll} from '../../JContent.refetches';
 import zipUnzipQueries from './zipUnzip.gql-queries';
 import zipUnzipMutation from './zipUnzip.gql-mutations';
 import {getNewCounter, removeFileExtension} from '../../JContent.utils';
-import {reduxAction} from '../reduxAction';
 import {cmClearSelection} from '../../ContentRoute/ContentLayout/contentSelection.redux';
+import {useDispatch} from 'react-redux';
+import {useApolloClient} from '@apollo/react-hooks';
+import {useNodeChecks} from '@jahia/data-helper';
+import {ComponentRendererContext} from '@jahia/ui-extender';
+import PropTypes from 'prop-types';
 
-export default composeActions(requirementsAction, withNotificationContextAction, componentRendererAction, reduxAction(() => ({}), dispatch => ({
-    clearSelection: () => dispatch(cmClearSelection())
-})), {
-    init: context => {
-        context.initRequirements({
-            requiredPermission: 'jcr:addChildNodes'
-        });
-    },
-    onClick: context => {
-        let name = context.node ? context.node.name : (context.nodes.length > 1 ? context.nodes[0].parent.name : context.nodes[0].name);
-        let nameWithoutExtension = removeFileExtension(name);
-        let paths = context.node ? [context.node.path] : context.paths;
-        let uuid = context.node ? context.node.uuid : context.nodes[0].uuid;
-        let parentPath = context.node ? context.node.parent.path : context.nodes[0].parent.path;
+export const ZipActionComponent = ({context, render: Render, loading: Loading, notificationContext}) => {
+    const componentRenderer = useContext(ComponentRendererContext);
+    const client = useApolloClient();
+    const dispatch = useDispatch();
 
-        // Query to have zip files in the same directory with the same name to add a counter
-        let siblings = context.client.query({
-            query: zipUnzipQueries.siblingsWithSameNameQuery,
-            variables: {uuid: uuid, name: nameWithoutExtension, extension: '.zip'},
-            fetchPolicy: 'network-only'
-        });
+    const res = useNodeChecks(
+        {path: context.path, paths: context.paths},
+        {
+            ...context,
+            getParent: true,
+            requiredPermission: ['jcr:addChildNodes']
+        }
+    );
 
-        let newName = '';
-        siblings.then(function (res) {
-            if (res.data && res.data.jcr && res.data.jcr.nodeById.parent.filteredSubNodes.nodes.length > 0) {
-                let siblings = res.data.jcr.nodeById.parent.filteredSubNodes.nodes;
-                newName = nameWithoutExtension.concat(getNewCounter(siblings) + '.zip');
-            } else {
-                newName = removeFileExtension(name).concat('.zip');
-            }
-
-            let handler = context.renderComponent(<ProgressOverlay/>);
-            // Zip mutation after calculating the new name of zip file
-            context.client.mutate({
-                variables: {parentPathOrId: parentPath, name: newName, paths: paths},
-                mutation: zipUnzipMutation.zip,
-                refetchQueries: [{
-                    query: zipUnzipQueries.siblingsWithSameNameQuery,
-                    variables: {uuid: uuid, name: nameWithoutExtension, extension: '.zip'}
-                }]
-            }).then(() => {
-                context.clearSelection();
-                triggerRefetchAll();
-                handler.destroy();
-            }).catch((reason => {
-                handler.destroy();
-                context.notificationContext.notify(reason.toString(), ['closeButton', 'noAutomaticClose']);
-            }));
-        });
+    if (res.loading && Loading) {
+        return <Loading context={context}/>;
     }
-});
+
+    if (!res.node && !res.nodes) {
+        return false;
+    }
+
+    let isVisible = res.checksResult;
+
+    return (
+        <Render context={{
+            ...context,
+            isVisible: isVisible,
+            enabled: isVisible,
+            onClick: () => {
+                let name = res.node ? res.node.name : (res.nodes.length > 1 ? res.nodes[0].parent.name : res.nodes[0].name);
+                let nameWithoutExtension = removeFileExtension(name);
+                let paths = res.node ? [res.node.path] : res.nodes.map(n => n.path);
+                let uuid = res.node ? res.node.uuid : res.nodes[0].uuid;
+                let parentPath = res.node ? res.node.parent.path : res.nodes[0].parent.path;
+
+                // Query to have zip files in the same directory with the same name to add a counter
+                let siblings = client.query({
+                    query: zipUnzipQueries.siblingsWithSameNameQuery,
+                    variables: {uuid: uuid, name: nameWithoutExtension, extension: '.zip'},
+                    fetchPolicy: 'network-only'
+                });
+
+                let newName = '';
+                siblings.then(function (siblingsRes) {
+                    if (siblingsRes.data && siblingsRes.data.jcr && siblingsRes.data.jcr.nodeById.parent.filteredSubNodes.nodes.length > 0) {
+                        let siblings = siblingsRes.data.jcr.nodeById.parent.filteredSubNodes.nodes;
+                        newName = nameWithoutExtension.concat(getNewCounter(siblings) + '.zip');
+                    } else {
+                        newName = removeFileExtension(name).concat('.zip');
+                    }
+
+                    componentRenderer.render('progressOverlay', ProgressOverlay);
+                    // Zip mutation after calculating the new name of zip file
+                    client.mutate({
+                        variables: {parentPathOrId: parentPath, name: newName, paths: paths},
+                        mutation: zipUnzipMutation.zip,
+                        refetchQueries: [{
+                            query: zipUnzipQueries.siblingsWithSameNameQuery,
+                            variables: {uuid: uuid, name: nameWithoutExtension, extension: '.zip'}
+                        }]
+                    }).then(() => {
+                        dispatch(cmClearSelection());
+                        triggerRefetchAll();
+                        componentRenderer.destroy('progressOverlay');
+                    }).catch((reason => {
+                        componentRenderer.destroy('progressOverlay');
+                        notificationContext.notify(reason.toString(), ['closeButton', 'noAutomaticClose']);
+                    }));
+                });
+            }
+        }}/>
+    );
+};
+
+ZipActionComponent.propTypes = {
+    context: PropTypes.object.isRequired,
+    render: PropTypes.func.isRequired,
+    loading: PropTypes.func,
+    notificationContext: PropTypes.object.isRequired
+};
+
+const zipAction = {
+    component: withNotifications()(ZipActionComponent)
+};
+
+export default zipAction;

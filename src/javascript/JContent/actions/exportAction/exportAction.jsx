8af574c0a8d618665c83995ca3eba59ddb6218c6
Thomas Draier <tdraier@jahia.com>
2020-03-23 22:58:44 +0100

    Refactored export with components

--- src/javascript/JContent/actions/exportAction/exportAction.jsx
+++ src/javascript/JContent/actions/exportAction/exportAction.jsx
@@ -1,28 +1,52 @@
-import React from 'react';
-import {composeActions, componentRendererAction} from '@jahia/react-material';
-import requirementsAction from '../requirementsAction';
-import {withDxContextAction} from '../withDxContextAction';
+import React, {useContext} from 'react';
 import Export from './Export';
+import {ComponentRendererContext} from '@jahia/ui-extender';
+import {useNodeInfo} from '@jahia/data-helper';
+import PropTypes from 'prop-types';
 
-export default composeActions(requirementsAction, withDxContextAction, componentRendererAction, {
+export const ExportActionComponent = ({context, render: Render, loading: Loading}) => {
+    const componentRenderer = useContext(ComponentRendererContext);
+    const res = useNodeInfo(
+        {path: context.path}
+    );
 
-    init: context => {
-        context.initRequirements({});
-    },
-
-    onClick: context => {
-        let handler = context.renderComponent(
-            <Export
-                open
-                contextPath={context.dxContext.contextPath}
-                path={context.node.path}
-                onClose={() => {
-                    handler.setProps({open: false});
-                }}
-                onExited={() => {
-                    handler.destroy();
-                }}
-            />
-        );
+    if (res.loading && Loading) {
+        return <Loading context={context}/>;
     }
-});
+
+    if (!res.node && !res.nodes) {
+        return false;
+    }
+
+    return (
+        <Render context={{
+            ...context,
+            isVisible: res.checksResult,
+            onClick: () => {
+                componentRenderer.render('exportDialog', Export, {
+                        open: true,
+                        path: res.node.path,
+                        onClose: () => {
+                            componentRenderer.setProperties('exportDialog', {open: false});
+                        },
+                        onExited: () => {
+                            componentRenderer.destroy('exportDialog');
+                        }
+                    }
+                );
+            }
+        }}/>
+    );
+};
+
+ExportActionComponent.propTypes = {
+    context: PropTypes.object.isRequired,
+    render: PropTypes.func.isRequired,
+    loading: PropTypes.func
+};
+
+const exportAction = {
+    component: ExportActionComponent
+};
+
+export default exportAction;

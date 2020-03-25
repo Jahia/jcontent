95b439e781cbaeaf874bcc7129ce3b6a4f097a9a
Thomas Draier <tdraier@jahia.com>
2020-03-25 13:51:45 +0100

    Refactored clearAllLockst with components

--- src/javascript/JContent/actions/clearAllLocksAction.jsx
+++ src/javascript/JContent/actions/clearAllLocksAction.jsx
@@ -1,34 +1,68 @@
-import {composeActions} from '@jahia/react-material';
-import requirementsAction from './requirementsAction';
-import {map} from 'rxjs/operators';
+import React from 'react';
 import gql from 'graphql-tag';
 import * as _ from 'lodash';
-import {withDxContextAction} from './withDxContextAction';
+import {useNodeChecks} from '@jahia/data-helper';
+import PropTypes from 'prop-types';
+import {useApolloClient} from '@apollo/react-hooks';
 
-export default composeActions(requirementsAction, withDxContextAction, {
-    init: context => {
-        context.initRequirements({
+export const ClearAllLockActionComponent = ({context, render: Render, loading: Loading}) => {
+    const client = useApolloClient();
+    const res = useNodeChecks(
+        {path: context.path},
+        {
             getLockInfo: true,
-            requiredPermission: 'clearLock',
-            enabled: context => context.node.pipe(map(node => node.lockTypes !== null && !_.includes(node.lockTypes.values, ' deletion :deletion')))
-        });
-    },
-    onClick: context => {
-        context.client.mutate({
-            variables: {pathOrId: context.path},
-            mutation: gql`mutation clearAllLocks($pathOrId: String!) {
-                jcr {
-                    mutateNode(pathOrId: $pathOrId) {
-                        clearAllLocks
-                    }
-                }
-            }`,
-            refetchQueries: [
-                {
-                    query: context.requirementQueryHandler.getQuery(),
-                    variables: context.requirementQueryHandler.getVariables()
-                }
-            ]
-        });
+            requiredPermission: ['clearLock'],
+            ...context
+        }
+    );
+
+    if (res.loading && Loading) {
+        return <Loading context={context}/>;
     }
-});
+
+    if (!res.node) {
+        return false;
+    }
+
+    const isVisible = res.checksResult && res.node.lockTypes !== null && !_.includes(res.node.lockTypes.values, ' deletion :deletion');
+
+    return (
+        <Render context={{
+            ...context,
+            isVisible: isVisible,
+            enabled: isVisible,
+            onClick: () => {
+                client.mutate({
+                    variables: {pathOrId: context.path},
+                    mutation: gql`mutation clearAllLocks($pathOrId: String!) {
+                        jcr {
+                            mutateNode(pathOrId: $pathOrId) {
+                                clearAllLocks
+                            }
+                        }
+                    }`,
+                    refetchQueries: [
+                        {
+                            query: res.query,
+                            variables: res.variables
+                        }
+                    ]
+                });
+            }
+        }}/>
+    );
+};
+
+ClearAllLockActionComponent.propTypes = {
+    context: PropTypes.object.isRequired,
+
+    render: PropTypes.func.isRequired,
+
+    loading: PropTypes.func
+};
+
+const clearAllLocksAction = {
+    component: ClearAllLockActionComponent
+};
+
+export default clearAllLocksAction;

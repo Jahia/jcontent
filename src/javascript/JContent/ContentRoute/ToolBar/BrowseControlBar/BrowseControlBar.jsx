4b381a051b7cbded6bbf008243d6e2a40aad0963
Thomas Draier <tdraier@jahia.com>
2020-03-25 13:49:04 +0100

    BACKLOG-12512 : Added additional actions in primary header

--- src/javascript/JContent/ContentRoute/ToolBar/BrowseControlBar/BrowseControlBar.jsx
+++ src/javascript/JContent/ContentRoute/ToolBar/BrowseControlBar/BrowseControlBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import {DisplayActions} from '@jahia/ui-extender';
+import React, {useMemo} from 'react';
+import {DisplayAction, DisplayActions, registry} from '@jahia/ui-extender';
+import {Separator} from '@jahia/moonstone';
 import FileModeSelector from '../FileModeSelector';
 import JContentConstants from '../../../JContent.constants';
 import {getButtonRenderer} from '~/utils/getButtonRenderer';
@@ -7,6 +8,14 @@ import {useSelector} from 'react-redux';
 import PropTypes from 'prop-types';
 
 const ButtonRenderer = getButtonRenderer({size: 'default', variant: 'ghost'});
+const ButtonRendererNoLabel = getButtonRenderer({
+    labelStyle: 'none',
+    size: 'default',
+    variant: 'ghost',
+    className: 'toto'
+});
+
+const excludedActions = ['pageComposer', 'subContents', 'edit', 'publishMenu', 'cut', 'lock', 'export', 'deletePermanently', 'publishDeletion', 'copy', 'paste', 'createFolder', 'createContentFolder', 'createContent', 'fileUpload'];
 
 export const BrowseControlBar = ({showActions}) => {
     const {path, mode, siteKey} = useSelector(state => ({
@@ -15,14 +24,35 @@ export const BrowseControlBar = ({showActions}) => {
         siteKey: state.site
     }));
 
+    const contentActions = useMemo(() => [
+        ...registry.find({type: 'action', target: 'headerPrimaryActions'}).map(action => action.key),
+        ...excludedActions
+    ], []);
     const isRootNode = (path === ('/sites/' + siteKey));
 
     return (
         <React.Fragment>
             {showActions && !isRootNode && (
-                <div className="flexRow">
-                    <DisplayActions target="headerPrimaryActions" context={{path: path}} render={ButtonRenderer} loading={() => false}/>
-                </div>
+                <>
+                    <div className="flexRow">
+                        <DisplayActions target="headerPrimaryActions"
+                                        context={{path: path}}
+                                        render={ButtonRenderer}
+                                        loading={() => false}/>
+                    </div>
+                    <div className="flexRow">
+                        <Separator variant="vertical" invisible="onlyChild"/>
+                        <DisplayAction actionKey="contentMenu"
+                                       context={{
+                                           path: path,
+                                           menuPreload: true,
+                                           menuFilter: action => contentActions.indexOf(action.key) === -1
+                                       }}
+                                       render={ButtonRendererNoLabel}
+                                       loading={() => false}
+                        />
+                    </div>
+                </>
             )}
             <div className="flexFluid"/>
             {showActions && mode === JContentConstants.mode.MEDIA &&

4a30c6e995d94078fb0b947cb17cfad1f751d9d3
alex <alexandrk_@hotmail.com>
2020-03-27 13:29:09 -0400

    BACKLOG-12571 Initial refactor of search results

--- src/javascript/JContent/ContentRoute/ContentRoute.jsx
+++ src/javascript/JContent/ContentRoute/ContentRoute.jsx
@@ -7,21 +7,27 @@ import ContentHeader from './ContentHeader';
 import ToolBar from './ToolBar';
 import ContentBreadcrumb from './ContentBreadcrumb';
 import ContentTitle from './ContentTitle';
+import ContentSearchTitle from './ContentSearchTitle';
 import ContentStatuses from './ContentStatuses';
 import {MainActionBar} from './MainActionBar';
+import JContentConstants from '../JContent.constants';
 
 const ContentRoute = () => {
-    const mode = useSelector(state => state.jcontent.mode);
+    const {mode} = useSelector(state => ({
+        mode: state.jcontent.mode
+    }));
+
+    const inSearchMode = JContentConstants.mode.SEARCH === mode || JContentConstants.mode.SQL2SEARCH === mode;
 
     return (
         <MainLayout
             header={
                 <ContentHeader
-                    title={<ContentTitle/>}
-                    mainAction={<MainActionBar/>}
-                    breadcrumb={<ContentBreadcrumb/>}
+                    title={inSearchMode ? <ContentSearchTitle/> : <ContentTitle/>}
+                    mainAction={!inSearchMode && <MainActionBar/>}
+                    breadcrumb={!inSearchMode && <ContentBreadcrumb/>}
+                    information={!inSearchMode && <ContentStatuses/>}
                     toolbar={<ToolBar/>}
-                    information={<ContentStatuses/>}
                 />
             }
         >

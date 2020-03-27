fac82219027849ac34cdc35e1be31e344b6ac580
alex <alexandrk_@hotmail.com>
2020-03-27 14:12:33 -0400

    BACKLOG-12571 Restore test

--- src/javascript/JContent/ContentRoute/ContentSearchTitle/ContentSearchTitle.test.jsx
+++ src/javascript/JContent/ContentRoute/ContentSearchTitle/ContentSearchTitle.test.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import {shallow} from '@jahia/test-framework';
 import ContentSearchTitle from './ContentSearchTitle';
+import {useDispatch} from 'react-redux';
 
-// This thrown an error of unknown nature
 jest.mock('react-redux', () => ({
     useDispatch: jest.fn()
 }));
 
+jest.mock('connected-react-router', () => jest.fn(() => {}));
+
 describe('Content search title', () => {
     it('Should render', async () => {
+        const dispatch = jest.fn();
+        useDispatch.mockImplementation(() => dispatch);
+
         const contentTitle = shallow(<ContentSearchTitle onClickBack={() => {}}/>);
         expect(contentTitle.contains('translated_jcontent:label.contentManager.title.search')).toBeTruthy();
     });

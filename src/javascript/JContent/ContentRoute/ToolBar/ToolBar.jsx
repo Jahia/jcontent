fef6e5dac3cc4a6982dbb1bc6da22cd0ca236545
Yousria <yousria.benchadi@gmail.com>
2020-03-27 13:57:18 +0100

    BACKLOG-12964 : make the separator invisible when there is no actions in the middle of the bar

--- src/javascript/JContent/ContentRoute/ToolBar/ToolBar.jsx
+++ src/javascript/JContent/ContentRoute/ToolBar/ToolBar.jsx
@@ -36,14 +36,16 @@ export const ToolBar = () => {
                 </Typography>
                 <div className={styles.spacer}/>
                 <Button icon={<Cancel/>} variant="ghost" size="small" onClick={() => dispatch(cmClearSelection())}/>
-                <Separator variant="vertical"/>
-                <DisplayActions
-                    target="selectedContentActions"
-                    context={{paths: selection}}
-                    filter={action => action.key !== 'deletePermanently' && action.key.indexOf('publish') === -1}
-                    render={getButtonRenderer({size: 'small', variant: 'ghost'})}
-                />
-                <Separator variant="vertical"/>
+                <div>
+                    <Separator variant="vertical" invisible="onlyChild"/>
+                    <DisplayActions
+                        target="selectedContentActions"
+                        context={{paths: selection}}
+                        filter={action => action.key !== 'deletePermanently' && action.key.indexOf('publish') === -1}
+                        render={getButtonRenderer({size: 'small', variant: 'ghost'})}
+                    />
+                </div>
+                <Separator variant="vertical" invisible="onlyChild"/>
                 <ButtonGroup size="small" variant="outlined" color="accent">
                     <DisplayAction actionKey="publish" context={{paths: selection}} render={ButtonRendererMultiple}/>
                     <DisplayAction actionKey="publishDeletion" context={{paths: selection}} render={ButtonRendererShortLabel}/>

646346fb9bfe066974d7dd38e6382fcc102ce316
alex <alexandrk_@hotmail.com>
2020-03-27 14:05:32 -0400

    BACKLOG-12571 Some improvements

--- src/javascript/JContent/ContentRoute/ToolBar/SearchControlBar/SearchControlBar.jsx
+++ src/javascript/JContent/ContentRoute/ToolBar/SearchControlBar/SearchControlBar.jsx
@@ -16,12 +16,17 @@ export const SearchControlBar = () => {
         mode: state.jcontent.mode
     }));
 
+    const advancedSearchMode = mode === JContentConstants.mode.SQL2SEARCH;
+
     return (
         <React.Fragment>
             <DisplayAction actionKey="search" context={{path, buttonLabel: 'Edit query', buttonIcon: <Edit/>}} render={ButtonRenderer} variant="ghost" data-sel-role="open-search-dialog"/>
             <Separator variant="vertical" invisible="firstOrLastChild"/>
-            {mode === JContentConstants.mode.SQL2SEARCH && <Chip className={styles.chipMargin} label="Advanced Search = On"/>}
-            <Chip className={styles.chipMargin} label="Type = Designer"/>
+            {advancedSearchMode &&
+            <>
+                <Chip className={styles.chipMargin} label="Advanced Search = On"/>
+                <Chip className={styles.chipMargin} label="Type = Designer"/>
+            </>}
             <Chip className={styles.chipMargin} label={`Location = ${site}`}/>
             <div className={`${styles.grow}`}/>
         </React.Fragment>

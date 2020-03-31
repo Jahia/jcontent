59950d1df8ac661fb58f98725886fe4298a44a70
Yousria <yousria.benchadi@gmail.com>
2020-03-31 11:15:26 +0200

    BACKLOG-12336 : raise parent version and check the new permission of clearAllLocks

--- src/javascript/ContentManager/actions/clearAllLocksAction.jsx
+++ src/javascript/ContentManager/actions/clearAllLocksAction.jsx
@@ -9,9 +9,8 @@ export default composeActions(requirementsAction, withDxContextAction, {
     init: context => {
         context.initRequirements({
             getLockInfo: true,
-            requiredPermission: 'jcr:lockManagement',
-            enabled: context => context.node.pipe(map(node => node.lockTypes !== null && !_.includes(node.lockTypes.values, ' deletion :deletion') &&
-                context.dxContext.userName === 'root'))
+            requiredPermission: 'clearLock',
+            enabled: context => context.node.pipe(map(node => node.lockTypes !== null && !_.includes(node.lockTypes.values, ' deletion :deletion')))
         });
     },
     onClick: context => {

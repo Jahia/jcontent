0f96b0ecfebd1bd24b04881e9d5907c20519d388
Thomas Draier <tdraier@jahia.com>
2020-04-06 11:28:41 +0200

    BACKLOG-12394: Always display a preview when available

--- src/javascript/JContent/ContentRoute/ContentLayout/PreviewDrawer/Preview/MultipleSelection/MultipleSelection.jsx
+++ src/javascript/JContent/ContentRoute/ContentLayout/PreviewDrawer/Preview/MultipleSelection/MultipleSelection.jsx
@@ -4,26 +4,32 @@ import classNames from 'classnames';
 import {Paper} from '@material-ui/core';
 import {Button, Typography} from '@jahia/design-system-kit';
 import {ContentCopy} from 'mdi-material-ui';
+import {useDispatch} from 'react-redux';
+import {cmClearSelection} from '../../../contentSelection.redux';
+import {useTranslation} from 'react-i18next';
 
-const MultipleSelection = ({classes, t, selection, clearSelection}) => (
-    <div className={classNames(classes.noPreviewContainer, classes.contentContainer)}>
-        <Paper elevation={1} className={classes.contentContainer} classes={{root: classes.center}}>
-            <Typography variant="gamma">
-                {t('jcontent:label.contentManager.selection.itemsSelected', {count: selection.length})}
-            </Typography>
-            <ContentCopy className={classes.centerIcon} color="inherit"/>
-            <Button onClick={clearSelection}>
-                {t('jcontent:label.contentManager.selection.clearMultipleSelection')}
-            </Button>
-        </Paper>
-    </div>
-);
+const MultipleSelection = ({classes, selection}) => {
+    const {t} = useTranslation();
+    const dispatch = useDispatch();
+
+    return (
+        <div className={classNames(classes.noPreviewContainer, classes.contentContainer)}>
+            <Paper elevation={1} className={classes.contentContainer} classes={{root: classes.center}}>
+                <Typography variant="gamma">
+                    {t('jcontent:label.contentManager.selection.itemsSelected', {count: selection.length})}
+                </Typography>
+                <ContentCopy className={classes.centerIcon} color="inherit"/>
+                <Button onClick={() => dispatch(cmClearSelection())}>
+                    {t('jcontent:label.contentManager.selection.clearMultipleSelection')}
+                </Button>
+            </Paper>
+        </div>
+    );
+};
 
 MultipleSelection.propTypes = {
     classes: PropTypes.object.isRequired,
-    clearSelection: PropTypes.func.isRequired,
-    selection: PropTypes.array.isRequired,
-    t: PropTypes.func.isRequired
+    selection: PropTypes.array.isRequired
 };
 
 export default MultipleSelection;

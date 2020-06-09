cc0adbc16fa6356b882d5837799927dd1de4ffd2
tdraier <tdraier@jahia.com>
2020-06-09 09:47:28 +0200

    Use common test config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
 const jestConfig = require('@jahia/test-framework').jestConfig;
 
-jestConfig.moduleNameMapper['@jahia/moonstone'] = '@jahia/moonstone/dist/lib/main';
-
 module.exports = jestConfig;

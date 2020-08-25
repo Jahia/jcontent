2df6124fe5bdf09fc841b958de2027eed56cedc7
Alex Karmanov <akarmanov@jahia.com>
2020-08-25 03:22:47 -0400

    QA-13098 Fix multiple selection issue by adding a check for undefined (#331)

--- src/javascript/JContent/actions/publishAction.jsx
+++ src/javascript/JContent/actions/publishAction.jsx
@@ -62,7 +62,7 @@ function getButtonLabelParams(context, language, res, t) {
     }
 
     return {
-        displayName: t('jcontent:label.contentManager.selection.items', {count: context.pages.length}),
+        displayName: t('jcontent:label.contentManager.selection.items', {count: context.paths.length}),
         language: res.nodes[0].site ? _.escape(uppercaseFirst(getLanguageLabel(res.nodes[0].site.languages, language).displayName)) : null
     };
 }

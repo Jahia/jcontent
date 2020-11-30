98972ac6011b81467e49c7c8657f192d146d2c35
Nico <71046052+nbugash-jahia@users.noreply.github.com>
2020-11-30 12:49:36 -0500

    BACKLOG-13977: Change lib from pretty-bytes to bytes (#356)
    Change node lib from `pretty-bytes` to `bytes`. Shows more accurate numbers.

--- src/javascript/JContent/ContentRoute/ContentLayout/FilesGrid/FileCard/FileSize/FileSize.jsx
+++ src/javascript/JContent/ContentRoute/ContentLayout/FilesGrid/FileCard/FileSize/FileSize.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import prettyBytes from 'pretty-bytes';
+import bytes from 'bytes';
 import PropTypes from 'prop-types';
 
 const FileSize = ({node}) => (
     <>
-        {node.children && node.children.nodes.length > 0 && node.children.nodes[0].data && prettyBytes(node.children.nodes[0].data.size)}
+        {node.children && node.children.nodes.length > 0 && node.children.nodes[0].data &&
+        bytes(node.children.nodes[0].data.size, {unitSeparator: ' '})}
     </>
 );
 FileSize.propTypes = {

3205bacdb5774252b65c09c9b52307655f736710
XiangYuHe <xiangyu.he1@gmail.com>
2021-02-12 17:43:13 -0500

    QA-13488 allowing the visibility of the cut to not be restricted like COPY. (#381)

--- src/javascript/JContent/actions/actions.constants.js
+++ src/javascript/JContent/actions/actions.constants.js
@@ -1,4 +1,2 @@
 export const PATH_CONTENTS_ITSELF = '^/sites/((?!/).)+/contents/?$';
-export const PATH_CONTENTS_DESCENDANTS = '^/sites/((?!/).)+/contents/.+';
 export const PATH_FILES_ITSELF = '^/sites/((?!/).)+/files/?$';
-export const PATH_FILES_DESCENDANTS = '^/sites/((?!/).)+/files/.+';

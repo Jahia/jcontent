bb9113a6c6c6fb228b16e569595cc257caab93bb
alex <alexandrk_@hotmail.com>
2021-03-23 08:18:46 -0400

    QA-13592 Show pdf icon only

--- src/javascript/JContent/ContentRoute/ContentLayout/FilesGrid/FileCard/FileCard.jsx
+++ src/javascript/JContent/ContentRoute/ContentLayout/FilesGrid/FileCard/FileCard.jsx
@@ -117,7 +117,7 @@ export const FileCard = ({
     let rowNumber = Math.floor(index / 2) + 1;
     let columnNumber = (index % 2) + 1;
     let encodedPath = node.path.replace(/[^/]/g, encodeURIComponent);
-    let isPdf = node.children ? node.children.nodes.filter(node => node.mimeType.value === 'application/pdf').length !== 0 : false;
+    // Let isPdf = node.children ? node.children.nodes.filter(node => node.mimeType.value === 'application/pdf').length !== 0 : false;
     let showSubNodes = node.primaryNodeType.name !== 'jnt:page' && node?.subNodes?.pageInfo?.totalCount > 0;
 
     return (
@@ -152,10 +152,7 @@ export const FileCard = ({
                     /> :
                 <div className={classes.detailedIcon}>
                     {node.primaryNodeType.name === 'jnt:folder' ?
-                        <Folder color="action"/> :
-                            isPdf ?
-                                <embed src={`${window.contextJsParameters.contextPath}/files/default/${encodedPath}?t=thumbnail`} className={classes.thumbCoverDetailed}/> :
-                                <FileIcon filename={node.path} color="disabled"/>}
+                        <Folder color="action"/> : <FileIcon filename={node.path} color="disabled"/>}
                 </div>}
 
             <div className={isImage ? classes.mediaCardContentContainer : classes.fileCardContentContainer}>

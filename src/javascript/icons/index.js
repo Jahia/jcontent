ba12770a13fafe5c27849b3bccb1567f5e9e4646
Thomas Draier <tdraier@jahia.com>
2021-10-25 13:47:48 +0200

    Revert "generate non-material icons"
    This reverts commit 37feaea2e4a98ba384b8bfea5dd454b0353a5909.

--- src/javascript/icons/index.js
+++
@@ -1,4 +0,0 @@
-export {default as DocumentIcon} from './DocumentIcon';
-export {default as FileIcon} from './FileIcon';
-export {default as ImageIcon} from './ImageIcon';
-export {default as ZipIcon} from './ZipIcon';

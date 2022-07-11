3dc48947cb89265af3f0b1efbb53b407abf006c8
Alex Karmanov <alexandrk_@hotmail.com>
2022-07-11 14:21:42 -0400

    BACKLOG-17225 Export components

---
+++ src/javascript/JContent/ContentRoute/ContentLayout/ContentTable/reactTable/index.js
@@ -0,0 +1,4 @@
+export * from './columns';
+export * from './components/cells';
+export * from './components/headers';
+export * from './plugins';
--- src/javascript/shared/index.js
+++ src/javascript/shared/index.js
@@ -1,3 +1,7 @@
 export {default as ContentTree} from '../JContent/ContentTree';
 export {default as ContentNavigation} from '../JContent/ContentNavigation';
 export {default as SiteSwitcher} from '../JContent/ContentNavigation/NavigationHeader/SwitchersLayout/SiteSwitcher';
+export {default as ContentLayout} from '../JContent/ContentRoute/ContentLayout';
+export {default as ContentTable} from '../JContent/ContentRoute/ContentLayout/ContentTable';
+export {default as ViewModeSelector} from '../JContent/ContentRoute/ToolBar/ViewModeSelector';
+export * as reactTable from '../JContent/ContentRoute/ContentLayout/ContentTable/reactTable';

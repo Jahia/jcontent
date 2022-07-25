884af9d8c7ede364d0f5bd55366cec8100b0c957
Alex Karmanov <alexandrk_@hotmail.com>
2022-07-25 14:51:47 -0400

    BACKLOG-17395 Expose some components

--- src/javascript/shared/index.js
+++ src/javascript/shared/index.js
@@ -4,4 +4,10 @@ export {default as SiteSwitcher} from '../JContent/ContentNavigation/NavigationH
 export {default as ContentTypeSelector} from '../JContent/ContentRoute/ContentLayout/ContentTable/ContentTypeSelector';
 export {default as ViewModeSelector} from '../JContent/ContentRoute/ToolBar/ViewModeSelector';
 export {default as FileModeSelector} from '../JContent/ContentRoute/ToolBar/FileModeSelector';
+export {default as UploadTransformComponent} from '../JContent/ContentRoute/ContentLayout/UploadTransformComponent';
+export {default as ContentEmptyDropZone} from '../JContent/ContentRoute/ContentLayout/ContentTable/ContentEmptyDropZone';
+export {default as ContentNotFound} from '../JContent/ContentRoute/ContentLayout/ContentTable/ContentNotFound';
+export {default as EmptyTable} from '../JContent/ContentRoute/ContentLayout/ContentTable/EmptyTable';
+export {default as ContentListHeader} from '../JContent/ContentRoute/ContentLayout/ContentTable/ContentListHeader';
+export {default as ContentTableWrapper} from '../JContent/ContentRoute/ContentLayout/ContentTable/ContentTableWrapper';
 export * as reactTable from '../JContent/ContentRoute/ContentLayout/ContentTable/reactTable';

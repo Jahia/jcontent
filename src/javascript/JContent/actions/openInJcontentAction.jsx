09072a4ad26e785210ff48a4a1a81ff5bd2098b4
Thomas Draier <tdraier@jahia.com>
2022-08-12 11:25:02 +0200

    BACKLOG-19911: Add open in jcontent action

---
+++ src/javascript/JContent/actions/openInJcontentAction.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {useNodeChecks} from '@jahia/data-helper';
+import {useSelector} from 'react-redux';
+import PropTypes from 'prop-types';
+import {registry} from '@jahia/ui-extender';
+
+export const OpenInJContentActionComponent = ({path, render: Render, loading: Loading, ...others}) => {
+    const {language} = useSelector(state => ({language: state.language}));
+    const res = useNodeChecks(
+        {path},
+        {
+            requiredPermission: ['jContentAccess']
+        }
+    );
+
+    if (res.loading && Loading) {
+        return <Loading {...others}/>;
+    }
+
+    if (!res.node) {
+        return false;
+    }
+
+    const matchArray = path.match(/\/sites\/([^/]+)\/(.*)/);
+    if (!matchArray) {
+        return false;
+    }
+
+    return (
+        <Render {...others}
+                isVisible={res.checksResult}
+                enabled={res.checksResult}
+                onClick={() => {
+                    const acc = registry.find({type: 'accordionItem', target: 'jcontent'}).find(acc => acc.canDisplayItem(res.node));
+                    const mode = acc.key;
+                    window.open(`${window.contextJsParameters.contextPath}/jahia/jcontent/${matchArray[1]}/${language}/${mode}/${matchArray[2]}`, '_blank');
+                }}
+        />
+    );
+};
+
+OpenInJContentActionComponent.propTypes = {
+    path: PropTypes.string.isRequired,
+    render: PropTypes.func.isRequired,
+    loading: PropTypes.func
+};

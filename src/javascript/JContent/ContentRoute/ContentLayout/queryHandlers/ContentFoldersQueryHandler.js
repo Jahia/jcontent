f85baffaea088bfa2573ffd73b82751c262361cc
Thomas Draier <tdraier@jahia.com>
2022-09-05 11:04:32 +0200

    Use lazy load in jcontent/content-folder/structured

--- src/javascript/JContent/ContentRoute/ContentLayout/queryHandlers/ContentFoldersQueryHandler.js
+++ src/javascript/JContent/ContentRoute/ContentLayout/queryHandlers/ContentFoldersQueryHandler.js
@@ -1,11 +1,22 @@
 import {BaseQueryHandler} from './BaseQueryHandler';
 import JContentConstants from '~/JContent/JContent.constants';
 import {BaseDescendantsQuery} from './BaseQueryHandler.gql-queries';
+import {BaseTreeQueryHandler} from '~/JContent/ContentRoute/ContentLayout/queryHandlers/BaseTreeQueryHandler';
 
 export const ContentFoldersQueryHandler = {
     ...BaseQueryHandler,
+    ...BaseTreeQueryHandler,
 
     getQuery: () => BaseDescendantsQuery,
+
+    getTreeParams: selection => {
+        const {openPaths, tableView} = selection;
+        if (openPaths && tableView.viewMode === JContentConstants.tableView.viewMode.STRUCTURED) {
+            return BaseTreeQueryHandler.getTreeParams(selection);
+        }
+
+        return null;
+    },
 
     getQueryVariables: selection => {
         const queryVariables = BaseQueryHandler.getQueryVariables(selection);

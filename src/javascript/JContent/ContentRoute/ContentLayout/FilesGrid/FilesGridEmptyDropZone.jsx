bec23425dbad67ebc1fa9e99c44db423b149c4c0
Thomas Draier <tdraier@jahia.com>
2022-09-14 21:01:35 +0200

    BACKLOG-17504: Adapt UploadTransformComponent and empty tables (#594)

--- src/javascript/JContent/ContentRoute/ContentLayout/FilesGrid/FilesGridEmptyDropZone.jsx
+++ src/javascript/JContent/ContentRoute/ContentLayout/FilesGrid/FilesGridEmptyDropZone.jsx
@@ -5,18 +5,18 @@ import EmptyDropZone from '../EmptyDropZone';
 import UploadTransformComponent from '../UploadTransformComponent';
 import styles from './FilesGridEmptyDropZone.scss';
 
-export const FilesGridEmptyDropZone = ({path, mode}) => (
-    <UploadTransformComponent uploadTargetComponent={Grid} uploadPath={path} mode={mode} className={styles.root}>
+export const FilesGridEmptyDropZone = ({path, uploadType}) => (
+    <UploadTransformComponent uploadTargetComponent={Grid} uploadPath={path} uploadType={uploadType} className={styles.root}>
         <Grid container className={styles.gridEmpty} data-cm-role="grid-content-list">
             <div className={styles.dragZoneRoot}>
-                <EmptyDropZone component="div" mode={mode}/>
+                <EmptyDropZone component="div" uploadType={uploadType}/>
             </div>
         </Grid>
     </UploadTransformComponent>
 );
 
 FilesGridEmptyDropZone.propTypes = {
-    mode: PropTypes.string.isRequired,
+    uploadType: PropTypes.string.isRequired,
     path: PropTypes.string.isRequired
 };
 

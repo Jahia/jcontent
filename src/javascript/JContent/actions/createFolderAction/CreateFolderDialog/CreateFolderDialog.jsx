986f30ed6935e968a8f6c967c3b230ab6c7c000e
Thomas Draier <tdraier@jahia.com>
2022-09-22 18:06:33 +0200

    BACKLOG-20016: limit lenght

--- src/javascript/JContent/actions/createFolderAction/CreateFolderDialog/CreateFolderDialog.jsx
+++ src/javascript/JContent/actions/createFolderAction/CreateFolderDialog/CreateFolderDialog.jsx
@@ -79,6 +79,7 @@ export const CreateFolderDialog = ({path, contentType, onExit}) => {
                 <TextField
                     fullWidth
                     autoFocus
+                    inputProps={{maxLength: 32}}
                     error={Boolean(errMsg)}
                     value={name}
                     id="folder-name"

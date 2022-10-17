2d8bdd4f861b20517760e3d9c211735fa5b79dd6
Thomas Draier <tdraier@jahia.com>
2022-10-17 10:26:33 +0200

    BACKLOG-20104: Download modal (#631)

---
+++ src/javascript/JContent/actions/downloadFileAction/DownloadFileDialog/DownloadFileDialog.gql-queries.js
@@ -0,0 +1,43 @@
+import gql from 'graphql-tag';
+import {PredefinedFragments} from '@jahia/data-helper';
+
+export const FileInfoQuery = gql`
+    query FileInfoQuery($path:String!, $language:String!) {
+        jcr {
+            nodeByPath(path: $path) {
+                name
+                path
+                primaryNodeType {
+                    name
+                }
+                aggregatedPublicationInfo(language: $language) {
+                    existsInLive
+                }
+                isFile: isNodeType(type: {types: ["jnt:file"]})
+                isImage: isNodeType(type: {types: ["jmix:image"]})
+                width: property(name: "j:width") {
+                    value
+                }
+                height: property(name: "j:height") {
+                    value
+                }
+                displayName(language: $language)
+                content: descendant(relPath: "jcr:content") {
+                    ...NodeCacheRequiredFields
+                    data: property(name: "jcr:data") {
+                        size
+                    }
+                    mimeType: property(name: "jcr:mimeType") {
+                        value
+                    }
+                }
+                parent {
+                    name
+                    ...NodeCacheRequiredFields
+                }
+                ...NodeCacheRequiredFields
+            }
+        }
+    }
+    ${PredefinedFragments.nodeCacheRequiredFields.gql}
+`;
---
+++ src/javascript/JContent/actions/downloadFileAction/DownloadFileDialog/DownloadFileDialog.jsx
@@ -0,0 +1,117 @@
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import PropTypes from 'prop-types';
+import {Dialog, DialogActions, DialogContent, DialogTitle} from '@material-ui/core';
+import styles from './DownloadFileDialog.scss';
+import {Button, Chip, Copy, Dropdown, Folder, Typography} from '@jahia/moonstone';
+import {useTranslation} from 'react-i18next';
+import {useQuery} from '@apollo/react-hooks';
+import {FileInfoQuery} from '~/JContent/actions/downloadFileAction/DownloadFileDialog/DownloadFileDialog.gql-queries';
+import clsx from 'clsx';
+import {NodeIcon} from '~/utils';
+import {FileSize} from '~/shared';
+
+export const DownloadFileDialog = ({path, onExit}) => {
+    const [open, setOpen] = useState(true);
+    const {t} = useTranslation('jcontent');
+    const aRef = useRef();
+    const [mode, setMode] = useState();
+
+    const {data} = useQuery(FileInfoQuery, {
+        variables: {path, language: 'en'}
+    });
+
+    const handleClose = () => setOpen(false);
+
+    const href = mode && new URL(window.contextJsParameters.contextPath + '/files/' + mode.value + path, window.location.href).toString();
+    const node = data?.jcr?.nodeByPath;
+
+    const dropdownData = useMemo(() => [
+        {label: t('jcontent:label.contentManager.downloadFile.default'), value: 'default'},
+        {label: t('jcontent:label.contentManager.downloadFile.live'), value: 'live', isDisabled: !(node?.aggregatedPublicationInfo?.existsInLive)}
+    ], [t, node]);
+
+    useEffect(() => {
+        if (node && !node.aggregatedPublicationInfo.existsInLive) {
+            setMode(dropdownData[0]);
+        } else {
+            setMode(dropdownData[1]);
+        }
+    }, [node, dropdownData]);
+
+    const sizeInfo = (node && node.height && node.width) ? `${parseInt(node.height.value, 10)} x ${parseInt(node.width.value, 10)} - ` : '';
+
+    return (
+        <Dialog open={open}
+                aria-labelledby="form-dialog-title"
+                classes={{paper: styles.root}}
+                onClose={handleClose}
+                onExited={onExit}
+        >
+            <DialogTitle id="form-dialog-title">{t('jcontent:label.contentManager.downloadFile.title')}</DialogTitle>
+            <DialogContent>
+                <Typography weight="bold">
+                    {t('jcontent:label.contentManager.downloadFile.selectVersion')}
+                </Typography>
+                {node && (
+                    <>
+                        <Dropdown
+                            maxWidth="100%"
+                            className={styles.dropdown}
+                            variant="outlined"
+                            data={dropdownData}
+                            label={mode.label}
+                            value={mode.value}
+                            onChange={(e, item) => setMode(item)}
+                        />
+                        <a ref={aRef} title="download" href={href} download={path.split('/').pop()}> </a>
+
+                        <div className={clsx(styles.card, 'flexRow_nowrap', 'alignCenter')}>
+                            <div className={styles.cardThumbnail}>
+                                {node.isImage ? <img src={href}/> : <NodeIcon node={node}/>}
+                            </div>
+                            <div className={clsx(styles.gap, 'flexFluid', 'flexCol')}>
+                                <Typography isNowrap>{node?.displayName}</Typography>
+                                <div className={clsx('flexRow', styles.gap)}>
+                                    <Chip icon={<Folder/>} label={node?.parent?.name}/>
+                                    <Typography variant="caption" className={styles.grey}>
+                                        {sizeInfo} <FileSize node={node}/>
+                                    </Typography>
+                                </div>
+                            </div>
+                            <Button
+                                className={styles.grey}
+                                icon={<Copy/>}
+                                size="default"
+                                label={t('jcontent:label.contentManager.downloadFile.copyUrl')}
+                                onClick={() => {
+                                    navigator.clipboard.writeText(href);
+                                }}
+                            />
+                        </div>
+                    </>
+                )}
+            </DialogContent>
+            <DialogActions>
+                <Button
+                    size="big"
+                    label={t('jcontent:label.cancel')}
+                    onClick={handleClose}
+                />
+                <Button
+                    color="accent"
+                    size="big"
+                    label={t('jcontent:label.contentManager.downloadFile.download')}
+                    onClick={() => {
+                        aRef.current.click();
+                    }}
+                />
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+DownloadFileDialog.propTypes = {
+    path: PropTypes.string.isRequired,
+    onExit: PropTypes.func.isRequired
+};
+

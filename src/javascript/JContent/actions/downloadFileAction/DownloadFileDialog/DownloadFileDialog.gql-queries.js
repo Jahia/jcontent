b98749d5bbda9cb49aada449a06a2083908e7e41
Thomas Draier <tdraier@jahia.com>
2022-10-18 11:50:46 +0200

    BACKLOG-20177: Use live information for download modal when selecting live mode

--- src/javascript/JContent/actions/downloadFileAction/DownloadFileDialog/DownloadFileDialog.gql-queries.js
+++ src/javascript/JContent/actions/downloadFileAction/DownloadFileDialog/DownloadFileDialog.gql-queries.js
@@ -41,3 +41,41 @@ export const FileInfoQuery = gql`
     }
     ${PredefinedFragments.nodeCacheRequiredFields.gql}
 `;
+
+export const FileInfoQueryLive = gql`
+    query FileInfoQueryLive($uuid:String!, $language:String!) {
+        jcr(workspace:LIVE) {
+            nodeById(uuid: $uuid) {
+                name
+                path
+                primaryNodeType {
+                    name
+                }
+                isFile: isNodeType(type: {types: ["jnt:file"]})
+                isImage: isNodeType(type: {types: ["jmix:image"]})
+                width: property(name: "j:width") {
+                    value
+                }
+                height: property(name: "j:height") {
+                    value
+                }
+                displayName(language: $language)
+                content: descendant(relPath: "jcr:content") {
+                    ...NodeCacheRequiredFields
+                    data: property(name: "jcr:data") {
+                        size
+                    }
+                    mimeType: property(name: "jcr:mimeType") {
+                        value
+                    }
+                }
+                parent {
+                    name
+                        ...NodeCacheRequiredFields
+                    }
+                    ...NodeCacheRequiredFields
+                }
+            }
+        }
+    ${PredefinedFragments.nodeCacheRequiredFields.gql}
+`;

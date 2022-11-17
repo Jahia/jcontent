21b12619dd5144f8e3beb17509abd83970202e9e
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2022-11-17 07:48:27 -0500

    BACKLOG-16839: Display language switcher only for multi-language sites (#645)

--- src/javascript/JContent/ContentNavigation/NavigationHeader/SwitchersLayout/SwitchersLayout.jsx
+++ src/javascript/JContent/ContentNavigation/NavigationHeader/SwitchersLayout/SwitchersLayout.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import SiteSwitcher from './SiteSwitcher';
 import LanguageSwitcher from './LanguageSwitcher';
 import styles from './SwitchersLayout.scss';
-import {Separator} from '@jahia/moonstone';
 
 const SwitchersLayout = () => {
     return (
         <div className={styles.root}>
             <SiteSwitcher/>
-            <Separator variant="vertical"/>
             <LanguageSwitcher/>
         </div>
     );

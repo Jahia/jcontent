7740074c1300436de834f57bfba83049ed8f9699
Cedric Mailleux <cmailleux@jahia.com>
2023-02-03 12:56:35 -0500

    BACKLOG-20119: Fix create button rendering, hide paste or new content depending on clipboard status. Add action to clear clipboard in main header

---
+++ src/javascript/JContent/actions/copyPaste/ClearClipboardActionComponent.jsx
@@ -0,0 +1,36 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {useApolloClient} from '@apollo/react-hooks';
+import copyPasteConstants from './copyPaste.constants';
+import {setLocalStorage} from './localStorageHandler';
+import {copypasteClear} from './copyPaste.redux';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {withNotifications} from '@jahia/react-material';
+import {useTranslation} from 'react-i18next';
+
+export const ClearClipboardActionComponent = withNotifications()(({render: Render, notificationContext, ...others}) => {
+    const client = useApolloClient();
+    const dispatch = useDispatch();
+    const {t} = useTranslation('jcontent');
+    const copyPaste = useSelector(state => state.jcontent.copyPaste);
+    const {nodes} = copyPaste;
+
+    let isVisible = nodes.length > 0;
+
+    return (
+        <Render
+            {...others}
+            isVisible={isVisible}
+            enabled={isVisible}
+            onClick={() => {
+                dispatch(copypasteClear());
+                setLocalStorage(copyPasteConstants.COPY, [], client);
+                notificationContext.notify(t('jcontent:label.contentManager.copyPaste.clear'), ['closeButton']);
+            }}
+        />
+    );
+});
+
+ClearClipboardActionComponent.propTypes = {
+    render: PropTypes.func.isRequired
+};

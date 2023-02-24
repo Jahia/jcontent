62893c90a4f2c5c3e145db75c3b5e986424eb058
Alex Karmanov <alexandrk_@hotmail.com>
2023-02-24 12:03:50 -0500

    BACKLOG-20592 Add size tracker, add action menu for narrow header

--- src/javascript/JContent/MainLayout/MainLayout.jsx
+++ src/javascript/JContent/MainLayout/MainLayout.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import styles from './MainLayout.scss';
+import {useResize, ResizeContext} from './ResizeObserver';
 
-export const MainLayout = ({header, children}) => (
-    <main className={clsx(styles.root, 'flexCol')}>
-        { header &&
-        header}
-        <div className={clsx(styles.content, 'flexCol_nowrap')}>
-            {children}
-        </div>
-    </main>
-);
+export const MainLayout = ({header, children}) => {
+    const {ref, width} = useResize();
+
+    return (
+        <main ref={ref} className={clsx(styles.root, 'flexCol')}>
+            <ResizeContext.Provider value={width}>
+                { header &&
+                    header}
+                <div className={clsx(styles.content, 'flexCol_nowrap')}>
+                    {children}
+                </div>
+            </ResizeContext.Provider>
+        </main>
+    );
+};
 
 MainLayout.propTypes = {
     header: PropTypes.node,

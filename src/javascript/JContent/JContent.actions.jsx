2b5faaf476b4bebb40989375bf962f3f676e3c94
Alex Karmanov <alexandrk_@hotmail.com>
2023-02-24 12:03:50 -0500

    BACKLOG-20592 Add size tracker, add action menu for narrow header

--- src/javascript/JContent/JContent.actions.jsx
+++ src/javascript/JContent/JContent.actions.jsx
@@ -52,6 +52,7 @@ import {CreateFolderActionComponent} from './actions/createFolderAction';
 import {ZipActionComponent} from './actions/zipUnzip/zipAction';
 import {UnzipActionComponent} from './actions/zipUnzip/unzipAction';
 import {SearchActionComponent} from './actions/searchAction';
+import {MenuLabelActionComponent} from './actions/menuLabelAction';
 import {MenuItemRenderer} from './MenuItemRenderer';
 import {MenuRenderer} from './MenuRenderer';
 import {triggerRefetchAll} from './JContent.refetches';
@@ -78,27 +79,27 @@ export const jContentActions = registry => {
     registry.add('action', 'createContentFolder', {
         buttonIcon: <AddFolder/>,
         buttonLabel: 'jcontent:label.contentManager.create.contentFolder',
-        targets: ['createMenuActions:3', 'contentActions:2', 'headerPrimaryActions:2'],
+        targets: ['createMenuActions:3', 'contentActions:2', 'headerPrimaryActions:2', 'narrowHeaderMenu:2'],
         createFolderType: 'contentFolder',
         component: CreateFolderActionComponent
     });
     registry.add('action', 'rename', {
         buttonIcon: <Edit/>,
         buttonLabel: 'jcontent:label.contentManager.rename',
-        targets: ['contentActions:2'],
+        targets: ['contentActions:2', 'narrowHeaderMenu:12'],
         component: RenameActionComponent
     });
     registry.add('action', 'createFolder', {
         buttonIcon: <AddFolder/>,
         buttonLabel: 'jcontent:label.contentManager.create.folder',
-        targets: ['createMenuActions:3', 'contentActions:3', 'headerPrimaryActions:2.5'],
+        targets: ['createMenuActions:3', 'contentActions:3', 'headerPrimaryActions:2.5', 'narrowHeaderMenu:2.5'],
         createFolderType: 'folder',
         component: CreateFolderActionComponent
     });
     registry.add('action', 'refresh', {
         buttonIcon: <Reload/>,
         buttonLabel: 'jcontent:label.contentManager.refresh',
-        targets: ['headerPrimaryActions:15'],
+        targets: ['headerPrimaryActions:15', 'narrowHeaderMenu:5'],
         onClick: () => {
             triggerRefetchAll();
         }
@@ -110,7 +111,7 @@ export const jContentActions = registry => {
     registry.add('action', 'fileUpload', {
         buttonIcon: <Publish/>,
         buttonLabel: 'jcontent:label.contentManager.fileUpload.uploadButtonLabel',
-        targets: ['createMenuActions:4', 'contentActions:4', 'headerPrimaryActions:3'],
+        targets: ['createMenuActions:4', 'contentActions:4', 'headerPrimaryActions:3', 'narrowHeaderMenu:2.3'],
         uploadType: 'fileUpload',
         component: FileUploadActionComponent
     });
@@ -124,7 +125,7 @@ export const jContentActions = registry => {
         buttonIcon: <CloudUpload/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.publish',
         buttonLabelShort: 'jcontent:label.contentManager.contentPreview.publishShort',
-        targets: ['publishMenu:1'],
+        targets: ['publishMenu:1', 'narrowHeaderSelectionMenu:11'],
         publishType: 'publish',
         isPublishingAllLanguages: false,
         component: PublishActionComponent
@@ -132,7 +133,7 @@ export const jContentActions = registry => {
     registry.add('action', 'publishInAllLanguages', {
         buttonIcon: <CloudUpload/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.publishInAllLanguages',
-        targets: ['publishMenu:2'],
+        targets: ['publishMenu:2', 'narrowHeaderSelectionMenu:12'],
         publishType: 'publish',
         isPublishingAllLanguages: true,
         component: PublishActionComponent
@@ -141,7 +142,7 @@ export const jContentActions = registry => {
         buttonIcon: <CloudUpload/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.publishAll',
         buttonLabelShort: 'jcontent:label.contentManager.contentPreview.publishShort',
-        targets: ['publishMenu:3'],
+        targets: ['publishMenu:3', 'narrowHeaderSelectionMenu:13'],
         publishType: 'publishAll',
         isPublishingAllLanguages: false,
         component: PublishActionComponent
@@ -149,7 +150,7 @@ export const jContentActions = registry => {
     registry.add('action', 'publishAllInAllLanguages', {
         buttonIcon: <CloudUpload/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.publishAllInAllLanguages',
-        targets: ['publishMenu:4'],
+        targets: ['publishMenu:4', 'narrowHeaderSelectionMenu:14'],
         publishType: 'publishAll',
         isPublishingAllLanguages: true,
         component: PublishActionComponent
@@ -157,13 +158,13 @@ export const jContentActions = registry => {
     registry.add('action', 'publishDeletion', {
         buttonIcon: <Delete/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.publishDeletion',
-        targets: ['contentActions:4', 'selectedContentActions:4'],
+        targets: ['contentActions:4', 'selectedContentActions:4', 'narrowHeaderSelectionMenu:4', 'narrowHeaderMenu:14'],
         component: PublishDeletionActionComponent
     });
     registry.add('action', 'unpublish', {
         buttonIcon: <NoCloud/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.unpublish',
-        targets: ['publishMenu:5'],
+        targets: ['publishMenu:5', 'narrowHeaderSelectionMenu:15'],
         publishType: 'unpublish',
         isPublishingAllLanguages: false,
         component: PublishActionComponent
@@ -171,7 +172,7 @@ export const jContentActions = registry => {
     registry.add('action', 'unpublishInAllLanguages', {
         buttonIcon: <NoCloud/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.unpublishInAllLanguages',
-        targets: ['publishMenu:6'],
+        targets: ['publishMenu:6', 'narrowHeaderSelectionMenu:16'],
         publishType: 'unpublish',
         isPublishingAllLanguages: true,
         component: PublishActionComponent
@@ -196,14 +197,14 @@ export const jContentActions = registry => {
     registry.add('action', 'copy', {
         buttonIcon: <Copy/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.copy',
-        targets: ['contentActions:3.8', 'selectedContentActions:3.8'],
+        targets: ['contentActions:3.8', 'selectedContentActions:3.8', 'narrowHeaderSelectionMenu:3.8'],
         copyCutType: 'copy',
         component: CopyCutActionComponent
     });
     registry.add('action', 'paste', {
         buttonIcon: <Paste/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.paste',
-        targets: ['headerPrimaryActions:10', 'contentActions:3.91'],
+        targets: ['headerPrimaryActions:10', 'contentActions:3.91', 'narrowHeaderMenu:4'],
         component: PasteActionComponent
     });
     registry.add('action', 'pasteReference', {
@@ -216,50 +217,50 @@ export const jContentActions = registry => {
     registry.add('action', 'cut', {
         buttonIcon: <Cut/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.cut',
-        targets: ['contentActions:3.9', 'selectedContentActions:3.9'],
+        targets: ['contentActions:3.9', 'selectedContentActions:3.9', 'narrowHeaderSelectionMenu:3.9'],
         copyCutType: 'cut',
         component: CopyCutActionComponent
     });
     registry.add('action', 'delete', {
         buttonIcon: <Delete/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.delete',
-        targets: ['contentActions:4', 'selectedContentActions:4'],
+        targets: ['contentActions:4', 'selectedContentActions:4', 'narrowHeaderMenu:12', 'narrowHeaderSelectionMenu:4'],
         component: DeleteActionComponent
     });
     registry.add('action', 'deletePermanently', {
         buttonIcon: <Delete/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.deletePermanently',
-        targets: ['contentActions:4', 'selectedContentActions:4'],
+        targets: ['contentActions:4', 'selectedContentActions:4', 'narrowHeaderMenu:12', 'narrowHeaderSelectionMenu:4'],
         component: DeletePermanentlyActionComponent
     });
     registry.add('action', 'undelete', {
         buttonIcon: <Undelete/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.undelete',
-        targets: ['contentActions:4.1', 'selectedContentActions:4.1'],
+        targets: ['contentActions:4.1', 'selectedContentActions:4.1', 'narrowHeaderMenu:12', 'narrowHeaderSelectionMenu:4'],
         component: UndeleteActionComponent
     });
     registry.add('action', 'lock', {
         buttonLabel: 'jcontent:label.contentManager.contextMenu.lockActions.lock',
-        targets: ['contentActions:5'],
+        targets: ['contentActions:5', 'narrowHeaderMenu:14'],
         buttonIcon: <Lock/>,
         component: LockActionComponent
     });
     registry.add('action', 'unlock', {
         buttonLabel: 'jcontent:label.contentManager.contextMenu.lockActions.unlock',
-        targets: ['contentActions:5'],
+        targets: ['contentActions:5', 'narrowHeaderMenu:14'],
         buttonIcon: <Unlock/>,
         component: UnlockActionComponent
     });
     registry.add('action', 'clearAllLocks', {
         buttonIcon: <Lock/>,
         buttonLabel: 'jcontent:label.contentManager.contextMenu.lockActions.clearAllLocks',
-        targets: ['contentActions:5.5'],
+        targets: ['contentActions:5.5', 'narrowHeaderMenu:14'],
         component: ClearAllLocksActionComponent
     });
     registry.add('action', 'locate', {
         buttonLabel: 'jcontent:label.contentManager.actions.locate',
         buttonIcon: <Search/>,
-        targets: ['contentActions:0.5'],
+        targets: ['contentActions:0.5', 'narrowHeaderMenu:10.5'],
         component: LocateActionComponent
     });
     registry.add('action', 'subContents', {
@@ -271,7 +272,7 @@ export const jContentActions = registry => {
     registry.add('action', 'exportPage', {
         buttonIcon: <Upload/>,
         buttonLabel: 'jcontent:label.contentManager.export.actionLabel',
-        targets: ['contentActions:4.2'],
+        targets: ['contentActions:4.2', 'narrowHeaderMenu:13'],
         showOnNodeTypes: ['jnt:page'],
         requiredSitePermission: [ACTION_PERMISSIONS.exportPageAction],
         component: ExportActionComponent
@@ -279,7 +280,7 @@ export const jContentActions = registry => {
     registry.add('action', 'export', {
         buttonIcon: <Upload/>,
         buttonLabel: 'jcontent:label.contentManager.export.actionLabel',
-        targets: ['contentActions:4.2'],
+        targets: ['contentActions:4.2', 'narrowHeaderMenu:13'],
         showOnNodeTypes: ['jnt:contentFolder', 'jnt:content'],
         requiredSitePermission: [ACTION_PERMISSIONS.exportAction],
         component: ExportActionComponent
@@ -287,46 +288,46 @@ export const jContentActions = registry => {
     registry.add('action', 'downloadAsZip', {
         buttonIcon: <Archive/>,
         buttonLabel: 'jcontent:label.contentManager.downloadAsZip',
-        targets: ['contentActions:4.21', 'selectedContentActions'],
+        targets: ['contentActions:4.21', 'selectedContentActions', 'narrowHeaderMenu:14', 'narrowHeaderSelectionMenu:1'],
         showOnNodeTypes: ['jnt:file', 'jnt:folder'],
         component: DownloadAsZipActionComponent
     });
     registry.add('action', 'import', {
         buttonIcon: <Download/>,
         buttonLabel: 'jcontent:label.contentManager.import.action',
-        targets: ['contentActions:4.3', 'createMenuActions:3.5'],
+        targets: ['contentActions:4.3', 'createMenuActions:3.5', 'narrowHeaderMenu:4', 'narrowHeaderSelectionMenu:4'],
         uploadType: 'import',
         component: FileUploadActionComponent
     });
     registry.add('action', 'editImage', {
         buttonIcon: <Edit/>,
         buttonLabel: 'jcontent:label.contentManager.editImage.action',
-        targets: ['contentActions:2.5'],
+        targets: ['contentActions:2.5', 'narrowHeaderMenu:12.5'],
         component: EditImageActionComponent
     });
     registry.add('action', 'downloadFile', {
         buttonIcon: <CloudDownload/>,
         buttonLabel: 'jcontent:label.contentManager.contentPreview.download',
-        targets: ['contentActions:3.7'],
+        targets: ['contentActions:3.7', 'narrowHeaderMenu:13.7'],
         component: DownloadFileActionComponent
     });
     registry.add('action', 'replaceFile', {
         buttonIcon: <Reload/>,
         buttonLabel: 'jcontent:label.contentManager.fileUpload.replaceWith',
-        targets: ['contentActions:0.2'],
+        targets: ['contentActions:0.2', 'narrowHeaderMenu:10.2'],
         uploadType: 'replaceWith',
         component: FileUploadActionComponent
     });
     registry.add('action', 'zip', {
         buttonIcon: <FileZip/>,
         buttonLabel: 'jcontent:label.contentManager.zipUnzip.zip',
-        targets: ['contentActions:2.1', 'selectedContentActions'],
+        targets: ['contentActions:2.1', 'selectedContentActions', 'narrowHeaderSelectionMenu:0.5', 'narrowHeaderMenu:12'],
         component: ZipActionComponent
     });
     registry.add('action', 'unzip', {
         buttonIcon: <FileZip/>,
         buttonLabel: 'jcontent:label.contentManager.zipUnzip.unzip',
-        targets: ['contentActions:2.2'],
+        targets: ['contentActions:2.2', 'narrowHeaderMenu:12.2'],
         component: UnzipActionComponent
     });
     registry.add('action', 'search', {
@@ -355,7 +356,7 @@ export const jContentActions = registry => {
     });
 
     registry.add('action', 'contentActionsSeparator2', {
-        targets: ['contentActions:10'],
+        targets: ['contentActions:10', 'narrowHeaderMenu:10'],
         isSeparator: true
     });
 
@@ -364,5 +365,43 @@ export const jContentActions = registry => {
         buttonLabel: 'jcontent:label.contentManager.contentPreview.clear',
         targets: ['headerPrimaryActions:14'],
         component: ClearClipboardActionComponent
+    });
+
+    /* Narrow header menu actions */
+
+    registry.add('action', 'narrowHeaderMenu', menuActionWithRenderer, {
+        buttonLabel: 'jcontent:label.contentManager.actions.menuLabel.actions',
+        targets: [],
+        menuTarget: 'narrowHeaderMenu',
+        menuItemProps: {
+            isShowIcons: true
+        }
+    });
+
+    registry.add('action', 'narrowHeaderSelectionMenu', menuActionWithRenderer, {
+        buttonLabel: 'jcontent:label.contentManager.actions.menuLabel.actions',
+        targets: [],
+        menuTarget: 'narrowHeaderSelectionMenu',
+        menuItemProps: {
+            isShowIcons: true
+        }
+    });
+
+    registry.add('action', 'actionsLabel', {
+        targets: ['narrowHeaderMenu:0', 'narrowHeaderSelectionMenu:0'],
+        label: 'jcontent:label.contentManager.actions.menuLabel.actions',
+        component: MenuLabelActionComponent
+    });
+
+    registry.add('action', 'moreActionsLabel', {
+        targets: ['narrowHeaderMenu:10.5'],
+        label: 'jcontent:label.contentManager.actions.menuLabel.moreActions',
+        component: MenuLabelActionComponent
+    });
+
+    registry.add('action', 'publicationActionsLabel', {
+        targets: ['narrowHeaderSelectionMenu:10.5'],
+        label: 'jcontent:label.contentManager.actions.menuLabel.publication',
+        component: MenuLabelActionComponent
     });
 };

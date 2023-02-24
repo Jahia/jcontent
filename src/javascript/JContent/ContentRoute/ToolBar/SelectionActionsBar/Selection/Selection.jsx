7edc1c1ea7ef5174434f41abf6c2c45d58eb8ea3
Alex Karmanov <alexandrk_@hotmail.com>
2023-02-24 12:17:46 -0500

    BACKLOG-20592 Restore files after merge

---
+++ src/javascript/JContent/ContentRoute/ToolBar/SelectionActionsBar/Selection/Selection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Button, Cancel, Typography} from '@jahia/moonstone';
+import styles from '~/JContent/ContentRoute/ToolBar/ToolBar.scss';
+import {useTranslation} from 'react-i18next';
+
+export const Selection = ({paths, clear}) => {
+    const {t} = useTranslation('jcontent');
+
+    return (
+        <div className="flexRow">
+            <Typography variant="caption"
+                        data-cm-role="selection-infos"
+                        data-cm-selection-size={paths.length}
+                        className={`${styles.selection}`}
+            >
+                {t('jcontent:label.contentManager.selection.itemsSelected', {count: paths.length})}
+            </Typography>
+            <div className={styles.spacer}/>
+            <Button icon={<Cancel/>} variant="ghost" size="default" onClick={clear}/>
+        </div>
+    );
+};
+
+Selection.propTypes = {
+    paths: PropTypes.string.isRequired,
+    clear: PropTypes.func.isRequired
+};

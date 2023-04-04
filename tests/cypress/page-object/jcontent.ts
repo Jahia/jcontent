77689a8521ed26e56f387e79140b43bac090207a
Janin Michel-Mathias <107043504+Janin-Michel-Mathias@users.noreply.github.com>
2023-04-04 10:19:39 +0200

    TECH-1199 Jcontent part of TECH-1045 (Implement CopyCutPaste tests) (#737)
    * move tests to jcontent
    
    * after lint --fix
    
    * fixes for static analysis

--- tests/cypress/page-object/jcontent.ts
+++ tests/cypress/page-object/jcontent.ts
@@ -118,6 +118,24 @@ export class JContent extends BasePage {
         cy.get('.moonstone-header button[data-sel-role="clearClipboard"]').click();
         return this;
     }
+
+    rightClickMenu(operation: string, elementName: string) {
+        cy.get('td[data-cm-role="table-content-list-cell-name"]').contains(elementName).rightclick();
+        return cy.get('menu[data-sel-role="jcontent-contentMenu"]:not([class*="moonstone-hidden"]').find(`li[data-sel-role="${operation}"]`).click();
+    }
+
+    paste() {
+        return cy.get('span:contains("Paste"):not(:contains("Paste "))').click();
+    }
+
+    checkUserCanNotPaste() {
+        cy.get('span:contains("Paste"):not(:contains("Pasteur"))').should('have.length', 0);
+    }
+
+    checkUserCanNotCut(elementName: string) {
+        cy.get('td[data-cm-role="table-content-list-cell-name"]').contains(elementName).rightclick();
+        cy.get('menu[data-sel-role="jcontent-contentMenu"]:not([class*="moonstone-hidden"]').find('li[data-sel-role="cut"]').should('not.exist');
+    }
 }
 
 export class JContentPageComposer extends JContent {
---
+++ tests/cypress/utils/graphqlUtils.ts
@@ -0,0 +1,114 @@
+export class GraphqlUtils {
+    // Graphql mutation to set property on a node
+    public static setProperties = (pathOrId: string, property: string, values: Array<string>, language: string) => {
+        cy.apollo({
+            variables: {
+                pathOrId: pathOrId,
+                property: property,
+                values: values,
+                language: language
+            },
+            mutationFile: 'jcontent/jcrSetProperties.graphql'
+        }).then(result => {
+            expect(result?.data?.jcr?.mutateNode?.mutateProperty?.setValues).to.eq(true);
+        });
+    };
+
+    // Graphql mutation to add a node
+    public static addNode = (parentPathOrId: string, primaryNodeType: string, name: string) => {
+        cy.apollo({
+            variables: {
+                parentPathOrId: parentPathOrId,
+                primaryNodeType: primaryNodeType,
+                name: name
+            },
+            mutationFile: 'jcontent/jcrAddNode.graphql'
+        }).then(result => {
+            expect(result?.data?.jcr?.addNode?.node?.name).to.eq(name);
+        });
+    };
+
+    // Graphql mutation to remove a node
+    public static deleteNode = (pathOrId: string) => {
+        cy.apollo({
+            variables: {
+                pathOrId: pathOrId
+            },
+            mutationFile: 'jcontent/jcrDeleteNode.graphql'
+        }).then(result => {
+            expect(result?.data?.jcr?.deleteNode).to.eq(true);
+        });
+    };
+
+    // Graphql mutation to remove a Node property
+    public static deleteProperty = (pathOrId: string, property: string, language: string) => {
+        cy.apollo({
+            variables: {
+                pathOrId: pathOrId,
+                property: property,
+                language: language
+            },
+            mutationFile: 'jcontent/jcrDeleteProperty.graphql'
+        }).then(result => {
+            expect(result?.data?.jcr?.mutateNode?.mutateProperty?.delete).to.eq(true);
+        });
+    };
+
+    public static getNodeId = (path: string, as?: string) => {
+        const asValue = typeof as === 'undefined' ? 'ret' : as;
+
+        cy.apollo({
+            variables: {
+                path: path
+            },
+            queryFile: 'jcontent/jcrGetNode.graphql'
+        }).its('data.jcr.nodeByPath.uuid').as(asValue);
+        if (typeof as === 'undefined') {
+            cy.get('@ret').then(uuid => {
+                return `${uuid}`;
+            });
+        }
+    };
+
+    public static setProperty = (pathOrId: string, property: string, value: string, language: string) => {
+        cy.apollo({
+            variables: {
+                pathOrId: pathOrId,
+                property: property,
+                value: value,
+                language: language
+            },
+            mutationFile: 'jcontent/jcrSetProperty.graphql'
+        }).then(result => {
+            expect(result?.data?.jcr?.mutateNode?.mutateProperty?.setValue).to.eq(true);
+        });
+    };
+
+    public static addMixins = (pathOrId: string, mixins: Array<string>, expectedMixins?: Array<string>) => {
+        cy.apollo({
+            variables: {
+                pathOrId: pathOrId,
+                mixins: mixins
+            },
+            mutationFile: 'jcontent/jcrAddMixins.graphql'
+        }).then(result => {
+            const mixinsRes = result?.data?.jcr?.mutateNode?.addMixins;
+            const expected = typeof expectedMixins === 'undefined' ? mixins : expectedMixins;
+            expect(JSON.stringify(mixinsRes)).to.eq(JSON.stringify(expected));
+        });
+    };
+
+    public static removeMixins = (pathOrId: string, mixins: Array<string>, expectedMixins?: Array<string>) => {
+        cy.apollo({
+            variables: {
+                pathOrId: pathOrId,
+                mixins: mixins
+            },
+            mutationFile: 'jcontent/jcrRemoveMixins.graphql'
+        }).then(result => {
+            const mixinsRes = result?.data?.jcr?.mutateNode?.removeMixins;
+            const expected = typeof expectedMixins === 'undefined' ? [] : expectedMixins;
+            expect(JSON.stringify(mixinsRes)).to.eq(JSON.stringify(expected));
+        });
+    };
+}

45575b2d464154651714a9ad87355a9bded55ae4
Thomas Draier <tdraier@jahia.com>
2023-04-07 14:13:27 +0200

    removed duplicates

--- src/javascript/JContent/ContentRoute/ContentLayout/ContentTable/reactTable/columns/columns.js
+++ src/javascript/JContent/ContentRoute/ContentLayout/ContentTable/reactTable/columns/columns.js
@@ -11,180 +11,84 @@ import {
 import {Header, HeaderSelection} from '../components/headers';
 import {CellUsages} from '../components/cells/CellUsages';
 
-export const mainColumnData = [
-    {
-        id: 'publicationStatus',
-        sortable: false,
-        Header: '',
-        Cell: CellPublicationStatus,
-        width: '15px'
-    },
-    {
-        id: 'selection',
-        sortable: false,
-        Header: HeaderSelection,
-        Cell: CellSelection,
-        width: '50px'
-    },
-    {
-        id: 'name',
-        accessor: 'displayName',
-        label: 'jcontent:label.contentManager.listColumns.name',
-        sortable: true,
-        property: 'displayName',
-        Cell: CellName,
-        Header: Header,
-        width: '300px'
-    },
-    {
-        id: 'status',
-        label: 'jcontent:label.contentManager.listColumns.status',
-        sortable: false,
-        Header: '',
-        Cell: CellStatus,
-        width: '115px'
-    },
-    {
-        id: 'type',
-        accessor: 'primaryNodeType.displayName',
-        label: 'jcontent:label.contentManager.listColumns.type',
-        sortable: true,
-        property: 'primaryNodeType.displayName',
-        Cell: CellType,
-        Header: Header,
-        width: '180px'
-    },
-    {
-        id: 'createdBy',
-        accessor: 'createdBy.value',
-        label: 'jcontent:label.contentManager.listColumns.createdBy',
-        sortable: true,
-        property: 'createdBy.value',
-        Header: Header,
-        Cell: Cell,
-        width: '150px'
+export const publicationStatus = {
+    id: 'publicationStatus',
+    sortable: false,
+    Header: '',
+    Cell: CellPublicationStatus,
+    width: '15px'
+};
+export const selection = {
+    id: 'selection',
+    sortable: false,
+    Header: HeaderSelection,
+    Cell: CellSelection,
+    width: '50px'
+};
+export const name = {
+    id: 'name',
+    accessor: 'displayName',
+    label: 'jcontent:label.contentManager.listColumns.name',
+    sortable: true,
+    property: 'displayName',
+    Cell: CellName,
+    Header: Header,
+    width: '300px'
+};
+export const status = {
+    id: 'status',
+    label: 'jcontent:label.contentManager.listColumns.status',
+    sortable: false,
+    Header: '',
+    Cell: CellStatus,
+    width: '115px'
+};
+export const type = {
+    id: 'type',
+    accessor: 'primaryNodeType.displayName',
+    label: 'jcontent:label.contentManager.listColumns.type',
+    sortable: true,
+    property: 'primaryNodeType.displayName',
+    Cell: CellType,
+    Header: Header,
+    width: '180px'
+};
+export const createdBy = {
+    id: 'createdBy',
+    accessor: 'createdBy.value',
+    label: 'jcontent:label.contentManager.listColumns.createdBy',
+    sortable: true,
+    property: 'createdBy.value',
+    Header: Header,
+    Cell: Cell,
+    width: '150px'
 
-    },
-    {
-        id: 'lastModified',
-        accessor: 'lastModified.value',
-        label: 'jcontent:label.contentManager.listColumns.lastModified',
-        sortable: true,
-        property: 'lastModified.value',
-        Cell: CellLastModified,
-        Header: Header,
-        width: '290px'
-    },
-    {
-        id: 'visibleActions',
-        Header: '',
-        Cell: CellVisibleActions,
-        width: '60px'
-    }
-];
-export const reducedColumnData = [
-    {
-        id: 'publicationStatus',
-        sortable: false,
-        Header: '',
-        Cell: CellPublicationStatus,
-        width: '15px'
-    },
-    {
-        id: 'selection',
-        sortable: false,
-        Header: HeaderSelection,
-        Cell: CellSelection,
-        width: '50px'
-    },
-    {
-        id: 'name',
-        accessor: 'displayName',
-        label: 'jcontent:label.contentManager.listColumns.name',
-        sortable: true,
-        property: 'displayName',
-        Cell: CellName,
-        Header: Header,
-        width: '300px'
-    },
-    {
-        id: 'status',
-        label: 'jcontent:label.contentManager.listColumns.status',
-        sortable: false,
-        Header: '',
-        Cell: CellStatus,
-        width: '115px'
-    },
-    {
-        id: 'createdBy',
-        accessor: 'createdBy.value',
-        label: 'jcontent:label.contentManager.listColumns.createdBy',
-        sortable: true,
-        property: 'createdBy.value',
-        Header: Header,
-        Cell: Cell,
-        width: '150px'
+};
+export const lastModified = {
+    id: 'lastModified',
+    accessor: 'lastModified.value',
+    label: 'jcontent:label.contentManager.listColumns.lastModified',
+    sortable: true,
+    property: 'lastModified.value',
+    Cell: CellLastModified,
+    Header: Header,
+    width: '290px'
+};
+export const visibleActions = {
+    id: 'visibleActions',
+    Header: '',
+    Cell: CellVisibleActions,
+    width: '60px'
+};
+export const usages = {
+    id: 'usages',
+    label: 'jcontent:label.contentManager.deleteAction.infoTable.headerLabel',
+    Header: Header,
+    Cell: CellUsages,
+    sortable: false,
+    width: '100px'
+};
 
-    },
-    {
-        id: 'lastModified',
-        accessor: 'lastModified.value',
-        label: 'jcontent:label.contentManager.listColumns.lastModified',
-        sortable: true,
-        property: 'lastModified.value',
-        Cell: CellLastModified,
-        Header: Header,
-        width: '290px'
-    },
-    {
-        id: 'visibleActions',
-        Header: '',
-        Cell: CellVisibleActions,
-        width: '60px'
-    }
-];
-export const deletionInfoColumnData = [
-    {
-        id: 'publicationStatus',
-        sortable: false,
-        Header: '',
-        Cell: CellPublicationStatus,
-        width: '15px'
-    },
-    {
-        id: 'name',
-        accessor: 'displayName',
-        label: 'jcontent:label.contentManager.listColumns.name',
-        sortable: false,
-        property: 'displayName',
-        Cell: CellName,
-        Header: Header,
-        width: '300px'
-    },
-    {
-        id: 'status',
-        label: 'jcontent:label.contentManager.listColumns.status',
-        sortable: false,
-        Header: '',
-        Cell: CellStatus,
-        width: '115px'
-    },
-    {
-        id: 'type',
-        accessor: 'primaryNodeType.displayName',
-        label: 'jcontent:label.contentManager.listColumns.type',
-        sortable: false,
-        property: 'primaryNodeType.displayName',
-        Cell: CellType,
-        Header: Header,
-        width: '180px'
-    },
-    {
-        id: 'usages',
-        label: 'jcontent:label.contentManager.deleteAction.infoTable.headerLabel',
-        Header: Header,
-        Cell: CellUsages,
-        sortable: false,
-        width: '100px'
-    }
-];
+export const mainColumnData = [publicationStatus, selection, name, status, type, createdBy, lastModified, visibleActions];
+export const reducedColumnData = [publicationStatus, selection, name, status, createdBy, lastModified, visibleActions];
+export const deletionInfoColumnData = [publicationStatus, name, status, type, usages];

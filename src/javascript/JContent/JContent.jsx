e92024193d99f8fa550e2c445d73260ae7b1c65d
Cedric Mailleux <cmailleux@jahia.com>
2023-04-28 15:56:01 -0400

    BACKLOG-20908: Allow to clear selection at the body level (#792)

--- src/javascript/JContent/JContent.jsx
+++ src/javascript/JContent/JContent.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {registry} from '@jahia/ui-extender';
 import {ErrorBoundary, LoaderSuspense} from '@jahia/jahia-ui-root';
 import {LayoutModule} from '@jahia/moonstone';
 import ContentNavigation from './ContentNavigation';
 import {Route, Switch} from 'react-router';
-import {shallowEqual, useSelector} from 'react-redux';
+import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import './colors.scss';
+import {cmClearSelection} from './redux/selection.redux';
 
 export const JContent = () => {
     const routes = registry.find({type: 'route', target: 'jcontent'});
     const {site, mode} = useSelector(state => ({site: state.jcontent.site, mode: state.jcontent.mode}), shallowEqual);
     const item = registry.get('accordionItem', mode);
     const itemEnabled = item && (!item.isEnabled || item.isEnabled(site));
+    const dispatch = useDispatch();
+    const clear = useCallback(() => dispatch(cmClearSelection()), [dispatch]);
+    const handleKeyboardNavigation = useCallback(event => {
+        if (event.key === 'Escape' || event.keyCode === 27) {
+            clear();
+        }
+    }, [clear]);
+
+    useEffect(() => {
+        document.querySelector('body').addEventListener('keydown', handleKeyboardNavigation);
+        return () => {
+            document.querySelector('body').removeEventListener('keydown', handleKeyboardNavigation);
+        };
+    }, [handleKeyboardNavigation]);
 
     return (
         <LayoutModule

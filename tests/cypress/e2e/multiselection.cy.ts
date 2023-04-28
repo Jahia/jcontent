158a005d096d67899776b11c54a191e3d9f64eca
Thomas Draier <tdraier@jahia.com>
2023-04-28 21:57:27 +0200

    BACKLOG-20907: fix test (#791)
    * BACKLOG-20907: fix test
    
    * lint

--- tests/cypress/e2e/multiselection.cy.ts
+++ tests/cypress/e2e/multiselection.cy.ts
@@ -1,5 +1,5 @@
 import {JContent} from '../page-object';
-import {Button, getComponent, getComponentBySelector, Menu} from '@jahia/cypress';
+import {Button, getComponentBySelector} from '@jahia/cypress';
 
 describe('Multi-selection tests', () => {
     beforeEach(function () {
@@ -60,9 +60,8 @@ describe('Multi-selection tests', () => {
         checkToolbar();
         checkSelectionCount(2);
         const selectionDropdown = jcontent.getSelectionDropdown();
-        selectionDropdown.get().find('.moonstone-dropdown').click({force: true});
-        selectionDropdown.get().find('.moonstone-menu').should('be.visible');
-        getComponent(Menu, selectionDropdown).select('images');
+        selectionDropdown.get().should('not.be.disabled');
+        selectionDropdown.select('images');
         checkToolbar();
         checkSelectionCount(1);
     });

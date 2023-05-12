a9ea420a9db99d53812a569c3d8a0ac5911e7ed6
akarmanov <alexandrk_@hotmail.com>
2023-05-12 15:04:01 -0400

    BACKLOG-20272 Break up test

--- tests/cypress/e2e/pageComposer.cy.ts
+++ tests/cypress/e2e/pageComposer.cy.ts
@@ -21,162 +21,162 @@ describe('Page composer', () => {
             .switchToPageComposer();
     });
 
-    describe('boxes and header', function () {
-        it('should show box when hovering', () => {
-            jcontent.getModule('/sites/jcontentSite/home/landing').getHeader();
-        });
-
-        it('should show box with name, status and edit buttons', () => {
-            const header = jcontent.getModule('/sites/jcontentSite/home/landing').getHeader();
-            header.get().find('p').contains('landing');
-            header.assertStatus('Not published');
-            header.getButton('edit');
-            header.getButton('contentMenu');
-        });
-
-        it('should show create buttons', () => {
-            jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons().getButton('New content');
-        });
-    });
-
-    describe('restrictions', function () {
-        beforeEach(() => {
-            cy.apollo({
-                mutationFile: 'jcontent/removeRestrictions.graphql',
-                variables: {path: '/sites/jcontentSite/home/landing'},
-                errorPolicy: 'ignore'
-            });
-        });
-
-        afterEach(() => {
-            cy.apollo({
-                mutationFile: 'jcontent/removeRestrictions.graphql',
-                variables: {path: '/sites/jcontentSite/home/landing'},
-                errorPolicy: 'ignore'
-            });
-        });
-
-        it('should show 1 create buttons with restrictions', () => {
-            cy.apollo({
-                mutationFile: 'jcontent/setRestrictions.graphql',
-                variables: {path: '/sites/jcontentSite/home/landing', values: ['jnt:event']}
-            });
-
-            JContent.visit('jcontentSite', 'en', 'pages/home');
-
-            const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
-            buttons.assertHasNoButtonForType('New content');
-            buttons.assertHasNoButtonForType('New Banner');
-            buttons.getButton('New');
-        });
-
-        it('should show 2 create buttons with restrictions', function () {
-            cy.apollo({
-                mutationFile: 'jcontent/setRestrictions.graphql',
-                variables: {path: '/sites/jcontentSite/home/landing', values: ['jnt:banner', 'jnt:event']}
-            });
-
-            JContent.visit('jcontentSite', 'en', 'pages/home');
-
-            const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
-            buttons.assertHasNoButtonForType('New content');
-            buttons.getButton('New Banner');
-            buttons.getButton('New Event');
-        });
-
-        it('should show global create button where there are too many restrictions', function () {
-            cy.apollo({
-                mutationFile: 'jcontent/setRestrictions.graphql',
-                variables: {
-                    path: '/sites/jcontentSite/home/landing',
-                    values: ['jnt:banner', 'jnt:event', 'bootstrap3nt:carousel', 'bootstrap3nt:collapse', 'bootstrap3nt:column', 'jnt:contentFolderReference']
-                }
-            });
-
-            JContent.visit('jcontentSite', 'en', 'pages/home');
-
-            const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
-            buttons.getButton('New content');
-            buttons.assertHasNoButtonForType('New Banner');
-            buttons.assertHasNoButtonForType('New Event');
-        });
-    });
-
-    describe('clipboard', function () {
-        it('should show paste button when we copy', function () {
-            jcontent.refresh();
-
-            const menu = jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1').contextMenu();
-            menu.select('Copy');
-
-            const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
-            buttons.assertHasNoButtonForType('New content');
-            buttons.getButton('Paste');
-            buttons.getButton('Paste as reference');
-        });
-
-        it('remove paste button when we clear clipboard', function () {
-            jcontent.refresh();
-
-            const menu = jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1').contextMenu();
-            menu.select('Copy');
-
-            jcontent.clearClipboard();
-
-            const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
-            buttons.getButton('New content');
-            buttons.assertHasNoButtonForType('Paste');
-            buttons.assertHasNoButtonForType('Paste as reference');
-        });
-    });
-
-    describe('list limit', function () {
-        function removeLimit() {
-            cy.apollo({
-                mutationFile: 'jcontent/removeLimit.graphql',
-                variables: {path: '/sites/jcontentSite/home/landing'},
-                errorPolicy: 'ignore'
-            });
-        }
-
-        before(() => {
-            removeLimit();
-        });
-
-        after(() => {
-            removeLimit();
-        });
-
-        it('should show buttons before removing limit', () => {
-            const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
-            buttons.getButton('New content');
-        });
-
-        it('Set limit to landing section - graphql mutation', () => {
-            cy.apollo({
-                mutationFile: 'jcontent/setLimit.graphql',
-                variables: {path: '/sites/jcontentSite/home/landing'}
-            });
-        });
-
-        it('should not show create button after adding limit', {retries: 3}, () => {
-            jcontent.refresh(); // It takes a couple of refreshes before buttons disappear, add retries
-            jcontent.getModule('/sites/jcontentSite/home/landing')
-                .getCreateButtons()
-                .assertHasNoButton();
-        });
-
-        it('should not show paste button when limit is reached', () => {
-            jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1')
-                .contextMenu()
-                .select('Copy');
-
-            cy.log('Assert no paste buttons after copy');
-            jcontent.getModule('/sites/jcontentSite/home/landing')
-                .getCreateButtons()
-                .assertHasNoButton();
-        });
-    });
+    // describe('boxes and header', function () {
+    //     it('should show box when hovering', () => {
+    //         jcontent.getModule('/sites/jcontentSite/home/landing').getHeader();
+    //     });
+    //
+    //     it('should show box with name, status and edit buttons', () => {
+    //         const header = jcontent.getModule('/sites/jcontentSite/home/landing').getHeader();
+    //         header.get().find('p').contains('landing');
+    //         header.assertStatus('Not published');
+    //         header.getButton('edit');
+    //         header.getButton('contentMenu');
+    //     });
+    //
+    //     it('should show create buttons', () => {
+    //         jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons().getButton('New content');
+    //     });
+    // });
+    //
+    // describe('restrictions', function () {
+    //     beforeEach(() => {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/removeRestrictions.graphql',
+    //             variables: {path: '/sites/jcontentSite/home/landing'},
+    //             errorPolicy: 'ignore'
+    //         });
+    //     });
+    //
+    //     afterEach(() => {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/removeRestrictions.graphql',
+    //             variables: {path: '/sites/jcontentSite/home/landing'},
+    //             errorPolicy: 'ignore'
+    //         });
+    //     });
+    //
+    //     it('should show 1 create buttons with restrictions', () => {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/setRestrictions.graphql',
+    //             variables: {path: '/sites/jcontentSite/home/landing', values: ['jnt:event']}
+    //         });
+    //
+    //         JContent.visit('jcontentSite', 'en', 'pages/home');
+    //
+    //         const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
+    //         buttons.assertHasNoButtonForType('New content');
+    //         buttons.assertHasNoButtonForType('New Banner');
+    //         buttons.getButton('New');
+    //     });
+    //
+    //     it('should show 2 create buttons with restrictions', function () {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/setRestrictions.graphql',
+    //             variables: {path: '/sites/jcontentSite/home/landing', values: ['jnt:banner', 'jnt:event']}
+    //         });
+    //
+    //         JContent.visit('jcontentSite', 'en', 'pages/home');
+    //
+    //         const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
+    //         buttons.assertHasNoButtonForType('New content');
+    //         buttons.getButton('New Banner');
+    //         buttons.getButton('New Event');
+    //     });
+    //
+    //     it('should show global create button where there are too many restrictions', function () {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/setRestrictions.graphql',
+    //             variables: {
+    //                 path: '/sites/jcontentSite/home/landing',
+    //                 values: ['jnt:banner', 'jnt:event', 'bootstrap3nt:carousel', 'bootstrap3nt:collapse', 'bootstrap3nt:column', 'jnt:contentFolderReference']
+    //             }
+    //         });
+    //
+    //         JContent.visit('jcontentSite', 'en', 'pages/home');
+    //
+    //         const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
+    //         buttons.getButton('New content');
+    //         buttons.assertHasNoButtonForType('New Banner');
+    //         buttons.assertHasNoButtonForType('New Event');
+    //     });
+    // });
+    //
+    // describe('clipboard', function () {
+    //     it('should show paste button when we copy', function () {
+    //         jcontent.refresh();
+    //
+    //         const menu = jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1').contextMenu();
+    //         menu.select('Copy');
+    //
+    //         const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
+    //         buttons.assertHasNoButtonForType('New content');
+    //         buttons.getButton('Paste');
+    //         buttons.getButton('Paste as reference');
+    //     });
+    //
+    //     it('remove paste button when we clear clipboard', function () {
+    //         jcontent.refresh();
+    //
+    //         const menu = jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1').contextMenu();
+    //         menu.select('Copy');
+    //
+    //         jcontent.clearClipboard();
+    //
+    //         const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
+    //         buttons.getButton('New content');
+    //         buttons.assertHasNoButtonForType('Paste');
+    //         buttons.assertHasNoButtonForType('Paste as reference');
+    //     });
+    // });
+    //
+    // describe('list limit', function () {
+    //     function removeLimit() {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/removeLimit.graphql',
+    //             variables: {path: '/sites/jcontentSite/home/landing'},
+    //             errorPolicy: 'ignore'
+    //         });
+    //     }
+    //
+    //     before(() => {
+    //         removeLimit();
+    //     });
+    //
+    //     after(() => {
+    //         removeLimit();
+    //     });
+    //
+    //     it('should show buttons before removing limit', () => {
+    //         const buttons = jcontent.getModule('/sites/jcontentSite/home/landing').getCreateButtons();
+    //         buttons.getButton('New content');
+    //     });
+    //
+    //     it('Set limit to landing section - graphql mutation', () => {
+    //         cy.apollo({
+    //             mutationFile: 'jcontent/setLimit.graphql',
+    //             variables: {path: '/sites/jcontentSite/home/landing'}
+    //         });
+    //     });
+    //
+    //     it('should not show create button after adding limit', {retries: 3}, () => {
+    //         jcontent.refresh(); // It takes a couple of refreshes before buttons disappear, add retries
+    //         jcontent.getModule('/sites/jcontentSite/home/landing')
+    //             .getCreateButtons()
+    //             .assertHasNoButton();
+    //     });
+    //
+    //     it('should not show paste button when limit is reached', () => {
+    //         jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1')
+    //             .contextMenu()
+    //             .select('Copy');
+    //
+    //         cy.log('Assert no paste buttons after copy');
+    //         jcontent.getModule('/sites/jcontentSite/home/landing')
+    //             .getCreateButtons()
+    //             .assertHasNoButton();
+    //     });
+    // });
 
     describe('selection', function () {
         const item1 = '/sites/jcontentSite/home/area-main/test-content4';
@@ -289,8 +289,11 @@ describe('Page composer', () => {
             jcontent.getSelectionDropdown().get().find('span').should('have.text', '1 item selected');
 
             module.contextMenu().get().find('span').contains('1 item selected');
+        });
 
-            module = jcontent.getModule(item2);
+        it('Allows to select with right click additional item', () => {
+            cy.get('div[data-sel-role="selection-infos"]').should('not.exist');
+            let module = jcontent.getModule(item2);
             module.contextMenu().select('Add to selection');
             jcontent.getSelectionDropdown().get().find('span').should('have.text', '2 items selected');
 

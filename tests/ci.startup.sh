114ccfe3d71f550a489ff17eb4f8a5a6d71de4e0
akarmanov <alexandrk_@hotmail.com>
2023-05-16 11:58:56 -0400

    BACKLOG-21002 Propagate license

--- tests/ci.startup.sh
+++ tests/ci.startup.sh
@@ -10,6 +10,18 @@ echo " MANIFEST: ${MANIFEST}"
 echo " TESTS_IMAGE: ${TESTS_IMAGE}"
 echo " JAHIA_IMAGE: ${JAHIA_IMAGE}"
 
+echo "$(date +'%d %B %Y - %k:%M') [LICENSE] == Check if license exists in env variable (JAHIA_LICENSE) =="
+if [[ -z ${JAHIA_LICENSE} ]]; then
+    echo "$(date +'%d %B %Y - %k:%M') [LICENSE] == Jahia license does not exist, checking if there is a license file in /tmp/license.xml =="
+    if [[ -f /tmp/license.xml ]]; then
+        echo "$(date +'%d %B %Y - %k:%M') [LICENSE] ==  License found in /tmp/license.xml, base64ing it"
+        export JAHIA_LICENSE=$(base64 /tmp/license.xml)
+    else
+        echo "$(date +'%d %B %Y - %k:%M') [LICENSE]  == STARTUP FAILURE, unable to find license =="
+        exit 1
+    fi
+fi
+
 docker-compose pull jahia
 docker-compose up -d --renew-anon-volumes --remove-orphans --force-recreate jahia
 

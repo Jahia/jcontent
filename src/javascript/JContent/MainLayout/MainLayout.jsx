744b5b385eecc663620d6352d4bc3726c72bcdc7
Thomas Draier <tdraier@jahia.com>
2023-05-16 17:25:50 +0200

    BACKLOG-20954: Fixed layout for small screens

--- src/javascript/JContent/MainLayout/MainLayout.jsx
+++ src/javascript/JContent/MainLayout/MainLayout.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {ResizeContext, useResize} from './ResizeObserver';
 import {LayoutContent} from '@jahia/moonstone';
-
+import styles from './MainLayout.scss';
 export const MainLayout = ({header, children}) => {
     const {ref, width} = useResize();
 
     return (
         <ResizeContext.Provider value={width}>
-            <LayoutContent ref={ref} hasPadding content={children} header={header}/>
+            <LayoutContent ref={ref} hasPadding content={children} header={header} className={styles.layout}/>
         </ResizeContext.Provider>
     );
 };

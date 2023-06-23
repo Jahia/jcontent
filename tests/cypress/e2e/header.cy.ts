5e38e536df284c8ce3ce681c973935f773197b04
Geofrey Flores <gflores@jahia.com>
2023-06-23 09:57:11 -0400

    Fix cypress test

--- tests/cypress/e2e/header.cy.ts
+++ tests/cypress/e2e/header.cy.ts
@@ -1,11 +1,11 @@
-import {JContent, JContentPageComposer} from "../page-object";
+import {JContent} from '../page-object';
 
 describe('jContent header tests', () => {
-    let jcontent: JContentPageComposer;
-    const path = 'pages/home';
+    let jcontent: JContent;
+    const siteKey = 'jcontentSite-header';
 
     before(() => {
-        cy.executeGroovy('jcontent/createSite.groovy', {SITEKEY: 'jcontentSite-header'});
+        cy.executeGroovy('jcontent/createSite.groovy', {SITEKEY: siteKey});
         cy.apollo({mutationFile: 'jcontent/createContent.graphql'});
         cy.apollo({mutationFile: 'jcontent/enablePageComposer.graphql'});
     });
@@ -17,32 +17,34 @@ describe('jContent header tests', () => {
 
     beforeEach(() => {
         cy.loginEditor();
-        jcontent = JContent
-            .visit('jcontentSite', 'en', path)
-            .switchToPageComposer();
+        jcontent = JContent.visit(siteKey, 'en', 'pages/home');
     });
 
-    it('Should open preview in new tab', () => {
-        let url: string = '';
-        cy.window().should(win => {
-            cy.stub(win, 'open', (u) => url = u).as('stubOpen')
-        });
+    function getHeaderSelector(role) {
+        return `.moonstone-header button[data-sel-role="${role}"]`;
+    }
 
+    it('Should open preview in new tab', () => {
+        cy.window().then(win => {
+            cy.stub(win, 'open', url => {
+                expect(url.includes('cms/render/default', `url: ${url}`)).to.be.true;
+                expect(url.endsWith(`${siteKey}/home.html`), `url: ${url}`).to.be.true;
+            });
+        });
         jcontent.getHeaderActionButton('openInPreview').click();
-        expect('@stubOpen').to.be.called;
-        expect(url.includes('cms/render/default')).to.be.true;
-        expect(url.endsWith(`${path}.html`)).to.be.true;
     });
 
     it('Does not show open preview and open live buttons in content folders accordion', () => {
         jcontent.getAccordionItem('content-folders').click();
-        jcontent.getHeaderActionButton('openInPreview').should('not.exist');
-        jcontent.getHeaderActionButton('openInLive').should('not.exist');
+        jcontent.getHeaderActionButton('publishAll').should('exist'); // Verify header is loaded first
+        cy.get(getHeaderSelector('openInPreview'), {timeout: 5000}).should('not.exist');
+        cy.get(getHeaderSelector('openInLive'), {timeout: 5000}).should('not.exist');
     });
 
     it('Does not show open preview and open live buttons in media accordion', () => {
         jcontent.getAccordionItem('media').click();
-        jcontent.getHeaderActionButton('openInPreview').should('not.exist');
-        jcontent.getHeaderActionButton('openInLive').should('not.exist');
+        jcontent.getHeaderActionButton('publishAll').should('exist'); // Verify header is loaded first
+        cy.get(getHeaderSelector('openInPreview'), {timeout: 5000}).should('not.exist');
+        cy.get(getHeaderSelector('openInLive'), {timeout: 5000}).should('not.exist');
     });
-})
+});

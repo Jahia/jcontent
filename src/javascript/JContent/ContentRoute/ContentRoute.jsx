f32e613ac9730d4dddd1395c7b3e7b8dd5114904
Cedric Mailleux <cmailleux@jahia.com>
2023-06-30 10:37:14 -0400

    BACKLOG-21257: Use same resolution for 404 in ContentRoute as PageComposerRoute (#878)
    * BACKLOG-21257: Use same resolution for 404 in ContentRoute as PageComposerRoute
    
    * BACKLOG-21257: Display route while loading node check

--- src/javascript/JContent/ContentRoute/ContentRoute.jsx
+++ src/javascript/JContent/ContentRoute/ContentRoute.jsx
@@ -2,16 +2,36 @@ import React from 'react';
 import ContentLayout from './ContentLayout';
 import MainLayout from '../MainLayout';
 import ContentHeader from '../ContentHeader';
-import {ErrorBoundary, LoaderSuspense} from '@jahia/jahia-ui-root';
+import {ErrorBoundary, Error404, LoaderSuspense} from '@jahia/jahia-ui-root';
+import {useNodeInfo} from '@jahia/data-helper';
+import {useSelector} from 'react-redux';
+import {useTranslation} from 'react-i18next';
 
-export const ContentRoute = () => (
-    <MainLayout header={<ContentHeader/>}>
-        <LoaderSuspense>
-            <ErrorBoundary>
-                <ContentLayout/>
-            </ErrorBoundary>
-        </LoaderSuspense>
-    </MainLayout>
-);
+export const ContentRoute = () => {
+    const {t} = useTranslation('jcontent');
+    const {path, mode} = useSelector(state => ({
+        path: state.jcontent.path,
+        mode: state.jcontent.mode
+    }));
+    const res = useNodeInfo({path});
+
+    if (!res.loading && (res.node === undefined || res.error)) {
+        if (mode === 'pages') {
+            return <Error404/>;
+        }
+
+        return <Error404 label={t('jcontent:label.contentManager.error.missingFolder')}/>;
+    }
+
+    return (
+        <MainLayout header={<ContentHeader/>}>
+            <LoaderSuspense>
+                <ErrorBoundary>
+                    <ContentLayout/>
+                </ErrorBoundary>
+            </LoaderSuspense>
+        </MainLayout>
+    );
+};
 
 export default ContentRoute;

fa0b38893ad4c5554bbf33441fcf05dcee82be48
Alex Karmanov <alexandrk_@hotmail.com>
2023-08-09 09:41:31 -0400

    BACKLOG-20282 Create content folder test (#931)
    * BACKLOG-20282 Add content folder test
    
    * BACKLOG-20282 Add content folder test

---
+++ tests/cypress/e2e/jcontent/contentFolder.cy.ts
@@ -0,0 +1,24 @@
+import {JContent, JContentPageBuilder} from '../../page-object';
+
+describe('Content folder', () => {
+    let jcontent: JContentPageBuilder;
+
+    before(() => {
+        cy.login();
+    });
+
+    after(() => {
+        cy.logout();
+    });
+
+    beforeEach(() => {
+        jcontent = JContent
+            .visit('digitall', 'en', 'pages/home')
+            .switchToPageBuilder();
+    });
+
+    it('Open content folders correctly', () => {
+        jcontent.getAccordionItem('content-folders').click();
+        cy.get('span').contains('Content Folder').should('exist');
+    });
+});

e47d4bc108f4c2f30b2b55ffaf65e289733080ef
Thomas Draier <tdraier@jahia.com>
2023-08-21 13:07:29 +0200

    test

--- tests/cypress/e2e/menuActions/actionBars.cy.ts
+++ tests/cypress/e2e/menuActions/actionBars.cy.ts
@@ -3,13 +3,13 @@ import {JContent} from '../../page-object/jcontent';
 describe('test jcontent actionbar', () => {
     it('test actionbar', () => {
         cy.login();
-        JContent.visit('digitall', 'en', 'pages/home/about');
+        const jcontent = JContent.visit('digitall', 'en', 'pages/home/about');
         cy.get('div[role="toolbar"]').find('[data-sel-role="refresh"]').should('exist');
-        JContent.visit('digitall', 'en', 'content-folders/contents');
+        jcontent.selectAccordion('content-folders');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="createContentFolder"]').should('exist');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="createContent"]').should('exist');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="refresh"]').should('exist');
-        JContent.visit('digitall', 'en', 'media/files');
+        jcontent.selectAccordion('media');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="createFolder"]').should('exist');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="fileUpload"]').should('exist');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="refresh"]').should('exist');

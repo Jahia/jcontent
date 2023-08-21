28280d4599c09081bb98b2801f0ae7facb2cd576
Thomas Draier <tdraier@jahia.com>
2023-08-21 15:19:40 +0200

    force viewMode switch

--- src/javascript/JContent/ContentRoute/ContentRoute.jsx
+++ src/javascript/JContent/ContentRoute/ContentRoute.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ContentLayout from './ContentLayout';
 import MainLayout from '../MainLayout';
 import ContentHeader from '../ContentHeader';
 import {LoaderOverlay, ErrorBoundary, Error404, LoaderSuspense} from '@jahia/jahia-ui-root';
 import {useNodeInfo} from '@jahia/data-helper';
-import {shallowEqual, useSelector} from 'react-redux';
+import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import {useTranslation} from 'react-i18next';
 import JContentConstants from '~/JContent/JContent.constants';
 import {EditFrame} from '../EditFrame';
+import {registry} from '@jahia/ui-extender';
+import {setTableViewMode} from '~/JContent/redux/JContent.redux';
 
 export const ContentRoute = () => {
     const {t} = useTranslation('jcontent');
@@ -16,9 +18,16 @@ export const ContentRoute = () => {
         mode: state.jcontent.mode,
         viewMode: state.jcontent.tableView.viewMode
     }), shallowEqual);
-
+    const dispatch = useDispatch();
     const nodeTypes = ['jnt:page', 'jmix:mainResource'];
     const res = useNodeInfo({path}, {getIsNodeTypes: nodeTypes});
+
+    useEffect(() => {
+        const accordionItem = registry.get('accordionItem', mode);
+        if (accordionItem.tableConfig?.availableModes?.indexOf?.(viewMode) === -1) {
+            dispatch(setTableViewMode(accordionItem.tableConfig.defaultViewMode || 'flatList'));
+        }
+    }, [dispatch, mode, viewMode]);
 
     if (res.loading) {
         return <LoaderOverlay/>;

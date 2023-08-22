222a5c638559aee1df84a9b05bffd79c9b789f48
Cedric Mailleux <cmailleux@jahia.com>
2023-08-22 11:05:01 -0400

    BACKLOG-21316: Fix click handling

--- src/javascript/JContent/EditFrame/Breadcrumbs/Breadcrumbs.jsx
+++ src/javascript/JContent/EditFrame/Breadcrumbs/Breadcrumbs.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Breadcrumb, BreadcrumbItem, Dropdown} from '@jahia/moonstone';
 import {useTranslation} from 'react-i18next';
-import {cmAddSelection} from '../../redux/selection.redux';
-import {useDispatch} from 'react-redux';
+import {cmAddSelection, cmClearSelection, cmRemoveSelection} from '../../redux/selection.redux';
+import {shallowEqual, useDispatch, useSelector} from 'react-redux';
+import {batchActions} from "redux-batched-actions";
+
+const handleItemOnClick = (selection, n, dispatch) => {
+    return event => {
+        event.preventDefault();
+        event.stopPropagation();
+        // Meta key works without issues, ctrl key has a conflict with contextmenu
+        if (event.ctrlKey || event.metaKey) {
+            const isSelected = selection.includes(n.path);
+            if (isSelected) {
+                dispatch(cmRemoveSelection(n.path));
+            } else {
+                dispatch(cmAddSelection(n.path));
+            }
+        } else {
+            dispatch(batchActions([cmClearSelection(), cmAddSelection(n.path)]));
+        }
+
+        return false;
+    };
+}
 
 export const Breadcrumbs = ({nodes, responsiveMode}) => {
     const {t} = useTranslation('jcontent');
     const dispatch = useDispatch();
-
+    const {selection} = useSelector(state => ({
+        selection: state.jcontent.selection
+    }), shallowEqual);
     if (responsiveMode) {
         const data = nodes.map(n => ({
             label: n.name,
@@ -33,17 +56,9 @@ export const Breadcrumbs = ({nodes, responsiveMode}) => {
             {nodes.map(n => (
                 <BreadcrumbItem key={n.path}
                                 label={n.name}
-                                icon={<img alt={n.name} src={`${window.contextJsParameters.contextPath}${n.primaryNodeType.icon}.png`}/>}
-                                onClick={event => {
-                                    event.preventDefault();
-                                    event.stopPropagation();
-                                    // Meta key works without issues, ctrl key has a conflict with contextmenu
-                                    if (event.ctrlKey || event.metaKey) {
-                                        dispatch(cmAddSelection(n.path));
-                                    }
-
-                                    return false;
-                                }}
+                                icon={<img alt={n.name}
+                                           src={`${window.contextJsParameters.contextPath}${n.primaryNodeType.icon}.png`}/>}
+                                onClick={handleItemOnClick(selection, n, dispatch)}
                 />
             ))}
         </Breadcrumb>

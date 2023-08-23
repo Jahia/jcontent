2e1bf8e3312895d566a397a0651a664c2a9510f1
Cedric Mailleux <cmailleux@jahia.com>
2023-08-23 13:59:46 -0400

    BACKLOG-21400: Fixing search test by ensuring order by name

--- tests/cypress/e2e/jcontent/search.cy.ts
+++ tests/cypress/e2e/jcontent/search.cy.ts
@@ -113,7 +113,7 @@ describe('Search tests', () => {
         });
     });
 
-    describe('advanced search', {testIsolation: false}, () => {
+    describe.only('advanced search', {testIsolation: false}, () => {
         before(() => {
             cy.loginAndStoreSession();
             jcontent = JContent.visit('jcontentSite', 'en', 'content-folders/contents');
@@ -124,6 +124,7 @@ describe('Search tests', () => {
             basicSearch = jcontent.getBasicSearch().openSearch().switchToAdvanced()
                 .searchFrom('jnt:event')
                 .executeSearch()
+                .sortBy('name')
                 .verifyResults(['test-content5', 'test-content4'])
                 .verifyResultType('Event');
         });

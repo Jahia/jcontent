2fca3fe48b5f0b1de5ac2ab5fca36f3ece24e5a3
Thomas Draier <tdraier@jahia.com>
2023-09-13 09:47:49 +0200

    add test

---
+++ tests/cypress/e2e/contentEditor/richText.cy.ts
@@ -0,0 +1,35 @@
+import {JContent} from '../../page-object';
+import {addNode} from '@jahia/cypress';
+import {MultipleLeftRightField} from '../../page-object/fields/multipleLeftRightField';
+import {RichTextField} from "../../page-object/fields";
+
+describe('constraints', {testIsolation: false}, () => {
+    let jcontent: JContent;
+    let contentEditor;
+    let richText;
+
+    before(() => {
+        cy.window().then(win => {
+            win.navigator.clipboard.writeText('test');
+        });
+        cy.loginAndStoreSession();
+        jcontent = JContent
+            .visit('digitall', 'en', 'pages/home')
+            .switchToListMode();
+        contentEditor = jcontent.editComponentByText('Digitall’s global network');
+    });
+
+    beforeEach(() => {
+        richText = contentEditor.getField(RichTextField, 'jnt:bigText_text');
+    });
+
+    it('can edit rich text with ckeditor', function () {
+        richText.type('test');
+    });
+
+    it('shows ckeditor notifications', function () {
+        richText.get().find('.cke_button__paste_icon').click();
+        cy.get('.cke_notifications_area');
+    });
+});
+

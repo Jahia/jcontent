52f55ea77018ed238734a53c9728f629c3636649
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2023-09-22 13:22:18 -0400

    BACKLOG-21532: Fix restrictions for subfolders (#1025)
    * BACKLOG-21532: Fix restricted content for subfolders
    
    * Add cypress tests
    
    * Fix lint issues

--- tests/cypress/e2e/contentEditor/constraints.cy.ts
+++ tests/cypress/e2e/contentEditor/constraints.cy.ts
@@ -1,4 +1,4 @@
-import {JContent} from '../../page-object';
+import {ContentEditor, JContent} from '../../page-object';
 import {addNode} from '@jahia/cypress';
 import {MultipleLeftRightField} from '../../page-object/fields/multipleLeftRightField';
 
@@ -7,7 +7,17 @@ describe('constraints', () => {
 
     before(() => {
         cy.executeGroovy('jcontent/createSite.groovy', {SITEKEY: 'jcontentSite'});
-        addNode({parentPathOrId: '/sites/jcontentSite/home', primaryNodeType: 'jnt:contentList', name: 'list'});
+        addNode({
+            name: 'list',
+            parentPathOrId: '/sites/jcontentSite/home',
+            primaryNodeType: 'jnt:contentList'
+        });
+        addNode({
+            name: 'folder1',
+            parentPathOrId: '/sites/jcontentSite/contents',
+            primaryNodeType: 'jnt:contentFolder',
+            children: [{name: 'subfolder1', primaryNodeType: 'jnt:contentFolder'}]
+        });
     });
 
     after(() => {
@@ -17,12 +27,12 @@ describe('constraints', () => {
 
     beforeEach(() => {
         cy.loginAndStoreSession();
+    });
+
+    it('can set restrictions on content list', () => {
         jcontent = JContent
             .visit('jcontentSite', 'en', 'pages/home')
             .switchToStructuredView();
-    });
-
-    it('can set restrictions', function () {
         const contentEditor = jcontent.editComponentByText('list');
         contentEditor.closeSection('Content');
         contentEditor.toggleOption('jmix:contributeMode', 'Content type restrictions');
@@ -30,6 +40,66 @@ describe('constraints', () => {
             .addNewValue('Banner')
             .addNewValue('Breadcrumb');
         contentEditor.save();
+    });
+
+    it('can set restrictions on content folder', () => {
+        jcontent = JContent
+            .visit('jcontentSite', 'en', 'content-folders/contents')
+            .switchToListMode();
+        const contentEditor = jcontent.editComponentByText('folder1');
+        contentEditor.closeSection('Content');
+        contentEditor.toggleOption('jmix:contributeMode', 'Content type restrictions');
+        contentEditor
+            .getField(MultipleLeftRightField, 'jmix:contributeMode_j:contributeTypes', true)
+            .addNewValue('Banner');
+        contentEditor.save();
+    });
+
+    it('can create restricted content', () => {
+        jcontent = JContent
+            .visit('jcontentSite', 'en', 'content-folders/contents/folder1');
+        // Verify create highlight button is present and create content
+        cy.get('[data-registry-key="action:createContent"][data-sel-role="jnt:banner"]').click();
+        new ContentEditor().create();
+
+        // Verify content created
+        jcontent.getTable().getRowByLabel('banner').should('be.visible');
+    });
+
+    it('can inherit restricted content to subfolders', () => {
+        jcontent = JContent
+            .visit('jcontentSite', 'en', 'content-folders/contents/folder1/subfolder1');
+        // Verify create highlight button is present and create content
+        cy.get('[data-registry-key="action:createContent"][data-sel-role="jnt:banner"]').click();
+        new ContentEditor().create();
+
+        // Verify content created
+        jcontent.getTable().getRowByLabel('banner').should('be.visible');
+    });
+
+    it('can add to current restrictions', () => {
+        jcontent = JContent
+            .visit('jcontentSite', 'en', 'content-folders/contents')
+            .switchToListMode();
+        const contentEditor = jcontent.editComponentByText('folder1');
+        contentEditor.closeSection('Content');
+        contentEditor
+            .getField(MultipleLeftRightField, 'jmix:contributeMode_j:contributeTypes', true)
+            .addNewValue('Simple text');
+        contentEditor.save();
+    });
+
+    it('can create restricted content', () => {
+        jcontent = JContent
+            .visit('jcontentSite', 'en', 'content-folders/contents/folder1');
+        // Verify create highlight button is still present
+        cy.get('[data-registry-key="action:createContent"][data-sel-role="jnt:banner"]').should('be.visible');
+        // Verify new text restriction is present and create content
+        cy.get('[data-registry-key="action:createContent"][data-sel-role="jnt:text"]').click();
+        new ContentEditor().create();
+
+        // Verify content created
+        jcontent.getTable().getRowByLabel('simple-text').should('be.visible');
     });
 });
 

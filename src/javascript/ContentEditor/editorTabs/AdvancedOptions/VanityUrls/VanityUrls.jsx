4adf576574d2114224639820434578b6584992ea
jahia-carp <carp@jahia.com>
2023-10-09 18:08:26 +0200

    QA-14983: Handle Vanity tab label based on permission

--- src/javascript/ContentEditor/editorTabs/AdvancedOptions/VanityUrls/VanityUrls.jsx
+++ src/javascript/ContentEditor/editorTabs/AdvancedOptions/VanityUrls/VanityUrls.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useTranslation} from 'react-i18next';
 import styles from './VanityUrls.scss';
 import {getButtonRenderer} from '~/ContentEditor/utils';
 import {DisplayAction} from '@jahia/ui-extender';
 import {Typography} from '@jahia/moonstone';
 import clsx from 'clsx';
+import {useNodeChecks} from '@jahia/data-helper';
+import {useContentEditorContext} from '~/ContentEditor/contexts';
 
 const ButtonRendererAccent = getButtonRenderer({defaultButtonProps: {variant: 'default', size: 'big', color: 'accent'}});
 
 export const VanityUrls = () => {
     const {t} = useTranslation('jcontent');
+
+    const {path} = useContentEditorContext();
+
+    const res  = useNodeChecks(
+        {path},
+        {
+            requiredPermission: ['viewVanityUrlModal']
+        }
+    );
+
+    const hasPermission = res.checksResult;
 
     return (
         <section className={styles.container}>
@@ -17,7 +30,9 @@ export const VanityUrls = () => {
                 {t('jcontent:label.contentEditor.vanityTab.title')}
             </Typography>
             <Typography className={styles.item}>
-                {t('jcontent:label.contentEditor.vanityTab.label')}
+                {hasPermission
+                    ? t('jcontent:label.contentEditor.vanityTab.label')
+                    : t('jcontent:label.contentEditor.vanityTab.noPermissionLabel')}
             </Typography>
             <div className={styles.item}>
                 <DisplayAction actionKey="vanityUrls"

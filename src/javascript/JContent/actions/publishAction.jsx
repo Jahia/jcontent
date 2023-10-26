3473bbdfd80dce343655859516a2bf92100e009f
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2023-10-26 17:16:36 -0400

    QA-14986: Use network-only to refresh cache (#1058)

--- src/javascript/JContent/actions/publishAction.jsx
+++ src/javascript/JContent/actions/publishAction.jsx
@@ -89,7 +89,7 @@ export const PublishActionComponent = props => {
     const {t} = useTranslation('jcontent');
 
     // Publication info needs to be refreshed in case subNodes have changed
-    const queryOptions = (publishType === 'publish') ? {fetchPolicy: 'cache-and-network'} : undefined;
+    const queryOptions = (publishType === 'publish') ? {fetchPolicy: 'network-only'} : undefined;
     const res = useNodeChecks({path, paths, language: languageToUse}, {
         getDisplayName: true,
         getProperties: ['jcr:mixinTypes'],

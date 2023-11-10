7a1af8fcffb16dafdeb25e5c47ba86bf6fa7c229
Thomas Draier <tdraier@jahia.com>
2023-11-10 17:16:12 +0100

    log

--- tests/assets/setLog.groovy
+++ tests/assets/setLog.groovy
@@ -1,1 +1,1 @@
-org.jahia.bin.listeners.LoggingConfigListener.setLoggerLevel("org.jahia.bin.errors.ErrorLoggingFilter", "info");
+org.jahia.bin.listeners.LoggingConfigListener.setLoggerLevel("org.jahia.bin.errors", "info");

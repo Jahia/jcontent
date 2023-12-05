c923f9e75144f644f2d618dcffbcf2e347135e57
Geofrey Flores <gflores@jahia.com>
2023-12-05 16:58:32 -0500

    Refactor override tests

--- tests/cypress/e2e/contentEditor/overrideTests.cy.ts
+++ tests/cypress/e2e/contentEditor/overrideTests.cy.ts
@@ -1,48 +1,38 @@
 import {JContent} from '../../page-object';
+import {createSite, deleteSite, enableModule} from "@jahia/cypress";
 
 describe('overrideTests', {testIsolation: false}, () => {
-    let jcontent: JContent;
+    const siteKey = 'contentEditorSite';
+    const moduleName = 'jcontent-test-module';
 
     before(() => {
-        cy.executeGroovy('contentEditor/createSite.groovy', {SITEKEY: 'contentEditorSite'});
-        cy.apollo({mutationFile: 'contentEditor/references.graphql'});
-
-        const fileName = 'contentEditor/overrideTests/modules/jcontent-helper-no-bigText.jar';
-        // Need to wait explicitly for the bundle listener to process event
-        // eslint-disable-next-line
-        cy.runProvisioningScript({ fileContent: '- installAndStartBundle: "' + fileName + '"', type: 'application/yaml' },
-            [{fileName: fileName, type: 'application/java-archive'}]
-        )
-            .then(res => {
-                expect(res.length).to.equal(1);
-                expect(res[0].start[0].message).to.equal('Operation successful');
-            })
-            .wait(3000);
-
+        createSite(siteKey);
+        cy.apollo({mutationFile: 'contentEditor/overrideTests/references.graphql'});
         cy.loginAndStoreSession();
     });
 
     after(() => {
-        cy.executeGroovy('contentEditor/deleteSite.groovy', {SITEKEY: 'contentEditorSite'});
+        deleteSite(siteKey);
     });
 
-    it('Can show and hide advancedMode button with json override', function () {
-        // Deploy jcontent-helper on site
-        cy.apollo({
-            mutationFile: 'contentEditor/overrideTests/deployModule.graphql'
-        });
-
-        jcontent = JContent.visit('contentEditorSite', 'en', 'content-folders/contents');
-        jcontent.getTable().getRowByLabel('Rich text').contextMenu().select('Edit');
+    it('can hide advancedMode button with json override', () => {
+        enableModule(moduleName, siteKey);
+        const jcontent = JContent.visit('contentEditorSite', 'en', 'content-folders/contents');
+        jcontent.editComponentByText('test-content1');
         cy.get('button[data-sel-role="advancedMode"]').should('not.exist');
+    });
 
+    it('can restore advanced mode button when module is undeployed', () => {
         cy.apollo({
-            mutationFile: 'contentEditor/overrideTests/undeployModule.graphql'
+            mutationFile: 'contentEditor/overrideTests/undeployModule.graphql',
+            variables: {moduleName, pathOrId: `/sites/${siteKey}`}
         });
-
-        jcontent = JContent.visit('contentEditorSite', 'en', 'content-folders/contents');
-        jcontent.getTable().getRowByLabel('Rich text').contextMenu().select('Edit');
+        const jcontent = JContent.visit('contentEditorSite', 'en', 'content-folders/contents');
+        jcontent.editComponentByText('test-content1');
         cy.get('button[data-sel-role="advancedMode"]').should('exist');
     });
+
+
+
 });
 

cdd34067a5c49c7a97a535b1b0c882f26cd7f377
Geofrey Flores <gflores@jahia.com>
2023-12-12 14:04:49 -0500

    BACKLOG-21892: Change label for language selector

--- src/javascript/JContent/ContentNavigation/NavigationHeader/SwitchersLayout/SwitchersLayout.jsx
+++ src/javascript/JContent/ContentNavigation/NavigationHeader/SwitchersLayout/SwitchersLayout.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import SiteSwitcher from './SiteSwitcher';
 import LanguageSwitcher from './LanguageSwitcher';
 import styles from './SwitchersLayout.scss';
-import {Separator} from '@jahia/moonstone';
 import PropTypes from 'prop-types';
 
 const SwitchersLayout = ({isDisplaySiteSwitcher, isDisplayLanguageSwitcher, languageSelector, setLanguageAction}) => {
     return (
         <div className={styles.root}>
             {isDisplaySiteSwitcher && <SiteSwitcher/>}
-            {isDisplaySiteSwitcher && isDisplayLanguageSwitcher && <Separator variant="vertical"/>}
-            {isDisplayLanguageSwitcher && <LanguageSwitcher setLanguageAction={setLanguageAction} selector={languageSelector}/>}
+            {isDisplayLanguageSwitcher && (
+                <LanguageSwitcher
+                    setLanguageAction={setLanguageAction}
+                    selector={languageSelector}
+                    isFullDropdown={!isDisplaySiteSwitcher}
+                />)}
         </div>
     );
 };

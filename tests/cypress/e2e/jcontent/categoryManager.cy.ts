7c2ba69f441eb8838e0f47e6b8ec0790a3783c3e
Geofrey Flores <gflores@jahia.com>
2023-12-18 16:18:19 -0500

    BACKLOG-21345: Add category manager tests

--- tests/cypress/e2e/jcontent/categoryManager.cy.ts
+++ tests/cypress/e2e/jcontent/categoryManager.cy.ts
@@ -1,44 +1,69 @@
 import {CategoryManager} from '../../page-object';
-const accordionItemName = 'category';
+import {deleteNode} from "@jahia/cypress";
+
 describe('Category Manager', () => {
     let categoryManager: CategoryManager;
 
-    before(() => {
-        cy.apollo({mutationFile: 'jcontent/createCategories.graphql'});
-    });
-
     after(() => {
+        deleteNode('/sites/systemsite/categories/rootTestCategory');
         cy.logout();
     });
 
     beforeEach(() => {
         cy.loginAndStoreSession();
+    });
+
+    it('can create and edit a new category', () => {
         categoryManager = CategoryManager.visitCategoryManager('en');
-    });
-
-    it('Should open Category Manager', () => {
         categoryManager.getSecondaryNav().get().contains('Category Manager').should('be.visible');
+
+        categoryManager.createCategoryNav('categories', {title: 'My Main Category', name: 'rootTestCategory'});
+        categoryManager.getTreeItem('rootTestCategory').get().contains('My Main Category');
+
+        categoryManager.editCategoryNav('rootTestCategory', {title: 'Root Test Category'});
+        categoryManager.getTreeItem('rootTestCategory').get().contains('Root Test Category');
     });
 
-    it('Create a new category', () => {
-        categoryManager.getCreateCategory();
-        cy.contains('Create category').should('be.visible');
-    });
+    it('create subcategories and navigate to it', () => {
+        categoryManager = CategoryManager.visitCategoryManager('en');
+        categoryManager.createCategoryNav('rootTestCategory', {title: 'test-category1'});
+        categoryManager.createCategoryNav('rootTestCategory', {title: 'test-category2'});
+        categoryManager.createCategoryNav('rootTestCategory', {title: 'test-category3'});
 
-    it('Navigates to sub category Category 2', () => {
-        const accordionItem = categoryManager.getAccordionItem(accordionItemName);
-        accordionItem.getTreeItem('rootTestCategory').click({multiple: true});
-        cy.contains('Test Category 1').should('be.visible');
+        const accordionItem = categoryManager.getAccordionItem();
         accordionItem.expandTreeItem('rootTestCategory');
+        accordionItem.getTreeItem('test-category1').get().should('be.visible');
+        accordionItem.getTreeItem('test-category2').get().should('be.visible');
+        accordionItem.getTreeItem('test-category3').get().should('be.visible');
+
+        categoryManager.createCategoryNav('test-category1', {title: 'test-subcategory1'});
+        categoryManager.createCategoryNav('test-category2', {title: 'test-subcategory2'});
+        categoryManager.createCategoryNav('test-category3', {title: 'test-subcategory3'});
+
         accordionItem.expandTreeItem('test-category1');
-        accordionItem.getTreeItem('test-category1').click({multiple: true});
-        cy.contains('Test Category 2').should('be.visible');
+        cy.contains('test-subcategory1').should('be.visible');
+        accordionItem.expandTreeItem('test-category2');
+        cy.contains('test-subcategory2').should('be.visible');
+        accordionItem.expandTreeItem('test-category3');
+        cy.contains('test-subcategory3').should('be.visible');
+    });
+
+    it('Test copy/cut/paste', () => {
+        categoryManager = CategoryManager.visitCategoryManager('en');
+        const accordionItem = categoryManager.getAccordionItem();
+        accordionItem.expandTreeItem('rootTestCategory');
+        accordionItem.getTreeItem('test-category2').contextMenu().select('Copy');
+        accordionItem.getTreeItem('test-category3').contextMenu().select('Paste');
+
+        // Visit and make sure copied category path exists
+        categoryManager = CategoryManager.visitCategoryManager('en', 'rootTestCategory/test-category3/test-category2/test-subcategory2');
+        categoryManager.getTreeItem('test-subcategory2').should('be.visible');
     });
 
     it('Contains only expected actions in primary header action', () => {
-        const accordionItem = categoryManager.getAccordionItem(accordionItemName);
-        accordionItem.getTreeItem('rootTestCategory').click({multiple: true});
-        cy.contains('Test Category 1').should('be.visible');
+        categoryManager = CategoryManager.visitCategoryManager('en');
+        categoryManager.getTreeItem('rootTestCategory').click({multiple: true});
+        cy.contains('test-category1').should('be.visible');
         const primaryActions = ['New category', 'Edit', 'Import content', 'Refresh'];
         cy.get('.moonstone-header').children('.moonstone-header_toolbar').children('.moonstone-header_actions').find('.moonstone-button').should('have.length', primaryActions.length + 1).and(elems => {
             primaryActions.forEach(action => expect(elems).to.contain(action));
@@ -46,21 +71,25 @@ describe('Category Manager', () => {
     });
 
     it('Performs a simple search at the root level', () => {
+        categoryManager = CategoryManager.visitCategoryManager('en');
         const basicSearch = categoryManager.getBasicSearch().openSearch().reset(true);
-        basicSearch.searchTerm('Test Category').executeSearch().verifyResults(['Root Test Category', 'Test Category 1', 'Test Category 2']).verifyTotalCount(3);
+        basicSearch.searchTerm('subcategory').executeSearch()
+            .verifyResults(['test-subcategory1', 'test-subcategory2', 'test-subcategory2', 'test-subcategory3'])
+            .verifyTotalCount(4);
     });
 
     it('Performs a simple search at the specified level', () => {
-        const accordionItem = categoryManager.getAccordionItem(accordionItemName).click();
-        accordionItem.getTreeItem('rootTestCategory').click({multiple: true});
-        cy.contains('Test Category 1').should('be.visible');
+        categoryManager = CategoryManager.visitCategoryManager('en', 'rootTestCategory/test-category3');
+        cy.contains('test-category3').should('be.visible');
         const basicSearch = categoryManager.getBasicSearch().openSearch().reset(true);
-        basicSearch.searchTerm('Test Category').executeSearch().verifyResults(['Test Category 1', 'Test Category 2']).verifyTotalCount(2);
+        basicSearch.searchTerm('subcategory').executeSearch()
+            .verifyResults(['test-subcategory2', 'test-subcategory3'])
+            .verifyTotalCount(2);
     });
 
     it('Shows usages for sub categories when deleting Companies category', () => {
-        const accordionItem = categoryManager.getAccordionItem(accordionItemName);
-        accordionItem.getTreeItem('categories').click({multiple: true});
+        categoryManager = CategoryManager.visitCategoryManager('en');
+        categoryManager.getAccordionItem().getTreeItem('categories').click({multiple: true});
         categoryManager.getTable().getRowByLabel('Companies')
             .contextMenu()
             .select('Delete');

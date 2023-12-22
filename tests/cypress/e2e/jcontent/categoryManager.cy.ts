7d58d01d375fbc0eddee3c3d6886b81a2552a6ce
Geofrey Flores <gflores@jahia.com>
2023-12-22 07:46:08 -0500

    Refactor import/export tests

--- tests/cypress/e2e/jcontent/categoryManager.cy.ts
+++ tests/cypress/e2e/jcontent/categoryManager.cy.ts
@@ -1,7 +1,8 @@
 import {CategoryManager} from '../../page-object';
-import {deleteNode} from '@jahia/cypress';
+import {BaseComponent, Button, deleteNode, Dropdown, getComponentByAttr} from '@jahia/cypress';
+import * as path from 'path';
 
-describe('Category Manager', () => {
+describe('Category Manager', {defaultCommandTimeout: 10000}, () => {
     let categoryManager: CategoryManager;
 
     before(() => {
@@ -21,6 +22,10 @@ describe('Category Manager', () => {
 
     // Root categories has depth=0
     function categoryExists(role: string, depth: number) {
+        // Workaround to get tree item for a given depth;
+        // the actual style text depends on chrome version
+        // - v107 has "--tree-depth:2" while v120 has "--tree-depth: 2" (with space)
+        // account for both scenarios (to be fixed in moonstone)
         cy.get(`[data-sel-role="${role}"][style*="depth: ${depth}"],[data-sel-role="${role}"][style*="depth:${depth}"]`)
             .should('be.visible');
     }
@@ -73,11 +78,6 @@ describe('Category Manager', () => {
 
         // Get the category pasted under test-category3
         accordionItem.expandTreeItem('test-category3');
-
-        // Workaround to get tree item for a given depth;
-        // the actual style text depends on chrome version
-        // - v107 has "--tree-depth:2" while v120 has "--tree-depth: 2" (with space)
-        // account for both scenarios (to be fixed in moonstone)
         categoryExists('test-category2', 2);
     });
 
@@ -114,9 +114,7 @@ describe('Category Manager', () => {
     it('Shows usages for sub categories when deleting Companies category', () => {
         categoryManager = CategoryManager.visitCategoryManager('en');
         categoryManager.getAccordionItem().getTreeItem('categories').click({multiple: true});
-        categoryManager.getTable().getRowByLabel('Companies')
-            .contextMenu()
-            .select('Delete');
+        categoryManager.getTable().getRowByLabel('Companies').contextMenu().select('Delete');
 
         const dialogCss = '[data-sel-role="delete-permanently-dialog"]';
         cy.get(dialogCss).as('deleteDialog');
@@ -130,24 +128,63 @@ describe('Category Manager', () => {
         });
     });
 
-    it('can import xml and zip exports', () => {
+    it('can import/export categories - zip', () => {
         categoryManager = CategoryManager.visitCategoryManager('en');
-        const fixturesDir = 'cypress/fixtures/categoryManager';
+        const downloadsFolder = Cypress.config('downloadsFolder');
+        cy.exec(`mkdir -p ${downloadsFolder}`, {failOnNonZeroExit: false});
+
+        // Export zip
+        categoryManager.getTreeItem('companies').contextMenu().select('Export');
+        const dialog = getComponentByAttr(BaseComponent, 'data-cm-role', 'export-options');
+        dialog.should('be.visible');
+        getComponentByAttr(Dropdown, 'data-cm-role', 'select-workspace', dialog).select('Staging and live content');
+        dialog.get().find('[data-cm-role="export-as-xml"] input[type="checkbox"]').should('be.disabled');
+        getComponentByAttr(Button, 'data-cm-role', 'export-button').click();
+        dialog.should('not.exist');
+
+        cy.waitUntil(() => cy.exec(`ls ${downloadsFolder}`).then(result => {
+            console.log(result.stdout);
+            return result.stdout.includes('companies.zip');
+        }), {timeout: 30000, interval: 1000, errorMsg: 'Unable to download companies.zip'});
+
+        // Import zip
+        categoryManager.getTreeItem('import-zip').contextMenu().select('Import content');
+        cy.get('#file-upload-input').selectFile(path.join(downloadsFolder, 'companies.zip'), {force: true});
+        categoryManager.getTreeItem('import-zip').get()
+            .find('.moonstone-treeView_itemToggle svg').should('be.visible');
+        categoryManager.getTreeItem('import-zip').expand();
+        categoryExists('companies', 1);
+    });
+
+    it('can import/export categories - xml', () => {
+        categoryManager = CategoryManager.visitCategoryManager('en');
+        const downloadsFolder = Cypress.config('downloadsFolder');
+
+        // Export xml
+        categoryManager.getTreeItem('companies').contextMenu().select('Export');
+        const dialog = getComponentByAttr(BaseComponent, 'data-cm-role', 'export-options');
+        dialog.should('be.visible');
+        getComponentByAttr(Dropdown, 'data-cm-role', 'select-workspace', dialog).select('Staging content only');
+        dialog.get().find('[data-cm-role="export-as-xml"] input[type="checkbox"]').should('not.be.disabled').check();
+        getComponentByAttr(Button, 'data-cm-role', 'export-button').click();
+        dialog.should('not.exist');
+
+        cy.waitUntil(() => cy.exec(`ls ${downloadsFolder}`).then(result => {
+            console.log(result.stdout);
+            return result.stdout.includes('companies.xml');
+        }), {timeout: 30000, interval: 1000, errorMsg: 'Unable to download companies.xml'});
 
         // Import xml
         categoryManager.getTreeItem('import-xml').contextMenu().select('Import content');
         cy.get('#file-upload-input').selectFile({
-            contents: `${fixturesDir}/sample-categories.xml`,
+            contents: path.join(downloadsFolder, 'companies.xml'),
             mimeType: 'text/xml' // Need to override default mimeType application/xml
         }, {force: true});
-        categoryManager.getTreeItem('import-xml').expand();
-        categoryExists('rootTestCategory', 1);
-        categoryManager.getTreeItem('import-xml').collapse();
 
-        // Import zip
-        categoryManager.getTreeItem('import-zip').contextMenu().select('Import content');
-        cy.get('#file-upload-input').selectFile(`${fixturesDir}/sample-categories.zip`, {force: true});
-        categoryManager.getTreeItem('import-zip').expand();
-        categoryExists('rootTestCategory', 1);
+        categoryManager.getTreeItem('import-xml').get()
+            .find('.moonstone-treeView_itemToggle svg').should('be.visible');
+        categoryManager.getTreeItem('import-xml').expand();
+        categoryExists('companies', 1);
+        categoryManager.getTreeItem('import-xml').collapse();
     });
 });

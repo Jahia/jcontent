091df2b80894362aad1ca6432bbc819eea8d2237
Francois G <fgerthoffert@jahia.com>
2024-01-15 16:25:33 +0100

    Trying out to set videos folder in the config itself

--- tests/cypress.config-categoryManager.ts
+++ tests/cypress.config-categoryManager.ts
@@ -2,5 +2,6 @@ import {defineConfig} from 'cypress';
 import {baseConfig} from './cypress.config.common';
 
 baseConfig.e2e.specPattern = ['cypress/e2e/jcontent/categoryManager.cy.ts'];
+baseConfig.videosFolder = './results/videos';
 
 export default defineConfig(baseConfig);

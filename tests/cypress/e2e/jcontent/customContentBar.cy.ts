db5307c4cbda151a193ad3735605e0a53b32e1fa
jsinovassin <jsinovassinnaik@jahia.com>
2024-01-24 11:12:47 +0100

    cypress tests

--- tests/cypress/e2e/jcontent/customContentBar.cy.ts
+++ tests/cypress/e2e/jcontent/customContentBar.cy.ts
@@ -16,12 +16,6 @@ describe('Custom content bar test', () => {
             children: [{
                 name: 'area-main', primaryNodeType: 'jnt:contentList'
             }]
-        }).then(() => {
-            addNode({
-                parentPathOrId: `/sites/digitall/home/${pageName}/area-main`,
-                name: 'default-value-test',
-                primaryNodeType: 'cent:defaultValueTest'
-            });
         });
     });
 
@@ -29,14 +23,38 @@ describe('Custom content bar test', () => {
         deleteNode(`/sites/digitall/home/${pageName}`);
     });
 
-    it('Check if custom bar is always displayed in page builder', () => {
-        const jcontentPageBuilder = JContent
-            .visit('digitall', 'en', `pages/home/${pageName}`)
-            .switchToPageBuilder();
-        const pageBuilderModuleHeader = jcontentPageBuilder
-            .getModule(`/sites/digitall/home/${pageName}/area-main/default-value-test`)
-            .getHeader();
-        pageBuilderModuleHeader.get().should('exist');
-        pageBuilderModuleHeader.get().find('div').contains('This is a custom bar').should('exist');
+    it('Check if custom bar is always displayed in page builder for cent:defaultValueTest', () => {
+        addNode({
+            parentPathOrId: `/sites/digitall/home/${pageName}/area-main`,
+            name: 'default-value-test',
+            primaryNodeType: 'cent:defaultValueTest'
+        }).then(() => {
+            const jcontentPageBuilder = JContent
+                .visit('digitall', 'en', `pages/home/${pageName}`)
+                .switchToPageBuilder();
+            const pageBuilderModuleHeader = jcontentPageBuilder
+                .getModule(`/sites/digitall/home/${pageName}/area-main/default-value-test`)
+                .getHeader();
+            pageBuilderModuleHeader.get().should('exist');
+            pageBuilderModuleHeader.get().find('div').contains('This is a custom bar').should('exist');
+        });
+    });
+
+    it('Check if custom bar is always displayed in page builder for cemix:withCustomBar', () => {
+        addNode({
+            parentPathOrId: `/sites/digitall/home/${pageName}/area-main`,
+            name: 'with-custom-bar',
+            primaryNodeType: 'cent:myComponent',
+            mixins: ['cemix:withCustomBar']
+        }).then(() => {
+            const jcontentPageBuilder = JContent
+                .visit('digitall', 'en', `pages/home/${pageName}`)
+                .switchToPageBuilder();
+            const pageBuilderModuleHeader = jcontentPageBuilder
+                .getModule(`/sites/digitall/home/${pageName}/area-main/with-custom-bar`)
+                .getHeader();
+            pageBuilderModuleHeader.get().should('exist');
+            pageBuilderModuleHeader.get().find('div').contains('This is a custom bar').should('exist');
+        });
     });
 });

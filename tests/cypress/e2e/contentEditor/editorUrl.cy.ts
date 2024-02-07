eeb20abd2c291e1390a9c515e7e3df467883b0ba
akarmanov <alexandrk_@hotmail.com>
2024-02-07 10:04:14 -0500

    BACKLOG-22328 Add wait time

--- tests/cypress/e2e/contentEditor/editorUrl.cy.ts
+++ tests/cypress/e2e/contentEditor/editorUrl.cy.ts
@@ -86,6 +86,7 @@ describe('Editor url test', () => {
         // eslint-disable-next-line cypress/no-unnecessary-waiting
         cy.wait(500);
         cy.go('forward');
+        cy.wait(500);
         cy.get('h1').contains('People First').should('exist');
         contentEditor.cancel();
 

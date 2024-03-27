06b4306b59a6bc41be63acbb2bee39ccd7e882b5
Alex Karmanov <alexandrk_@hotmail.com>
2024-03-27 09:58:58 -0400

    BACKLOG-22519 Do not invoke gwt on create callback (#1197)
    * BACKLOG-22519 Do not invoke gwt on create callback
    
    * BACKLOG-22519 Clean up

--- src/javascript/JContent/ContentRoute/ContentLayout/ContentLayout.container.jsx
+++ src/javascript/JContent/ContentRoute/ContentLayout/ContentLayout.container.jsx
@@ -64,17 +64,6 @@ export const ContentLayoutContainer = React.memo(() => {
 
     const {isStructured, result, error, loading, refetch} = useLayoutQuery(options);
 
-    function onGwtCreate(nodePath) {
-        let parentPath = nodePath.substring(0, nodePath.lastIndexOf('/'));
-        client.cache.flushNodeEntryByPath(parentPath);
-        if (path !== parentPath) {
-            // Make sure the created CONTENT is visible in the main panel.
-            setPath(parentPath);
-        }
-
-        return client.reFetchObservableQueries();
-    }
-
     function onGwtDelete(nodePath) {
         // Clear cache entries for subnodes
         Object.keys(client.cache.idByPath)
@@ -165,7 +154,8 @@ export const ContentLayoutContainer = React.memo(() => {
         }
 
         if (operation === 'create') {
-            await onGwtCreate(nodePath);
+            // This seems to be no longer used
+            // Do nothing
         } else if (operation === 'delete') {
             await onGwtDelete(nodePath);
         } else if (operation === 'rename') {

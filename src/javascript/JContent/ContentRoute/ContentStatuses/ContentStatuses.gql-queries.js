aab602e09acb417a70c2a93aab239b7f90a6aa2a
akarmanov <alexandrk_@hotmail.com>
2024-03-28 12:21:01 -0400

    BACKLOG-22172 Use either query or node if it is available

--- src/javascript/JContent/ContentRoute/ContentStatuses/ContentStatuses.gql-queries.js
+++ src/javascript/JContent/ContentRoute/ContentStatuses/ContentStatuses.gql-queries.js
@@ -1,4 +1,5 @@
 import gql from 'graphql-tag';
+import {PredefinedFragments} from '@jahia/data-helper';
 
 export const contentStatusFragment = {
     applyFor: 'node',
@@ -38,3 +39,57 @@ export const contentStatusFragment = {
         }
     }`
 };
+
+export const GetContentStatuses = gql`
+    query getContentStatuses($path: String!, $language: String!) {
+        jcr {
+            result: nodeByPath(path: $path) {
+                ...NodeCacheRequiredFields
+                aggregatedPublicationInfo(language: $language, subNodes: true) {
+                    publicationStatus
+                }
+                lastModifiedBy: property(name: "jcr:lastModifiedBy", language: $language) {
+                    value
+                }
+                lastModified: property(name: "jcr:lastModified", language: $language) {
+                    value
+                }
+                lastPublished: property(name: "j:lastPublished", language: $language) {
+                    value
+                }
+                lastPublishedBy: property(name: "j:lastPublishedBy", language: $language) {
+                    value
+                }
+                deletedBy: property(name: "j:deletionUser", language: $language) {
+                    value
+                }
+                deleted: property(name: "j:deletionDate", language: $language) {
+                    value
+                }
+                mixinTypes {
+                    name
+                }
+                lockOwner: property(name: "jcr:lockOwner") {
+                    value
+                }
+                lockTypes: property(name: "j:lockTypes") {
+                    values
+                }                
+                wipStatus: property(name: "j:workInProgressStatus") {
+                    value
+                }
+                wipLangs: property(name: "j:workInProgressLanguages") {
+                    values
+                }
+                ancestors(fieldFilter: {filters: {fieldName: "deleted", evaluation: NOT_EMPTY}}) {
+                    deleted:property(name: "j:deletionDate") {
+                        value
+                    }
+                    deletedBy: property(name: "j:deletionUser") {
+                        value
+                    }
+                }
+            }
+        }
+    }
+    ${PredefinedFragments.nodeCacheRequiredFields.gql}`;

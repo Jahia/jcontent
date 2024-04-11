d81590eae71bb6e088bf5ba99788135c64683afb
akarmanov <alexandrk_@hotmail.com>
2024-04-11 09:39:59 -0400

    BACKLOG-22134 Fix selector options parsing

--- src/javascript/ContentEditor/SelectorTypes/Picker/Picker.utils.js
+++ src/javascript/ContentEditor/SelectorTypes/Picker/Picker.utils.js
@@ -47,18 +47,61 @@ export const getDetailedPathArray = fullPath => {
 
 export const set = (target, path, value) => {
     const splitRes = path.split('.');
+    const regex = /(\[\d+])$/;
 
     let key;
+    let position;
+    let match;
     let current = target;
     while ((splitRes.length > 1) && (key = splitRes.shift())) {
-        if (!current[key]) {
-            current[key] = {};
+        // Case items[0].key = 'something'
+        match = regex.exec(key);
+
+        if (match) {
+            key = key.replace(match[0], '');
+            // eslint-disable-next-line
+            position = ParseInt(match[0].replace(/[\[\]]/g, ''));
         }
 
-        current = current[key];
+        if (!current[key]) {
+            if (match) {
+                current[key] = [];
+            } else {
+                current[key] = {};
+            }
+        }
+
+        if (Array.isArray(current[key])) {
+            if (!current[key][position]) {
+                current[key][position] = {};
+            }
+
+            current = current[key][position];
+            match = null;
+            position = null;
+        } else {
+            current = current[key];
+        }
     }
 
-    current[splitRes.shift()] = value;
+    key = splitRes.shift();
+
+    // Case items[0] = 'something'
+    match = regex.exec(key);
+
+    if (match) {
+        key = key.replace(match[0], '');
+        // eslint-disable-next-line
+        position = ParseInt(match[0].replace(/[\[\]]/g, ''));
+
+        if (!current[key]) {
+            current[key] = [];
+        }
+
+        current[key][position] = value;
+    } else {
+        current[key] = value;
+    }
 };
 
 export const isObject = item => {

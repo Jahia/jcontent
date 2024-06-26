20ae6cb0f955b21aeb306fbf0159cdbe31698b01
Geofrey Flores <gflores@jahia.com>
2024-06-26 18:52:54 -0400

    QA-14593: use jmix:droppableContent instead of jmix:editorialContent

--- src/javascript/JContent/actions/searchAction/SearchDialog/BasicSearch/BasicSearch.gql-queries.js
+++ src/javascript/JContent/actions/searchAction/SearchDialog/BasicSearch/BasicSearch.gql-queries.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 const SiteContentTypesQuery = gql`
     query SiteContentTypesQuery($siteKey: String!, $language:String!) {
         jcr {
-            nodeTypes(filter: {includeMixins: false, siteKey: $siteKey, includeTypes: ["jmix:editorialContent", "jnt:page", "jnt:file"], excludeTypes: ["jmix:studioOnly", "jmix:hiddenType", "jnt:editableFile"]}) {
+            nodeTypes(filter: {includeMixins: false, siteKey: $siteKey, includeTypes: ["jmix:droppableContent", "jnt:page", "jnt:file"], excludeTypes: ["jmix:studioOnly", "jmix:hiddenType", "jnt:editableFile"]}) {
                 nodes {
                     name
                     displayName(language: $language)

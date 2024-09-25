e0732f823ce791a32b695ecbd83d2186f132eb73
akarmanov <alexandrk_@hotmail.com>
2024-09-25 10:47:11 -0400

    BACKLOG-23230 Simplify a bit

--- src/javascript/ContentEditor/actions/contenteditor/copyLanguage/copyLanguage.utils.js
+++ src/javascript/ContentEditor/actions/contenteditor/copyLanguage/copyLanguage.utils.js
@@ -24,11 +24,9 @@ export function getI18nFieldAndValues(formAndData) {
     const {forms, jcr} = formAndData.data;
     const adaptedSections = adaptSections(forms.editForm.sections);
     const initialValues = getInitialValues(jcr.result, adaptedSections);
-    let i18nFields = adaptedSections
+    return adaptedSections
         .flatMap(section => section.fieldSets)
         .flatMap(fieldSet => fieldSet.fields)
         .filter(field => field.i18n)
-        .map(field => ({...field, propertyName: field.name}));
-
-    return i18nFields.reduce((acc, field) => ({...acc, [field.propertyName]: initialValues[field.propertyName]}), {});
+        .reduce((acc, field) => ({...acc, [field.name]: initialValues[field.name]}), {});
 }

1c47d4b4aa688eb7f09cc174f9e9186434c87fba
Geofrey Flores <gflores@jahia.com>
2024-09-26 11:50:48 -0400

    Fix yarn lint

--- tests/cypress/page-object/fields/tagField.ts
+++ tests/cypress/page-object/fields/tagField.ts
@@ -1,7 +1,6 @@
 import {Field} from './field';
 
 export class TagField extends Field {
-
     addNewValue(text: string): void {
         this.get().find(`#${this.fieldName}`).type(`${text}{enter}`, {delay: 500});
         this.get().find(`#${this.fieldName} [role="button"]`).contains(text).should('be.visible');

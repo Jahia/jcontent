9feb27c24b355dad5d576d8422b0df6f3f96d673
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2024-11-06 09:46:53 -0500

    QA-15348: Add logic for checking mixin nodetypes on edit/create (#1329)
    * QA-15348: Add logic for checking mixin nodetypes on edit/create
    
    * Add cypress test for create scenario

---
+++ tests/cypress/page-object/fields/choicelistField.ts
@@ -0,0 +1,13 @@
+import {getComponentBySelector, Menu} from '@jahia/cypress';
+import {Field} from './field';
+
+export class ChoicelistField extends Field {
+    addNewValue(value: string): void {
+        this.get().click();
+        const choicelistMenu = getComponentBySelector(Menu, '[role="listbox"]');
+
+        const itemSel = `.moonstone-menuItem[data-value="${value}"]`;
+        choicelistMenu.get().find(itemSel).scrollIntoView().should('be.visible');
+        choicelistMenu.get().find(itemSel).click();
+    }
+}

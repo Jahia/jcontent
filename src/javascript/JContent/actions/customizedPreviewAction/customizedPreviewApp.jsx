7cec111f28b9adcdca88e4907e751224cf71ca1d
Félix Vallé <fvalle@jahia.com>
2024-11-14 15:49:38 +0100

    BACKLOG-23321: WIP center the iframe in the customized preview

--- src/javascript/JContent/actions/customizedPreviewAction/customizedPreviewApp.jsx
+++ src/javascript/JContent/actions/customizedPreviewAction/customizedPreviewApp.jsx
@@ -23,6 +23,42 @@ const getUrlParamsStr = ({user, date, channel, variant}) => {
     return paramsStr ? `?${paramsStr}` : '';
 };
 
+const getIframeWidth = params => {
+    const bodyPadding = 8 * 2;
+
+    if (params.variant === 'landscape') {
+        switch (params.channel) {
+            case 'generic_android':
+                return 770 + bodyPadding;
+            case 'apple_iphone_ver1':
+                return 797 + bodyPadding;
+            case 'android_tablet':
+                return 1174 + bodyPadding;
+            case 'apple_ipad_ver1':
+                return 1174 + bodyPadding;
+            default:
+                return 770 + bodyPadding;
+        }
+    }
+
+    if (params.variant === 'portrait') {
+        switch (params.channel) {
+            case 'generic_android':
+                return 480 + bodyPadding;
+            case 'apple_iphone_ver1':
+                return 495 + bodyPadding;
+            case 'android_tablet':
+                return 845 + bodyPadding;
+            case 'apple_ipad_ver1':
+                return 1024 + bodyPadding;
+            default:
+                return 480 + bodyPadding;
+        }
+    }
+
+    return '100%';
+};
+
 const CustomizedPreviewAppContainer = () => (
     <CustomizedPreviewContextProvider>
         <CustomizedPreviewApp/>
@@ -52,13 +88,14 @@ const CustomizedPreviewApp = () => {
     const node = res?.data?.jcr.result;
     const renderUrl = !res.error && (node?.previewAvailable || node?.displayableNode !== null) && node.renderUrl;
     const urlParams = getUrlParamsStr(params.openDialog.params);
+
     return (
         <Dialog fullScreen open>
             <LayoutContent
                 header={<CustomizedPreviewHeader {...params.openDialog?.params}/>}
                 content={
                     (renderUrl) ?
-                        (<iframe className={styles.iframe} src={`${renderUrl}${urlParams}`}/>) :
+                        (<iframe className={styles.iframe} width={getIframeWidth(params.openDialog.params)} src={`${renderUrl}${urlParams}`}/>) :
                         (<Typography variant="heading">No preview to render</Typography>)
                 }
             />

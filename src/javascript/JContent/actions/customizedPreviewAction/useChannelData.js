40ba9db3db407074536ff3b63129e651625bd26e
Geofrey Flores <gflores@jahia.com>
2024-11-19 14:25:09 -0500

    Translations, small fixes

--- src/javascript/JContent/actions/customizedPreviewAction/useChannelData.js
+++ src/javascript/JContent/actions/customizedPreviewAction/useChannelData.js
@@ -4,7 +4,7 @@ import {GET_CHANNEL_DATA} from './channels.gql-queries';
 export const useChannelData = () => {
     const {data, loading, error} = useQuery(GET_CHANNEL_DATA, {fetchPolicy: 'cache-and-network'});
 
-    const channelData = data?.jcontent.channels.filter(c => c.isVisible) || [];
+    const channelData = data?.jcontent.channels?.filter(c => c.isVisible) || [];
     const getChannel = channel => {
         return channelData.find(item => item.value === channel) || {};
     };

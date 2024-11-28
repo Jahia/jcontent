6a3ca935fbad671e6887c632d106fc86da10c2af
akarmanov <alexandrk_@hotmail.com>
2024-11-28 07:02:14 -0500

    QA-15386: Add view test

--- tests/cypress/e2e/jcontent/preview.cy.ts
+++ tests/cypress/e2e/jcontent/preview.cy.ts
@@ -1,9 +1,12 @@
 import {JContent} from '../../page-object';
+import gql from 'graphql-tag';
+import {enableModule} from '@jahia/cypress';
 
 describe('JContent preview tests', () => {
     beforeEach(() => {
         cy.executeGroovy('jcontent/createSite.groovy', {SITEKEY: 'jcontentSite'});
         cy.apollo({mutationFile: 'jcontent/createContent.graphql'});
+        enableModule('jcontent-test-module', 'jcontentSite');
         cy.loginAndStoreSession(); // Edit in chief
         const jcontent = JContent.visit('jcontentSite', 'en', 'pages/home');
         jcontent.publishAll();
@@ -26,6 +29,29 @@ describe('JContent preview tests', () => {
             .its('0.contentDocument.body.textContent')
             .should('equal',
                 'test 7');
+    });
+
+    it('should show cm view if available and no default', () => {
+        cy.apollo({
+            mutation: gql`mutation {
+                            jcr {
+                                mutateNode(pathOrId: "/sites/jcontentSite/home/area-main") {
+                                    addChild(name: "test-no-default", primaryNodeType: "cent:noDefaultView") {
+                                        uuid
+                                    }
+                                }
+                            }
+                        }`
+        });
+
+        const jcontent = JContent.visit('jcontentSite', 'en', 'pages/home');
+        jcontent.switchToListMode();
+        jcontent.getTable().getRowByLabel('test-no-default').contextMenu().select('Preview');
+        cy.get('iframe[data-sel-role="edit-preview-frame"]')
+            .its('0.contentDocument.body')
+            .should('be.visible')
+            .should('contain.html',
+                '<div>No default view</div>');
     });
 
     afterEach(() => {

82d350f11b3c29ab7e010c0985bb343f97b073e8
akarmanov <alexandrk_@hotmail.com>
2024-12-05 11:35:53 -0500

    QA-15375: Use new mutation for properties

--- src/javascript/ContentEditor/ContentEditor/updateNode/updateNode.gql-mutation.js
+++ src/javascript/ContentEditor/ContentEditor/updateNode/updateNode.gql-mutation.js
@@ -5,10 +5,9 @@ export const SavePropertiesMutation = gql`
     mutation saveNodeProperties(
         $uuid:String!,
         $propertiesToSave: [InputJCRProperty],
-        $propertiesToDelete: [String],
+        $propertiesToDelete: [InputJCRPropertyI18n],
         $mixinsToAdd: [String]!,
         $mixinsToDelete: [String]!,
-        $language: String,
         $shouldModifyChildren: Boolean!,
         $childrenOrder: [String]!,
         $shouldRename: Boolean!,
@@ -27,8 +26,8 @@ export const SavePropertiesMutation = gql`
                 setPropertiesBatch(properties: $propertiesToSave) {
                     path
                 }
-                mutateProperties(names: $propertiesToDelete) {
-                    delete(language: $language)
+                mutateLocalizedProperties(properties: $propertiesToDelete) {
+                    delete
                 }
                 reorderChildren(names: $childrenOrder) @include(if: $shouldModifyChildren)
                 node {

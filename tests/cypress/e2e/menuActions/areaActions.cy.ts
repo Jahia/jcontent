525bdc386b0b96e989f0f2cffaff0cfd79acf9f3
akarmanov <alexandrk_@hotmail.com>
2025-01-07 10:34:02 -0500

    BACKLOG-23408: Restore test

--- tests/cypress/e2e/menuActions/areaActions.cy.ts
+++ tests/cypress/e2e/menuActions/areaActions.cy.ts
@@ -35,8 +35,7 @@ describe('Area actions', () => {
         menu.shouldHaveItem('Edit');
     });
 
-    // FIXME issue with right click probably due to empty content with new centered buttons
-    it.skip('Checks that content can be pasted into the area', () => {
+    it('Checks that content can be pasted into the area', () => {
         const jcontentPageBuilder = jcontent.switchToPageBuilder();
         jcontentPageBuilder.getModule('/sites/jcontentSite/home/landing/test-content-path2').contextMenu(true).select('Copy');
 

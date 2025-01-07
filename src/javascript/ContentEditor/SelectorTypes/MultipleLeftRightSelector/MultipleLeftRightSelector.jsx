89ee8388cedf1cc0fd5b334daa73891489666818
Geofrey Flores <gflores@jahia.com>
2025-01-07 16:19:08 -0500

    BACKLOG-23099: Fix value update when selection is inconsistent with options

--- src/javascript/ContentEditor/SelectorTypes/MultipleLeftRightSelector/MultipleLeftRightSelector.jsx
+++ src/javascript/ContentEditor/SelectorTypes/MultipleLeftRightSelector/MultipleLeftRightSelector.jsx
@@ -9,13 +9,13 @@ const toArray = value => (Array.isArray(value) ? value : [value]);
 export const MultipleLeftRightSelector = ({field, onChange, value, labels}) => {
     const {t} = useTranslation('jcontent');
     const labelBase = 'label.contentEditor.picker.selectors.multipleLeftRightSelector';
-    const availableValues = field.valueConstraints.map(valueConstraint => valueConstraint.value.string);
-    const arrayValue = value ? toArray(value).filter(v => availableValues.includes(v)) : [];
+    const arrayValue = value ? toArray(value) : [];
 
     // Reset selection if previously selected option no longer available
+    const availableValues = field.valueConstraints.map(valueConstraint => valueConstraint.value.string);
+    const actualValues = arrayValue.filter(v => availableValues.includes(v));
     useEffect(() => {
         if (arrayValue && arrayValue.length > 0) {
-            const actualValues = arrayValue.filter(v => availableValues.includes(v));
             if (actualValues.length !== arrayValue.length) {
                 onChange(actualValues);
             }
@@ -43,7 +43,7 @@ export const MultipleLeftRightSelector = ({field, onChange, value, labels}) => {
         }
     }
 
-    return (
+    return (arrayValue.length !== actualValues.length) ? null : (
         <ListSelector
             isReadOnly={field.readOnly || field.valueConstraints.length === 0}
             label={listLabels}

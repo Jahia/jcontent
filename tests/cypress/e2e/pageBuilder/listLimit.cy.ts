c32acd076ec0f16ac1eba6ff8c299edffb006906
akarmanov <alexandrk_@hotmail.com>
2025-01-14 13:55:31 -0500

    BACKLOG-23416: Skip tests which conflict with new behaviour

--- tests/cypress/e2e/pageBuilder/listLimit.cy.ts
+++ tests/cypress/e2e/pageBuilder/listLimit.cy.ts
@@ -67,7 +67,8 @@ describe('Page builder', () => {
                 .assertHasNoButton();
         });
 
-        it('should not show paste button when limit is reached', () => {
+        //Skipping for now
+        it.skip('should not show paste button when limit is reached', () => {
             jcontent.getModule('/sites/jcontentSite/home/area-main/test-content1')
                 .contextMenu(true)
                 .select('Copy');

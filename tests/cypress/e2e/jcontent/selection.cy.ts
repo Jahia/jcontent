29d3159935c3c91b0853afb7c3a904896726a0c7
akarmanov <alexandrk_@hotmail.com>
2025-01-15 07:28:59 -0500

    BACKLOG-23416: Frameloaded check bypass

--- tests/cypress/e2e/jcontent/selection.cy.ts
+++ tests/cypress/e2e/jcontent/selection.cy.ts
@@ -226,7 +226,7 @@ describe('Multi-selection tests', {testIsolation: false}, () => {
              * As a workaround, we go through the rich text and select area-main using footer.
              * Then use the selector from parentFrame to check the checkbox (we cannot use module here).
              */
-            const module = pageBuilder.getModule('/sites/digitall/home/area-main/area/area/area/area-main/global-network-rich-text');
+            const module = pageBuilder.getModule('/sites/digitall/home/area-main/area/area/area/area-main/global-network-rich-text', false);
             module.click(); // Bring up footer
             module.getFooter().getBreadcrumbs().selectPos(5); // Navigate to area-main
             module.parentFrame.get().find('[data-clicked="true"]').click({force: true, metaKey: true});

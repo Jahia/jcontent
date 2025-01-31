fa351c348865e8e3ce22946e6ed8b17cd2fa2834
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2025-01-31 19:10:12 +0100

    DMF-6423: Update content footer background-color

--- src/javascript/JContent/EditFrame/Box.jsx
+++ src/javascript/JContent/EditFrame/Box.jsx
@@ -291,6 +291,9 @@ export const Box = React.memo(({
                             data-jahia-id={element.getAttribute('id')}
                             jahiatype="footer" // eslint-disable-line react/no-unknown-property
                             onClick={onClick}
+                            style={{
+                                '--backgroundColorSelected': backgroundColorSelected
+                            }}
                     >
                         <Breadcrumbs currentNode={node} nodes={breadcrumbs} setCurrentElement={setCurrentElement} onSelect={onSelect}/>
                     </footer>}

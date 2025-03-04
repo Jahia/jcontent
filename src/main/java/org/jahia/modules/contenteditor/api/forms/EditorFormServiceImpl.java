c6daeb020c41767fd015b2099c6b8c1c1ef8a2b5
Alex Karmanov <alexandrk_@hotmail.com>
2025-03-04 08:29:18 -0500

    style: Remove comment

--- src/main/java/org/jahia/modules/contenteditor/api/forms/EditorFormServiceImpl.java
+++ src/main/java/org/jahia/modules/contenteditor/api/forms/EditorFormServiceImpl.java
@@ -197,7 +197,6 @@ public class EditorFormServiceImpl implements EditorFormService {
                     for (Field field : fieldSet.getFields()) {
                         // Set field label and description if not set
                         field.initializeLabel(uiLocale, site, primaryNodeType);
-                        // j:bindedComponent property is normally hidden, but we want to have it in the form in order to choose component to bind
                         field.setVisible((field.isHide() == null || !field.isHide()) &&
                             (field.getRequiredPermission() == null || site.hasPermission(field.getRequiredPermission())));
                         field.setI18n(field.isI18n() != null && field.isI18n());

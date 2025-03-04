1d89a52830389911c365cc1b01e11ed7ad6d7912
Geofrey Flores <gflores@jahia.com>
2025-03-04 15:04:25 -0500

    Revert "temp: run only selected tests"
    This reverts commit 629ef4cd10994f3c5a0f9087f8a15c49277685cc.

--- tests/cypress.config-pageBuilder.ts
+++ tests/cypress.config-pageBuilder.ts
@@ -1,12 +1,6 @@
 import {defineConfig} from 'cypress';
 import {baseConfig} from './cypress.config.common';
 
-baseConfig.e2e.specPattern = [
-    'cypress/e2e/pageBuilder/**/**.cy.ts',
-    'cypress/e2e/jcontent/**/createContent.cy.ts',
-    'cypress/e2e/jcontent/**/selection.cy.ts',
-    'cypress/e2e/jcontent/**/compareStagingLive.cy.ts',
-    'cypress/e2e/menuActions/**/areaActions.cy.ts',
-];
+baseConfig.e2e.specPattern = ['cypress/e2e/pageBuilder/**/**.cy.ts'];
 
 export default defineConfig(baseConfig);

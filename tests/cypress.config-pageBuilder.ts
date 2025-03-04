629ef4cd10994f3c5a0f9087f8a15c49277685cc
Geofrey Flores <gflores@jahia.com>
2025-03-04 10:53:18 -0500

    temp: run only selected tests

--- tests/cypress.config-pageBuilder.ts
+++ tests/cypress.config-pageBuilder.ts
@@ -1,6 +1,12 @@
 import {defineConfig} from 'cypress';
 import {baseConfig} from './cypress.config.common';
 
-baseConfig.e2e.specPattern = ['cypress/e2e/pageBuilder/**/**.cy.ts'];
+baseConfig.e2e.specPattern = [
+    'cypress/e2e/pageBuilder/**/**.cy.ts',
+    'cypress/e2e/jcontent/**/createContent.cy.ts',
+    'cypress/e2e/jcontent/**/selection.cy.ts',
+    'cypress/e2e/jcontent/**/compareStagingLive.cy.ts',
+    'cypress/e2e/menuActions/**/areaActions.cy.ts',
+];
 
 export default defineConfig(baseConfig);

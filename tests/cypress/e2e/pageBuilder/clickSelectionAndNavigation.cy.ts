85c10f44f8bee26d045b63695c14809e1178b3f6
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2025-03-05 11:17:52 -0500

    chore: Re-enable disabled tests from adding area headers (#1617)
    * Update node version; add runner debug step
    
    * temp: run only selected tests
    
    * fix listLimit
    
    * fix more tests
    
    * Re-enable pagebuilder/selections test
    
    * Re-enable pagebuilder,jcontent navigation,selection tests
    
    * Fix areaActions tests
    
    * Re-enable compare staging live tests
    
    * Revert "temp: run only selected tests"
    
    This reverts commit 629ef4cd10994f3c5a0f9087f8a15c49277685cc.
    
    * fix lint warnings, errors
    
    * Fix usages test
    
    * aaarrgghhh (lint fix)

--- tests/cypress/e2e/pageBuilder/clickSelectionAndNavigation.cy.ts
+++ tests/cypress/e2e/pageBuilder/clickSelectionAndNavigation.cy.ts
@@ -1,8 +1,33 @@
 import {JContent, JContentPageBuilder} from '../../page-object';
+import {addNode, deleteNode} from '@jahia/cypress';
 
-describe('Page builder navigation tests', () => {
+describe('Page builder - navigation tests', () => {
+    before(() => {
+        addNode({
+            parentPathOrId: '/sites/digitall/home',
+            name: 'external-link',
+            primaryNodeType: 'jnt:externalLink',
+            properties: [{name: 'j:url', value: 'http://www.google.com'}]
+        });
+
+        addNode({
+            parentPathOrId: '/sites/digitall/home',
+            name: 'internal-link',
+            primaryNodeType: 'jnt:nodeLink',
+            properties: [
+                {name: 'j:node', value: '/sites/digitall/home', type: 'REFERENCE'},
+                {name: 'jcr:title', language: 'en', value: 'internal-xxx'}
+            ]
+        });
+    });
+
     beforeEach(function () {
         cy.loginAndStoreSession();
+    });
+
+    after(() => {
+        deleteNode('/sites/digitall/home/external-link');
+        deleteNode('/sites/digitall/home/internal-link');
     });
 
     it('Can unselect self', () => {
@@ -45,5 +70,18 @@ describe('Page builder navigation tests', () => {
         module = pageBuilder.getModule('/sites/digitall/home/area-main/highlights/our-companies');
         module.click();
         module.getHeader().assertHeaderText('Our Companies');
+    });
+
+    it('Click on links should open modal', () => {
+        const jcontent = JContent
+            .visit('digitall', 'en', 'pages/home')
+            .switchToPageBuilder();
+        jcontent.getSecondaryNav().get().find('[data-sel-role="home"] .moonstone-treeView_itemToggle').click();
+        cy.contains('external-link').click();
+        cy.contains('The link redirects to an external URL');
+        cy.get('[data-sel-role="cancel-button"]').click();
+        cy.contains('internal-xxx').click();
+        cy.contains('The link redirects to Home');
+        cy.get('[data-sel-role="cancel-button"]').click();
     });
 });

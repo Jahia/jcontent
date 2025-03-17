43396c4fb6d893c382ba204942ba6f2065c27fd6
Sonia Dusolle <42536778+sdusolle@users.noreply.github.com>
2025-03-17 16:41:19 +0100

    test: Add test (#1658)
    * test: add test to check size file is displayed

--- tests/cypress/e2e/jcontent/tableViews.cy.ts
+++ tests/cypress/e2e/jcontent/tableViews.cy.ts
@@ -20,4 +20,12 @@ describe('Table view tests', () => {
         jcontent.switchToStructuredView();
         jcontent.getTable().getHeaderByRole('name').isNotSortable();
     });
+
+    it('Check we display size file in Media list view', function () {
+        const jcontent = JContent.visit('jcontentSite', 'en', 'media/files/bootstrap/css');
+        jcontent.switchToListMode();
+        cy.get('[data-cm-role="table-content-list-cell-fileSize"]')
+            .should('be.visible')
+            .and('contain', '142.64 KB');
+    });
 });

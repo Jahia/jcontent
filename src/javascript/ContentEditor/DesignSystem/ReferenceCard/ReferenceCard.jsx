caadaf60d22900e0d304452348ce89bba63a4441
Eevolee <eevee4224@gmail.com>
2025-04-01 14:46:47 +0200

    replace referenceCard with cardSelector

--- src/javascript/ContentEditor/DesignSystem/ReferenceCard/ReferenceCard.jsx
+++ src/javascript/ContentEditor/DesignSystem/ReferenceCard/ReferenceCard.jsx
@@ -1,125 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import {Typography} from '@jahia/moonstone';
-import {withStyles} from '@material-ui/core';
-import {HandleDrag} from '@jahia/moonstone';
-
-const styles = theme => ({
-    container: {
-        flex: 1,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    add: {
-        width: '100%',
-        color: 'var(--color-gray_dark60)',
-        height: theme.spacing.unit * 9,
-        backgroundColor: theme.palette.ui.epsilon,
-        border: '1px var(--color-gray40) dashed',
-        fontSize: '0.875rem',
-        borderRadius: '2px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        cursor: 'pointer',
-        '&:hover': {
-            border: '1px var(--color-gray40) solid'
-        },
-        '&:focus': {
-            outline: 'none',
-            border: '1px var(--color-gray40) solid'
-        },
-        '& svg': {
-            marginBottom: 'var(--spacing-nano)'
-        }
-    },
-    addError: {
-        border: '1px var(--color-warning) solid',
-        '&:hover': {
-            border: '1px var(--color-warning) solid'
-        },
-        '&:focus': {
-            border: '1px var(--color-warning) solid'
-        }
-    },
-    addReadOnly: {
-        outline: 'none',
-        background: theme.palette.ui.alpha,
-        border: '1px var(--color-gray40) solid',
-        '&:hover': {
-            border: '1px var(--color-gray40) solid'
-        },
-        '&:focus': {
-            border: '1px var(--color-gray40) solid'
-        },
-        cursor: 'default'
-    },
-    fieldContainer: {
-        width: '100%',
-        height: theme.spacing.unit * 9,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        border: `1px ${theme.palette.ui.zeta} solid`,
-        boxShadow: '1px 5px 6px rgba(64, 77, 86, 0.1)',
-        borderRadius: '2px',
-        paddingRight: theme.spacing.unit,
-        '& button': {
-            color: theme.palette.font.beta
-        },
-        cursor: 'pointer'
-    },
-    fieldContainerReadOnly: {
-        border: `1px ${theme.palette.ui.omega} solid`,
-        boxShadow: 'none',
-        cursor: 'default'
-    },
-    fieldFigureContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: `calc(${theme.spacing.unit * 9}px - 2px)`,
-        width: theme.spacing.unit * 9,
-        overflow: 'hidden',
-        backgroundColor: theme.palette.ui.omega
-    },
-    fieldImage: {
-        textAlign: 'center',
-        margin: 0,
-        maxWidth: theme.spacing.unit * 9,
-        maxHeight: `calc(${theme.spacing.unit * 9}px - 2px)`,
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'contain'
-    },
-    fieldSelectedMetadata: {
-        flexGrow: 1,
-        padding: '1rem 2rem',
-        width: 'calc(100% - 144px)',
-        '& p': {
-            width: '360px',
-            padding: 0,
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis'
-        }
-    },
-    referenceButtonEmptyContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center'
-    },
-    error: {
-        color: 'var(--color-warning)'
-    },
-    draggableIcon: {
-        cursor: 'grab'
-    }
-});
+import {CardSelector, Chip, EmptyCardSelector} from '@jahia/moonstone';
 
 const ReferenceCardCmp = ({
+    id,
     classes,
     isReadOnly,
     isError,
@@ -128,77 +12,45 @@ const ReferenceCardCmp = ({
     fieldData,
     labelledBy,
     onClick,
-    isDraggable
+    isDraggable,
+    cardAction
 }) => {
     // If card have already data
     if (fieldData) {
-        const nameId = `${labelledBy}-name`;
         return (
-            <div className={classes.container}>
-                {isDraggable &&
-                <div className={classes.draggableIcon}>
-                    <HandleDrag size="big"/>
-                </div>}
-
-                <article
-                    className={clsx(
-                        classes.fieldContainer,
-                        (isReadOnly ? classes.fieldContainerReadOnly : ''),
-                        (isDraggable ? classes.draggableIcon : '')
-                    )}
-                    data-sel-field-picker="filled"
-                    data-sel-field-picker-action="openPicker"
-                    role="button"
-                    tabIndex="0"
-                    aria-labelledby={labelledBy}
-                    onClick={() => {
-                        if (isReadOnly) {
-                            return;
-                        }
-
-                        onClick(true);
-                    }}
-                >
-                    <div className={classes.fieldFigureContainer}>
-                        <img src={fieldData.url} className={classes.fieldImage} aria-labelledby={nameId} alt=""/>
-                    </div>
-                    <div className={classes.fieldSelectedMetadata}>
-                        <Typography data-sel-field-picker-name variant="caption" id={nameId}>
-                            {fieldData.name}
-                        </Typography>
-                        <Typography data-sel-field-picker-info variant="body">
-                            {fieldData.info}
-                        </Typography>
-                    </div>
-                </article>
-            </div>
+            <CardSelector
+                id={id}
+                isDraggable={isDraggable}
+                thumbnailType="icon"
+                thumbnailURL={fieldData.url && fieldData.url}
+                displayName={fieldData.name && fieldData.name}
+                chips={fieldData.type && [<Chip key={fieldData.name && fieldData.name} color="accent" label={fieldData.type}/>]}
+                systemName={fieldData.systemname && fieldData.systemname}
+                information={fieldData.info && fieldData.info}
+                isReadOnly={isReadOnly}
+                isDisabled={false}
+                hasError={isError}
+                data-sel-field-picker="filled"
+                data-sel-field-picker-action="openPicker"
+                cardAction={cardAction}
+                onClick={onClick}
+            />
         );
     }
 
     return (
-        <button
+        <EmptyCardSelector
+            id={id}
+            iconStart={emptyIcon}
+            label={emptyLabel}
+            isReadOnly={isReadOnly}
             data-sel-media-picker="empty"
             data-sel-field-picker-action="openPicker"
-            className={clsx(classes.add, isReadOnly && classes.addReadOnly, isError && classes.addError)}
             type="button"
             aria-disabled={isReadOnly}
             aria-labelledby={labelledBy}
-            onClick={() => {
-                if (isReadOnly) {
-                    return;
-                }
-
-                onClick(true);
-            }}
-        >
-            {!isReadOnly &&
-            <div className={clsx(classes.referenceButtonEmptyContainer, isError && classes.error)}>
-                {emptyIcon}
-                <Typography variant="body" component="span">
-                    {emptyLabel}
-                </Typography>
-            </div>}
-        </button>
+            onClick={onClick}
+        />
     );
 };
 
@@ -212,21 +64,25 @@ ReferenceCardCmp.defaultProps = {
 };
 
 ReferenceCardCmp.propTypes = {
+    id: PropTypes.string,
     isReadOnly: PropTypes.bool,
     isError: PropTypes.bool,
-    classes: PropTypes.object.isRequired,
+    classes: PropTypes.object,
     onClick: PropTypes.func,
     fieldData: PropTypes.shape({
         url: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
+        type: PropTypes.string,
+        systemname: PropTypes.string,
         info: PropTypes.string.isRequired
     }),
+    cardAction: PropTypes.element,
     isDraggable: PropTypes.bool,
     emptyLabel: PropTypes.string,
     emptyIcon: PropTypes.element,
     labelledBy: PropTypes.string
 };
 
-export const ReferenceCard = withStyles(styles)(ReferenceCardCmp);
+export const ReferenceCard = (ReferenceCardCmp);
 
 ReferenceCard.displayName = 'ReferenceCard';

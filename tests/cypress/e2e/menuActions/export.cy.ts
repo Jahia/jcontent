58d42d390c86bbaaa30e22b6c378446e03edd083
sagnard <ksagnard@jahia.com>
2025-05-28 14:00:01 +0200

    change the test have a loop on a set on characters

--- tests/cypress/e2e/menuActions/export.cy.ts
+++ tests/cypress/e2e/menuActions/export.cy.ts
@@ -4,18 +4,11 @@ import {createSite, deleteSite} from '@jahia/cypress';
 describe('Export tests', () => {
     const siteKey = 'jContentSite-export';
     const downloadsFolder = Cypress.config('downloadsFolder');
-    const labelWithSpecialCharacters =  "With special characters"
+
 
     before(() => {
+        deleteSite(siteKey);
         createSite(siteKey);
-        cy.apollo({
-            mutationFile: 'jcontent/createTextContentUnderPath.graphql',
-            variables: {path: `/sites/${siteKey}/contents`}
-        });
-        cy.apollo({
-            mutationFile: 'jcontent/createTextContentUnderPath.graphql',
-            variables: {contentName: labelWithSpecialCharacters, path: `/sites/${siteKey}/contents`}
-        });
         cy.apollo({
             mutationFile: 'jcontent/createContentFolder.graphql',
             variables: {folderName: 'importFolder', parentPath: `/sites/${siteKey}/contents`}
@@ -27,39 +20,37 @@ describe('Export tests', () => {
     });
 
     after(() => {
-        deleteSite(siteKey);
+       // deleteSite(siteKey);
         cy.logout();
-    });
-
-    it('should export a content with sub-contents as zip', () => {
-        cy.login();
-        const jcontent = JContent.visit(siteKey, 'en', 'content-folders/content');
-
-        jcontent.exportComponentByText('landing', downloadsFolder, 'Staging content only', 'zip');
-
-        // Reimport exported content
-        jcontent.getTable().getRowByLabel('importFolder').get().dblclick();
-        jcontent.getBrowseControlMenu().selectByRole('import');
-        cy.get('#file-upload-input').selectFile(`${downloadsFolder}/landing.zip`, {force: true});
-        jcontent.getTable().getRowByLabel('landing').get().dblclick();
-        jcontent.getTable().getRowByLabel('test path 1').should('exist');
-        jcontent.getTable().getRowByLabel('test path 2').should('exist');
-        jcontent.getTable().getRowByLabel('test path 3').should('exist');
     });
 
     it('should export a content with special characters as zip', () => {
         cy.login();
-        const jcontent = JContent.visit(siteKey, 'en', 'content-folders/content');
 
-        jcontent.exportComponentByText(labelWithSpecialCharacters, downloadsFolder, 'Staging content only', 'zip');
+        const characters = ['1', ' ', 'à', '-', '.'];
 
-        // Reimport exported content
-        jcontent.getTable().getRowByLabel('importFolder').get().dblclick();
-        jcontent.getBrowseControlMenu().selectByRole('import');
-        cy.get('#file-upload-input').selectFile(`${downloadsFolder}/${labelWithSpecialCharacters}.zip`, {force: true});
-        jcontent.getTable().getRowByLabel(labelWithSpecialCharacters).get().dblclick();
-        jcontent.getTable().getRowByLabel('test path 1').should('exist');
-        jcontent.getTable().getRowByLabel('test path 2').should('exist');
-        jcontent.getTable().getRowByLabel('test path 3').should('exist');
+        characters.forEach(character => {
+
+            const labelWithSpecialCharacters =  "Special" + character +  "characters"
+
+            cy.apollo({
+                mutationFile: 'jcontent/createTextContentUnderPath.graphql',
+                variables: {contentName: labelWithSpecialCharacters, path: `/sites/${siteKey}/contents`}
+            });
+
+            const jcontent = JContent.visit(siteKey, 'en', 'content-folders/content');
+
+            jcontent.exportComponentByText(labelWithSpecialCharacters, downloadsFolder, 'Staging content only', 'zip');
+
+            // Reimport exported content
+            jcontent.getTable().getRowByLabel('importFolder').get().dblclick();
+            jcontent.getBrowseControlMenu().selectByRole('import');
+            cy.get('#file-upload-input').selectFile(`${downloadsFolder}/${labelWithSpecialCharacters}.zip`, {force: true});
+            jcontent.getTable().getRowByLabel(labelWithSpecialCharacters).get().dblclick();
+            jcontent.getTable().getRowByLabel('test path 1').should('exist');
+            jcontent.getTable().getRowByLabel('test path 2').should('exist');
+            jcontent.getTable().getRowByLabel('test path 3').should('exist');
+
+        })
     });
 });

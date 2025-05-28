eafe1b28c91d399dff5f4cfc866bce903158dfed
sagnard <ksagnard@jahia.com>
2025-05-28 15:18:18 +0200

    refactor to ease debug

--- tests/cypress/e2e/menuActions/export.cy.ts
+++ tests/cypress/e2e/menuActions/export.cy.ts
@@ -5,8 +5,8 @@ describe('Export tests', () => {
     const siteKey = 'jContentSite-export';
     const downloadsFolder = Cypress.config('downloadsFolder');
 
-
     before(() => {
+        cy.login();
         createSite(siteKey);
         cy.apollo({
             mutationFile: 'jcontent/createContentFolder.graphql',
@@ -14,42 +14,59 @@ describe('Export tests', () => {
         });
     });
 
-    beforeEach(() => {
-        cy.loginAndStoreSession();
-    });
-
     after(() => {
         deleteSite(siteKey);
         cy.logout();
     });
 
-    it('should export a content with special characters as zip', () => {
-        cy.login();
-
-        const characters = ['1', ' ', '-', '.'];
-
-        characters.forEach(character => {
-
-            const labelWithSpecialCharacters =  "Special" + character +  "characters"
-
-            cy.apollo({
-                mutationFile: 'jcontent/createTextContentUnderPath.graphql',
-                variables: {contentName: labelWithSpecialCharacters, path: `/sites/${siteKey}/contents`}
-            });
-
-            const jcontent = JContent.visit(siteKey, 'en', 'content-folders/content');
-
-            jcontent.exportComponentByText(labelWithSpecialCharacters, downloadsFolder, 'Staging content only', 'zip');
-
-            // Reimport exported content
-            jcontent.getTable().getRowByLabel('importFolder').get().dblclick();
-            jcontent.getBrowseControlMenu().selectByRole('import');
-            cy.get('#file-upload-input').selectFile(`${downloadsFolder}/${labelWithSpecialCharacters}.zip`, {force: true});
-            jcontent.getTable().getRowByLabel(labelWithSpecialCharacters).get().dblclick();
-            jcontent.getTable().getRowByLabel('test path 1').should('exist');
-            jcontent.getTable().getRowByLabel('test path 2').should('exist');
-            jcontent.getTable().getRowByLabel('test path 3').should('exist');
-
-        })
+    it('should export a content with characters 1 as zip', () => {
+        exportAndReimport('1');
     });
+
+    it('should export a content with characters quote as zip', () => {
+        exportAndReimport('\'');
+    });
+
+    it('should export a content with characters & as zip', () => {
+        exportAndReimport('&');
+    });
+
+    it('should export a content with characters + as zip', () => {
+        exportAndReimport('+');
+    });
+
+    it('should export a content with characters . as zip', () => {
+        exportAndReimport('.');
+    });
+
+    it('should export a content with characters - as zip', () => {
+        exportAndReimport('-');
+    });
+
+    it('should export a content with characters space as zip', () => {
+        exportAndReimport(' ');
+    });
+
+    function exportAndReimport(character: string) {
+        cy.login();
+        const labelWithSpecialCharacters = 'Special' + character + 'characters';
+
+        cy.apollo({
+            mutationFile: 'jcontent/createTextContentUnderPath.graphql',
+            variables: {contentName: labelWithSpecialCharacters, path: `/sites/${siteKey}/contents`}
+        });
+
+        const jcontent = JContent.visit(siteKey, 'en', 'content-folders/content');
+
+        jcontent.exportComponentByText(labelWithSpecialCharacters, downloadsFolder, 'Staging content only', 'zip');
+
+        // Reimport exported content
+        jcontent.getTable().getRowByLabel('importFolder').get().dblclick();
+        jcontent.getBrowseControlMenu().selectByRole('import');
+        cy.get('#file-upload-input').selectFile(`${downloadsFolder}/${labelWithSpecialCharacters}.zip`, {force: true});
+        jcontent.getTable().getRowByLabel(labelWithSpecialCharacters).get().dblclick();
+        jcontent.getTable().getRowByLabel('test path 1').should('exist');
+        jcontent.getTable().getRowByLabel('test path 2').should('exist');
+        jcontent.getTable().getRowByLabel('test path 3').should('exist');
+    }
 });

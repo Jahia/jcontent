25d23ef143a383b24c6dc023d4d69e26367f4159
Geofrey Flores <gflores@jahia.com>
2025-05-30 13:52:06 -0400

    Create publication status util fn, refactor constants

--- src/javascript/ContentEditor/ContentEditor.constants.js
+++ src/javascript/ContentEditor/ContentEditor.constants.js
@@ -1,18 +1,9 @@
+import JContentConstants from '~/JContent/JContent.constants';
+
 export const Constants = {
     editPanel: {
-        publicationStatus: {
-            NOT_PUBLISHED: 'NOT_PUBLISHED',
-            PUBLISHED: 'PUBLISHED',
-            MODIFIED: 'MODIFIED',
-            UNPUBLISHED: 'UNPUBLISHED',
-            MANDATORY_LANGUAGE_UNPUBLISHABLE: 'MANDATORY_LANGUAGE_UNPUBLISHABLE',
-            LIVE_MODIFIED: 'LIVE_MODIFIED',
-            LIVE_ONLY: 'LIVE_ONLY',
-            CONFLICT: 'CONFLICT',
-            MANDATORY_LANGUAGE_VALID: 'MANDATORY_LANGUAGE_VALID',
-            DELETED: 'DELETED',
-            MARKED_FOR_DELETION: 'MARKED_FOR_DELETION'
-        },
+        // DEPRECATED: use JContentConstants.availablePublicationStatuses instead
+        publicationStatus: JContentConstants.availablePublicationStatuses,
         editTab: 'EDIT'
     },
     field: {

45f4602a72ac44350b885c86553f3a12a6fa996f
Michael Krasnov <mihalKrasnov@gmail.com>
2025-06-03 15:52:13 -0400

    fix: improved dnd behaviour in pickers & content lists (#1812)
    * fix: merge conflict resolution
    
    * fix: style and lint fixes

--- src/javascript/ContentEditor/DesignSystem/OrderableValue/OrderableValue.jsx
+++ src/javascript/ContentEditor/DesignSystem/OrderableValue/OrderableValue.jsx
@@ -1,71 +1,72 @@
 import {useTranslation} from 'react-i18next';
-import {useDrag, useDrop} from 'react-dnd';
-import clsx from 'clsx';
 import styles from '~/ContentEditor/utils/dragAndDrop.scss';
 import {Button, Tooltip, Close, HandleDrag} from '@jahia/moonstone';
-import React from 'react';
+import React, {useRef} from 'react';
 import PropTypes from 'prop-types';
+import {useReorderDrag, useReorderDrop} from '~/ContentEditor/utils';
+import clsx from 'clsx';
 
-export const OrderableValue = ({field, onFieldRemove, onValueReorder, index, component, isReferenceCard = false, isDraggable = false}) => {
+export const OrderableValue = ({id, field, onFieldRemove, onValueReorder, onValueReorderDropped, onValueReorderAborted, index, component, isReferenceCard = false, isDraggable = false}) => {
     const {t} = useTranslation('jcontent');
+    const ref = useRef(null);
     const name = `${field.name}[${index}]`;
-    const [{isDropping}, drop] = useDrop({
-        accept: `REFERENCE_CARD_${field.name}`,
-        drop: item => onValueReorder(item.name, index),
-        collect: monitor => {
-            return {
-                isDropping: monitor.isOver() && monitor.canDrop() && monitor.getItem().name !== name
-            };
-        }
-    });
-    const [{isDragging}, drag] = useDrag({
-        canDrag: () => isDraggable,
-        type: `REFERENCE_CARD_${field.name}`,
-        item: {name: name},
-        collect: monitor => ({
-            isDragging: monitor.isDragging()
-        })
-    });
+    const [{handlerId}, drop] = useReorderDrop(
+        {ref, index, onReorder: onValueReorder},
+        {
+            accept: `REFERENCE_CARD_${field.name}`
+        });
+    const [{isDragging}, drag, dragPreview] = useReorderDrag(
+        {item: {id, index}, onDrop: onValueReorderDropped, onAbort: onValueReorderAborted},
+        {
+            type: `REFERENCE_CARD_${field.name}`,
+            canDrag: () => isDraggable
+        });
+
+    if (isReferenceCard) {
+        drag(drop(ref));
+    } else {
+        drop(dragPreview(ref));
+    }
 
     return (
-        <div key={name}
-             ref={field.readOnly ? null : drop}
-             id={name}
-             className={styles.fieldComponentContainer}
-             data-sel-content-editor-multiple-generic-field={name}
-             data-sel-content-editor-field-readonly={field.readOnly}
-        >
-            <div className={`${styles.referenceDropGhostHidden} ${isDropping ? styles.referenceDropGhost : ''}`} data-droppable-zone={name}/>
-            {component &&
-                <div ref={isReferenceCard && isDraggable ? drag : null} className={styles.draggableCard}>
-                    {!isDragging &&
-                        <>
-                            {!isReferenceCard &&
-                            <div ref={isDraggable ? drag : null} className={clsx(isDraggable ? styles.draggableIcon : styles.notDraggableIcon)}>
-                                <HandleDrag size="big"/>
-                            </div>}
-                            {component}
-                        </>}
-                    {!isDragging &&
-                    <Tooltip label={t('jcontent:label.contentEditor.edit.fields.actions.clear')}>
-                        <Button variant="ghost"
-                                data-sel-action={`removeField_${index}`}
-                                aria-label={t('jcontent:label.contentEditor.edit.fields.actions.clear')}
-                                icon={<Close/>}
-                                onClick={() => onFieldRemove(index)}
-                        />
-                    </Tooltip>}
-                </div>}
+        <div className={styles.fieldComponentContainer}>
+            <div
+                ref={field.readOnly ? undefined : ref}
+                className={clsx(
+                    styles.draggableCard,
+                    isDragging && styles.draggingCard
+                )}
+                data-sel-content-editor-multiple-generic-field={name}
+                data-sel-content-editor-field-readonly={field.readOnly}
+                data-handler-id={handlerId}
+            >
+                {!isReferenceCard &&
+                    <div ref={isDraggable ? drag : null} className={clsx(isDraggable ? styles.draggableIcon : styles.notDraggableIcon)}>
+                        <HandleDrag size="big"/>
+                    </div>}
+                {component}
+                <Tooltip label={t('jcontent:label.contentEditor.edit.fields.actions.clear')}>
+                    <Button variant="ghost"
+                            data-sel-action={`removeField_${index}`}
+                            aria-label={t('jcontent:label.contentEditor.edit.fields.actions.clear')}
+                            icon={<Close/>}
+                            onClick={() => onFieldRemove(index)}
+                    />
+                </Tooltip>
+            </div>
         </div>
     );
 };
 
 OrderableValue.propTypes = {
+    id: PropTypes.string.isRequired,
     field: PropTypes.object.isRequired,
     onFieldRemove: PropTypes.func,
     onValueReorder: PropTypes.func,
+    onValueReorderDropped: PropTypes.func,
+    onValueReorderAborted: PropTypes.func,
     index: PropTypes.number.isRequired,
-    component: PropTypes.object,
+    component: PropTypes.object.isRequired,
     isReferenceCard: PropTypes.bool,
     isDraggable: PropTypes.bool
 };
--- src/javascript/ContentEditor/editorTabs/EditPanelContent/FormBuilder/Field/MultipleField.jsx
+++ src/javascript/ContentEditor/editorTabs/EditPanelContent/FormBuilder/Field/MultipleField.jsx
@@ -4,13 +4,16 @@ import * as PropTypes from 'prop-types';
 import {useTranslation} from 'react-i18next';
 import {FastField, useFormikContext} from 'formik';
 import {FieldPropTypes} from '~/ContentEditor/ContentEditor.proptypes';
-import {onListReorder} from '~/ContentEditor/utils';
+import {useReorderList} from '~/ContentEditor/utils';
 import styles from '~/ContentEditor/utils/dragAndDrop.scss';
 import {OrderableValue} from '~/ContentEditor/DesignSystem/OrderableValue/OrderableValue';
 
 export const MultipleField = ({editorContext, inputContext, field, onChange, onBlur}) => {
     const {values, setFieldValue, setFieldTouched} = useFormikContext();
     const {t} = useTranslation('jcontent');
+    const fieldValue = values[field.name] ?? [];
+
+    const {reorderedItems, handleReorder, reset} = useReorderList(fieldValue);
 
     const multipleFieldOnChange = (index, newData) => {
         let updatedValues = [...values[field.name]];
@@ -33,46 +36,46 @@ export const MultipleField = ({editorContext, inputContext, field, onChange, onB
         onBlur();
     };
 
-    const onValueReorder = (droppedId, index) => {
-        setFieldValue(field.name, onListReorder(values[field.name], droppedId, index, field.name));
+    const handleFinalReorder = () => {
+        const newValues = reorderedItems.map(({item}) => item);
+        setFieldValue(field.name, newValues);
         setFieldTouched(field.name, true, false);
     };
 
     return (
         <>
             <div className="flexFluid">
-                {values[field.name] && values[field.name].length > 0 && (
-                    values[field.name].map((value, index) => {
+                {reorderedItems?.length > 0 && (
+                    reorderedItems.map(({item, id, index}) => {
                         const FieldComponent = inputContext.selectorType.cmp;
                         const name = `${field.name}[${index}]`;
 
                         return (
-                            // eslint-disable-next-line react/no-array-index-key
-                            <OrderableValue key={`${field.name}_${index}`}
-                                            component={<FastField component={FieldComponent}
-                                                                  id={name}
-                                                                  field={field}
-                                                                  value={value}
-                                                                  values={values}
-                                                                  editorContext={editorContext}
-                                                                  inputContext={inputContext}
-                                                                  onChange={newData => {
+                            <OrderableValue
+                                key={id}
+                                id={id}
+                                component={<FastField component={FieldComponent}
+                                                      id={name}
+                                                      field={field}
+                                                      value={item}
+                                                      values={values}
+                                                      editorContext={editorContext}
+                                                      inputContext={inputContext}
+                                                      onChange={newData => {
                                                                       multipleFieldOnChange(index, newData);
                                                                   }}
-                                                                  onBlur={onBlur}
+                                                      onBlur={onBlur}
                                             />}
-                                            field={field}
-                                            index={index}
-                                            isDraggable={!field.readOnly && values[field.name].length > 1}
-                                            onFieldRemove={onFieldRemove}
-                                            onValueReorder={onValueReorder}/>
+                                field={field}
+                                index={index}
+                                isDraggable={!field.readOnly && values[field.name].length > 1}
+                                onFieldRemove={onFieldRemove}
+                                onValueReorder={handleReorder}
+                                onValueReorderDropped={handleFinalReorder}
+                                onValueReorderAborted={reset}
+                            />
                         );
                     })
-                )}
-                {values[field.name] && values[field.name].length > 0 && (
-                    <OrderableValue field={field}
-                                    index={values[field.name].length}
-                                    onValueReorder={onValueReorder}/>
                 )}
             </div>
             {!field.readOnly &&

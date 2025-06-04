a287359fc352f1076d33b06aeb32589b5cc3611b
Geofrey Flores <gflores@jahia.com>
2025-06-04 16:29:47 -0400

    Fix TextEncoder on jest tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ const jestConfig = require('@jahia/test-framework').jestConfig;
 
 module.exports = {
     ...jestConfig,
+    setupFiles: ['<rootDir>/src/javascript/setupTextEncoder.js'],
+    setupFilesAfterEnv: ['<rootDir>/src/javascript/setupTests.js'],
     moduleNameMapper: {
         ...jestConfig.moduleNameMapper,
         '@jahia/moonstone': '<rootDir>/node_modules/@jahia/moonstone/dist/index.cjs',

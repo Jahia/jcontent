03716cc7e1801b707cb7caede140c5bcdc388bb8
Alex Karmanov <alexandrk_@hotmail.com>
2025-06-05 13:47:28 -0400

    chore: Bump react to 18.3.1 (#1826)
    * chore: Bump react to 18.3.1
    
    * chore: Fix jest tests
    
    * chore: Fix lint
    
    * chore: Remove content type selector unit test

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,15 @@ const jestConfig = require('@jahia/test-framework').jestConfig;
 
 module.exports = {
     ...jestConfig,
+    setupFiles: ['<rootDir>/src/javascript/setupTextEncoder.js'],
+    setupFilesAfterEnv: ['<rootDir>/src/javascript/setupTests.js'],
     moduleNameMapper: {
         ...jestConfig.moduleNameMapper,
         '@jahia/moonstone': '<rootDir>/node_modules/@jahia/moonstone/dist/index.cjs',
+        'react-dnd': 'react-dnd-cjs',
+        'react-dnd-html5-backend': 'react-dnd-html5-backend-cjs',
+        'dnd-core': 'dnd-core-cjs'
     },
-    modulePathIgnorePatterns: ['<rootDir>/tests']
+    modulePathIgnorePatterns: ['<rootDir>/tests'],
+    testEnvironment: 'jsdom'
 };

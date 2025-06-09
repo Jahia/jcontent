6a00ac3c51db6786698a713fbc34b628eb4967e6
Michael Krasnov <mihalKrasnov@gmail.com>
2025-06-09 11:36:23 -0400

    chore: migrated graphql to groovy for patches

---
+++ src/main/resources/META-INF/patches/groovy/02-componentVisibility.started.groovy
@@ -0,0 +1,37 @@
+import org.jahia.osgi.BundleUtils
+import org.osgi.service.cm.ConfigurationAdmin
+
+def patchConfigForComponentVisibility() {
+
+    def configAdmin = BundleUtils.getOsgiService(ConfigurationAdmin.class, null);
+    def configs = configAdmin.listConfigurations("(service.pid=org.jahia.modules.jcontent)");
+
+    if (configs == null || configs.length == 0) {
+        log.warn("ABACABA No JContent configuration found to update for component visibility.");
+        return;
+    }
+    def config = configs[0];
+    if (config != null) {
+        log.info("Updating JContent configuration for component visibility...");
+        def props = config.getProperties();
+        if (props != null) {
+            // Add or update the property for component visibility
+            props.put("hideLegacyPageComposer", "true");
+            props.put("showPageBuilder", "true");
+            props.put("createChildrenDirectButtons", "true");
+            props.remove("showPageComposer");
+            props.remove("showCatMan");
+
+            config.update(props);
+            log.info("JContent configuration updated for component visibility.");
+        } else {
+            log.warn("No properties found in JContent configuration to update.");
+        }
+
+    } else {
+        log.warn("No JContent configuration found to update.");
+    }
+}
+
+patchConfigForComponentVisibility()
+

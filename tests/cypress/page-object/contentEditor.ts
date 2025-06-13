0ba738ed6f6a2128e1594f764cc53a2b5ddf1341
sagnard <ksagnard@jahia.com>
2025-06-13 14:25:46 +0200

    fix eslint

--- tests/cypress/page-object/contentEditor.ts
+++ tests/cypress/page-object/contentEditor.ts
@@ -106,7 +106,7 @@ export class ContentEditor extends BasePage {
 
     cancel() {
         getComponentByRole(Button, 'backButton').click();
-        getComponentByRole(Button, 'backButton').should('not.be.visible')
+        getComponentByRole(Button, 'backButton').should('not.be.visible');
     }
 
     cancelAndDiscard() {

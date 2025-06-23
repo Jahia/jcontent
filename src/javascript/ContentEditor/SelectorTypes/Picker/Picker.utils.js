2780686deb9108471c1ce591b2e924987d007e54
Geofrey Flores <gflores@jahia.com>
2025-06-23 19:29:31 -0400

    Replace with es-toolkit set function for parsing selector options

--- src/javascript/ContentEditor/SelectorTypes/Picker/Picker.utils.js
+++ src/javascript/ContentEditor/SelectorTypes/Picker/Picker.utils.js
@@ -47,63 +47,6 @@ export const getDetailedPathArray = fullPath => {
         [];
 };
 
-export const set = (target, path, value) => {
-    const splitRes = path.split('.');
-    const regex = /(\[\d+])$/;
-
-    let key;
-    let position;
-    let match;
-    let current = target;
-    while ((splitRes.length > 1) && (key = splitRes.shift())) {
-        // Case items[0].key = 'something'
-        match = regex.exec(key);
-
-        if (match) {
-            key = key.replace(match[0], '');
-            position = parseInt(match[0].replace(/[[\]]/g, ''), 10);
-        }
-
-        if (!current[key]) {
-            if (match) {
-                current[key] = [];
-            } else {
-                current[key] = {};
-            }
-        }
-
-        if (Array.isArray(current[key])) {
-            if (!current[key][position]) {
-                current[key][position] = {};
-            }
-
-            current = current[key][position];
-            match = null;
-            position = null;
-        } else {
-            current = current[key];
-        }
-    }
-
-    key = splitRes.shift();
-
-    // Case items[0] = 'something'
-    match = regex.exec(key);
-
-    if (match) {
-        key = key.replace(match[0], '');
-        position = parseInt(match[0].replace(/[[\]]/g, ''), 10);
-
-        if (!current[key]) {
-            current[key] = [];
-        }
-
-        current[key][position] = value;
-    } else {
-        current[key] = value;
-    }
-};
-
 export const getBaseSearchContextData = ({t, currentSite, accordion, node, currentPath}) => (
     [
         {

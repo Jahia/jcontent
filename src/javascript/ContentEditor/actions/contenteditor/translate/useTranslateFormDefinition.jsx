f8b2dcf9e749b65ebbf7d26c01ad49972b5dd372
Geofrey Flores <gflores@jahia.com>
2025-07-24 14:10:09 -0400

    Filter sections; display all fields and make shared languages field read-only

--- src/javascript/ContentEditor/actions/contenteditor/translate/useTranslateFormDefinition.jsx
+++ src/javascript/ContentEditor/actions/contenteditor/translate/useTranslateFormDefinition.jsx
@@ -6,9 +6,13 @@ export const useTranslateFormDefinition = () => {
     const {data, refetch, loading, error, errorMessage} = useEditFormDefinition();
 
     if (data) {
+        // Display only content and SEO sections in the translation panel
+        data.sections = data.sections.filter(s => ['content', 'seo'].includes(s.name));
         data.sections?.forEach(section => {
             section.fieldSets?.forEach(fieldSet => {
-                fieldSet.fields = fieldSet.fields.filter(field => field.i18n === true);
+                fieldSet.fields.filter(field => !field.i18n).forEach(f => {
+                    f.readOnly = true;
+                });
                 if (sbsContext.readOnly) {
                     fieldSet.readOnly = true;
                     fieldSet.fields.forEach(field => {

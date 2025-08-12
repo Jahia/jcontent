2e6c8ce53ec2f772ca87ff15a12665015a879593
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2025-08-12 09:42:09 -0400

    feat: Add new target for translate UI header actions; Add translate action to edit header (#1954)
    * feat: Add translate action target
    
    * feat: Add sbs translate action to CE modal header
    
    * lint fixes
    
    * Fix copy language visibility jest test

---
+++ src/javascript/ContentEditor/actions/contenteditor/translate/translateEditAction.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {useContentEditorApiContext, useContentEditorContext} from '~/ContentEditor/contexts';
+import {TranslatePanel} from './TranslatePanel';
+import {useTranslateFormDefinition} from './useTranslateFormDefinition';
+
+export const TranslateEditActionComponent = ({path, render: Render, ...otherProps}) => {
+    const api = useContentEditorApiContext();
+    const {nodeData, lang, siteInfo} = useContentEditorContext();
+
+    const languages = siteInfo.languages?.filter(l => l.activeInEdit) || [];
+    const sourceLang = languages.find(l => l.language !== lang) || languages[0];
+
+    return (
+        <Render {...otherProps}
+                isVisible={languages.length > 1}
+                onClick={() => {
+                    api.edit({
+                        uuid: nodeData.uuid,
+                        lang: sourceLang.language,
+                        isFullscreen: true,
+                        dialogProps: {
+                            classes: {}
+                        },
+                        sideBySideContext: {lang},
+                        layout: TranslatePanel,
+                        useFormDefinition: useTranslateFormDefinition
+                    });
+                }}
+        />
+    );
+};
+
+TranslateEditActionComponent.propTypes = {
+    render: PropTypes.func.isRequired,
+    path: PropTypes.string.isRequired
+};
+
+export const translateEditAction = {
+    component: TranslateEditActionComponent
+};

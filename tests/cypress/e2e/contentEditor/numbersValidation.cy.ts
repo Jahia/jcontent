9e1a408544d158c7e801e2a5604f541b784865a8
akarmanov <alexandrk_@hotmail.com>
2025-08-18 12:07:34 -0400

    fix: Use react-number-format as was done in design system

--- tests/cypress/e2e/contentEditor/numbersValidation.cy.ts
+++ tests/cypress/e2e/contentEditor/numbersValidation.cy.ts
@@ -1,6 +1,6 @@
 import {createSite, deleteSite, enableModule} from '@jahia/cypress';
 import {JContent} from '../../page-object';
-import {NumberField} from '../../page-object/fields';
+import {SmallTextField} from '../../page-object/fields';
 
 describe('Numbers validation tests', {testIsolation: false}, () => {
     const siteKey = 'validation';
@@ -26,27 +26,14 @@ describe('Numbers validation tests', {testIsolation: false}, () => {
         cy.contains('There is one validation error').find('a').contains('longReq');
         ce.discardErrorDialog();
 
-        // Incorrect long does not pass validation
-        ce.getField(NumberField, 'cent:numbers_longReq', false).addNewValue('234');
-        ce.getField(NumberField, 'cent:numbers_long', false).addNewValue('2.3');
-        ce.createUnchecked();
-        cy.contains('There is one validation error').find('a').contains('long');
-        ce.discardErrorDialog();
-
-        ce.getField(NumberField, 'cent:numbers_long', false).addNewValue('23');
-        // Note that cypress does not allow to type something like 32.-+323, which would be useful here to test empty value
-        ce.getField(NumberField, 'cent:numbers_longReq', false).addNewValue('32.34');
-        ce.createUnchecked();
-        cy.contains('There is one validation error').find('a').contains('longReq');
-        ce.discardErrorDialog();
-
-        // Content is saved
-        ce.getField(NumberField, 'cent:numbers_longReq', false).addNewValue('234');
-        ce.getField(NumberField, 'cent:numbers_long', false).addNewValue('23');
-        ce.getField(NumberField, 'cent:numbers_double', false).addNewValue('32');
+        // Make sure chars are not allowed
+        ce.getField(SmallTextField, 'cent:numbers_longReq', false).addNewValue('234');
+        ce.getField(SmallTextField, 'cent:numbers_long', false).addNewValue('2.3e-+tty34', false, false);
+        ce.getField(SmallTextField, 'cent:numbers_long', false).checkValue('-2334');
+        ce.getField(SmallTextField, 'cent:numbers_double', false).addNewValue('32.45##df4ee', false, false);
+        ce.getField(SmallTextField, 'cent:numbers_double', false).checkValue('32.454');
         ce.createUnchecked();
 
         cy.contains('There is one validation error').should('not.exist');
     });
 });
-

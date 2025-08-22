697fa0d64da5ceaf9f417b24807f13a38b2083fe
Sonia Dusolle <sdusolle@jahia.com>
2025-08-22 15:50:38 +0200

    test: fix lint issues

--- tests/cypress/e2e/menuActions/visibility.cy.ts
+++ tests/cypress/e2e/menuActions/visibility.cy.ts
@@ -99,18 +99,18 @@ describe('Actions visibility', () => {
         cy.loginAndStoreSession('irina', 'password');
         jcontent = JContent.visit('digitall', 'en', 'content-folders/contents');
 
-        // check new content folder action is not displayed
+        // Check new content folder action is not displayed
         cy.get('div[role="toolbar"]').find('button[data-sel-role="createContentFolder"]').should('not.exist');
         cy.get('div[role="toolbar"]').find('button[data-sel-role="createContent"]').should('not.exist');
 
-        // check action 'new content' under my-list is not displayed
-        const contextMenu = jcontent.openContextMenuByRowName('my-list')
+        // Check action 'new content' under my-list is not displayed
+        const contextMenu = jcontent.openContextMenuByRowName('my-list');
         contextMenu.shouldNotHaveItem('New content');
 
-        // close contextual menu
+        // Close contextual menu
         cy.get('.moonstone-menu_overlay').click({force: true});
 
-        // check actions new constraintChild1... under constraintlist3 are not displayed
+        // Check actions new constraintChild1... under constraintlist3 are not displayed
         const expectedConstraints = [
             'New constraintChild1',
             'New constraintChild2',

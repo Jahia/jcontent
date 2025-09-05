6ba32e2118d1c889078d3002abaa99cd892a50c3
Alex Karmanov <alexandrk_@hotmail.com>
2025-09-05 10:14:18 -0400

    fix: Avoid showing 'unselected' if lang available (#2003)
    * fix: Avoid showing 'unselected' if lang available
    
    * fix: Fix lint

--- src/javascript/ContentEditor/actions/contenteditor/copyLanguage/CopyLanguageDialog.jsx
+++ src/javascript/ContentEditor/actions/contenteditor/copyLanguage/CopyLanguageDialog.jsx
@@ -47,16 +47,14 @@ export const CopyLanguageDialog = ({
         .find(e => e.value === sideBySideContext.lang);
 
     const defaultLanguageOption = useMemo(() => {
-        if (defaultLanguage !== language) {
-            const availableLanguageOption = availableLanguages.find(al => al.language === defaultLanguage);
-            if (availableLanguageOption) {
-                return {
-                    label: availableLanguageOption.uiLanguageDisplayName,
-                    value: availableLanguageOption.language
-                };
-            }
+        const availableLanguageOption = availableLanguages.find(al => al.language === defaultLanguage) || availableLanguages[0];
+        if (availableLanguageOption) {
+            return {
+                label: availableLanguageOption.uiLanguageDisplayName,
+                value: availableLanguageOption.language
+            };
         }
-    }, [defaultLanguage, language, availableLanguages]);
+    }, [defaultLanguage, availableLanguages]);
 
     const defaultOption = sbsOption || {
         label: t('jcontent:label.contentEditor.edit.action.copyLanguage.defaultValue'),
@@ -115,12 +113,12 @@ export const CopyLanguageDialog = ({
                         size="medium"
                         data-sel-role="from-language-selector"
                         isDisabled={Boolean(sbsOption)}
-                        data={[defaultOption].concat(availableLanguages.map(element => {
+                        data={sbsOption || defaultLanguageOption ? availableLanguages.map(element => {
                             return {
                                 value: element.language,
                                 label: element.uiLanguageDisplayName
                             };
-                        }))}
+                        }) : [defaultOption]}
                         onChange={handleOnChange}
                     />}
                 <Typography className={styles.label}>

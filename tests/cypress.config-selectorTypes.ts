6cfab06c846d986ae1351e4a194de2c8d91cca73
Geofrey Flores <gflores@jahia.com>
2025-09-05 15:59:04 -0400

    Refactor and adding a new parallel test suite for CE selector type tests

---
+++ tests/cypress.config-selectorTypes.ts
@@ -0,0 +1,6 @@
+import {defineConfig} from 'cypress';
+import {baseConfig} from './cypress.config.common';
+
+baseConfig.e2e.specPattern = ['cypress/e2e/selectorTypes/**/**.cy.ts'];
+
+export default defineConfig(baseConfig);

a39b5bc30c919bb572b880d959e2d1f5000dfbb1
akarmanov <alexandrk_@hotmail.com>
2025-11-10 11:15:38 -0500

    fix: Do not require field to be focused

--- tests/cypress/e2e/menuActions/translate.cy.ts
+++ tests/cypress/e2e/menuActions/translate.cy.ts
@@ -159,9 +159,7 @@ describe('translate action tests', () => {
 
         cy.log('Add user translation in de for long field');
         cy.waitUntil(() => translateEditor.getTranslateLanguageSwitcher().selectLangByValue('de'));
-        // Need to make sure focus on first field in source column is focused before typing
         translateEditor.getSourceColumn().get().find('.moonstone-loader', {timeout: 5000}).should('not.exist');
-        translateEditor.getSourceFields().find('input');
         translateEditor
             .getTranslateField(SmallTextField, 'qant:allFields_long')
             .addNewValue('123');

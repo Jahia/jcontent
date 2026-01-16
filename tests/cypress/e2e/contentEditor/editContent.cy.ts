4f3641c505433d4992fc98c5700a760b9e75d681
Sonia Dusolle <sdusolle@jahia.com>
2026-01-16 10:33:19 +0100

    test: migrate testWarningBadge and testSharedByAllLanguages to Cypress

--- tests/cypress/e2e/contentEditor/editContent.cy.ts
+++ tests/cypress/e2e/contentEditor/editContent.cy.ts
@@ -1,6 +1,6 @@
 import {ContentEditor, JContent} from '../../page-object';
 import {RichTextField} from '../../page-object/fields';
-import {addNode, createSite, deleteSite} from '@jahia/cypress';
+import {addNode, createSite, deleteSite, enableModule} from '@jahia/cypress';
 
 describe('Create content tests', () => {
     let jcontent: JContent;
@@ -9,6 +9,13 @@ describe('Create content tests', () => {
     before(() => {
         createSite(siteKey);
         cy.apollo({mutationFile: 'contentEditor/references.graphql'});
+        enableModule('qa-module', siteKey);
+        addNode({
+            parentPathOrId: `/sites/${siteKey}/contents`,
+            name: 'AllFieldsSimple',
+            primaryNodeType: 'qant:allFields',
+            properties: [{name: "sharedSmallText", value: "Initial text", language: "en"}, {name: "sharedTextarea", value: "Initial text in area", language: "en"}]
+        });
         addNode({
             parentPathOrId: `/sites/${siteKey}/contents`,
             name: 'My simple text',
@@ -25,6 +32,10 @@ describe('Create content tests', () => {
         cy.loginAndStoreSession();
         jcontent = JContent.visit(siteKey, 'en', 'content-folders/contents');
     });
+
+    const fieldsWithoutBadge = [
+        "qant:allFields_smallText", "qant:allFields_textarea" , "qant:allFields_sharedSmallText", "qant:allFields_sharedTextarea"
+    ];
 
     it('can edit content', () => {
         jcontent.getTable().getRowByLabel('Rich text').contextMenu().select('Edit');
@@ -57,5 +68,19 @@ describe('Create content tests', () => {
         contentEditor.save();
         // Check unsaved badge is not displayed
         cy.get('div[data-sel-role="unsaved-info-chip"]', {timeout: 5000}).should('not.exist');
+    });
+
+    it('Checks "shared by all languages" badge', () => {
+        jcontent.editComponentByRowName('AllFieldsSimple');
+        const contentEditor = new ContentEditor();
+        contentEditor.switchToAdvancedMode();
+
+        // Check "shared by all languages" badge is NOT displayed on non internationalized fields
+        fieldsWithoutBadge.forEach((field) => {
+            cy.get(`[data-sel-content-editor-field="${field}"]`)
+                .find('.moonstone-chip')
+                .should('not.exist');
+        });
+        contentEditor.cancel();
     });
 });

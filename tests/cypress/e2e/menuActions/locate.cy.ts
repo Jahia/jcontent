9603dd2f36a39bfe4ccd538a3ca3372c65cec89d
Sonia Dusolle <sdusolle@jahia.com>
2026-02-19 14:24:54 +0100

    test: add Cypress test on locate action

---
+++ tests/cypress/e2e/menuActions/locate.cy.ts
@@ -0,0 +1,73 @@
+import {JContent} from '../../page-object';
+import {addNode, createSite, deleteSite} from "@jahia/cypress";
+
+describe('Locate action', () => {
+    let jcontent: JContent;
+    const siteKey = 'jContentSite-locate';
+
+    before('Add all needed data', () => {
+        cy.login();
+        createSite(siteKey);
+        jcontent = JContent.visit(siteKey, 'en', 'media/files');
+        jcontent.getMedia()
+            .open()
+            .uploadFileViaDragAndDrop("myfile.pdf", 'assets/uploadMedia');
+        addNode({
+            parentPathOrId: `/sites/${siteKey}/home`,
+            name: 'page-ex',
+            primaryNodeType: 'jnt:page',
+            properties: [
+                {name: 'jcr:title', value: 'page-ex', language: 'en'},
+                {name: 'j:templateName', type: 'STRING', value: 'simple'}
+            ],
+            children: [{
+                name: 'area-main',
+                primaryNodeType: 'jnt:contentList',
+                children: [{
+                    name: 'rich-text-ex',
+                    primaryNodeType: 'jnt:bigText',
+                    properties: [{name: 'text', language: 'en', value: 'my example of richtext'}]
+                }]
+            }]
+        });
+    });
+
+    after(() => {
+        deleteSite(siteKey);
+        cy.logout();
+    });
+
+
+    it('Tests locate action', () => {
+        jcontent = JContent.visit(siteKey, 'en', 'pages/home');
+        // Open search
+        jcontent.getBasicSearch().openSearch().searchInWholeSite().searchTerm('example').executeSearch()
+            .verifyResults(['myfile.pdf (Document1)']);
+
+        // Open contextual menu and select locate
+        const menu = jcontent.getTable().getRowByName('myfile.pdf')
+            .contextMenu();
+        menu.shouldHaveItem('Locate');
+        menu.select('Locate');
+
+        // Verfiy we are redirected to media > thumbnails view
+        jcontent.shouldBeInMode('Thumbnails');
+        cy.get('h1').contains('files');
+        jcontent.getGrid().getCardByName('myfile.pdf').should('be.visible');
+
+        // Open again search
+        jcontent.getBasicSearch().openSearch().searchInWholeSite().searchTerm('example').executeSearch()
+            .verifyResults(['my example of richtext']);
+
+        // Open contextual menu and select locate
+        jcontent.getTable().getRowByName('rich-text-ex')
+            .contextMenu();
+        menu.shouldHaveItem('Locate');
+        menu.select('Locate');
+
+        // Verfiy we are redirected to pages > page-ex
+        cy.get('h1').contains('page-ex');
+        jcontent.switchToListMode();
+        jcontent.getTable().getRowByName('rich-text-ex').should('be.visible');
+    });
+});

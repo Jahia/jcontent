2b1ce42be4b1be72be92f4266e09591b15ec967e
Sonia Dusolle <sdusolle@jahia.com>
2026-04-15 14:05:51 +0200

    test: add contentEditorFieldsTest to Cypress

---
+++ tests/cypress/e2e/contentEditor/contentEditorFields.cy.ts
@@ -0,0 +1,316 @@
+import {JContent} from '../../page-object/jcontent';
+import {
+    addNode,
+    enableModule
+} from '@jahia/cypress';
+
+describe('Content editor fields tests', () => {
+    let jcontent: JContent;
+    const siteKey = 'contentEditorFieldsSite';
+
+    before(function () {
+        cy.executeGroovy('contentEditor/createSiteI18N.groovy', {SITEKEY: siteKey});
+        enableModule('qa-module', siteKey);
+        addNode({
+            parentPathOrId: `/sites/${siteKey}/contents`,
+            name: 'protectedFields',
+            primaryNodeType: 'qant:protectedFields'
+        });
+        addNode({
+            parentPathOrId: `/sites/${siteKey}/contents`,
+            name: 'allFieldsSimple',
+            primaryNodeType: 'qant:allFields'
+        });
+        addNode({
+            parentPathOrId: `/sites/${siteKey}/contents`,
+            name: 'allFieldsMultiple',
+            primaryNodeType: 'qant:allFieldsMultiple'
+        });
+    });
+
+    after(function () {
+        cy.logout();
+        cy.executeGroovy('contentEditor/deleteSite.groovy', {SITEKEY: siteKey});
+    });
+
+    beforeEach(() => {
+        cy.loginAndStoreSession();
+        jcontent = JContent.visit(siteKey, 'en', 'content-folders/contents');
+    });
+
+    it('should add and remove multiple values on small text field', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsMultiple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_smallText"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_smallText[0]"]')
+            .find('input')
+            .type('testa=a');
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_smallText"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_smallText[1]"]')
+            .find('input')
+            .type('testb=b');
+        contentEditor.save();
+
+        cy.get('input[name="qant:allFieldsMultiple_smallText[0]"]').should('have.value', 'testa=a');
+        cy.get('input[name="qant:allFieldsMultiple_smallText[1]"]').should('have.value', 'testb=b');
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_smallText[0]"]')
+            .find('[data-sel-action="removeField_0"]')
+            .click();
+        cy.get('input[name="qant:allFieldsMultiple_smallText[0]"]').should('have.value', 'testb=b');
+
+        cy.get('input[name="qant:allFieldsMultiple_smallText[0]"]').clear().type('testb=c', {force: true});
+        contentEditor.save();
+        cy.get('input[name="qant:allFieldsMultiple_smallText[0]"]').should('have.value', 'testb=c');
+    });
+
+    it('should add and remove multiple values on text area field', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsMultiple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_textarea"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_textarea[0]"]')
+            .find('textarea')
+            .type('valueA');
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_textarea"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_textarea[1]"]')
+            .find('textarea')
+            .type('valueB');
+        contentEditor.save();
+
+        cy.get('textarea[name="qant:allFieldsMultiple_textarea[0]"]').should('have.value', 'valueA');
+        cy.get('textarea[name="qant:allFieldsMultiple_textarea[1]"]').should('have.value', 'valueB');
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_textarea[0]"]')
+            .find('[data-sel-action="removeField_0"]')
+            .click();
+        cy.get('textarea[name="qant:allFieldsMultiple_textarea[0]"]').should('have.value', 'valueB');
+    });
+
+    it('should add and remove multiple values on long field', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsMultiple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_long"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_long[0]"]')
+            .find('input')
+            .type('123456789');
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_long"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_long[1]"]')
+            .find('input')
+            .type('987654321');
+        contentEditor.save();
+
+        cy.get('input[name="qant:allFieldsMultiple_long[0]"]').should('have.value', '123456789');
+        cy.get('input[name="qant:allFieldsMultiple_long[1]"]').should('have.value', '987654321');
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_long[0]"]')
+            .find('[data-sel-action="removeField_0"]')
+            .click();
+        cy.get('input[name="qant:allFieldsMultiple_long[0]"]').should('have.value', '987654321');
+    });
+
+    it('should add and remove multiple values on double field', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsMultiple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_double"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_double[0]"]')
+            .find('input')
+            .type('1.1');
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_double"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_double[1]"]')
+            .find('input')
+            .type('2.2');
+        contentEditor.save();
+
+        cy.get('input[name="qant:allFieldsMultiple_double[0]"]').should('have.value', '1.1');
+        cy.get('input[name="qant:allFieldsMultiple_double[1]"]').should('have.value', '2.2');
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_double[0]"]')
+            .find('[data-sel-action="removeField_0"]')
+            .click();
+        cy.get('input[name="qant:allFieldsMultiple_double[0]"]').should('have.value', '2.2');
+    });
+
+    it('should add and remove multiple boolean', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsMultiple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_boolean"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_boolean[0]"]')
+            .find('input[type="checkbox"]')
+            .check({force: true});
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_boolean"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_boolean[1]"]')
+            .find('input[type="checkbox"]')
+        contentEditor.save();
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_boolean[0]"]')
+            .find('input[type="checkbox"]')
+            .should('have.attr', 'aria-checked', 'true');
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_boolean[1]"]')
+            .find('input[type="checkbox"]')
+            .should('have.attr', 'aria-checked', 'false');
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_boolean[0]"]')
+            .find('[data-sel-action="removeField_0"]')
+            .click();
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_boolean[0]"]')
+            .find('input[type="checkbox"]')
+            .should('have.attr', 'aria-checked', 'false');
+    });
+
+
+    it('should add and remove multiple values on date field', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsMultiple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_date"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_date[0]"]')
+            .find('input[type="text"]')
+            .type('25/12/2024 10:30');
+
+        cy.get('[data-sel-content-editor-field="qant:allFieldsMultiple_date"]')
+            .find('[data-sel-action="addField"]')
+            .click();
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_date[1]"]')
+            .find('input[type="text"]')
+            .type('10/11/2025 20:30');
+
+        cy.get('input[id="qant:allFieldsMultiple_date[0]"]').should('have.value', '25/12/2024 10:30');
+        cy.get('input[id="qant:allFieldsMultiple_date[1]"]').should('have.value', '10/11/2025 20:30');
+
+        cy.get('[data-sel-content-editor-multiple-generic-field="qant:allFieldsMultiple_date[0]"]')
+            .find('[data-sel-action="removeField_0"]')
+            .click();
+        cy.get('input[id="qant:allFieldsMultiple_date[0]"]').should('have.value', '10/11/2025 20:30');
+        contentEditor.cancelAndDiscard();
+    });
+
+
+    it('should add values to number fields', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsSimple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_long"]')
+            .find('input')
+            .type('123456789');
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_double"]')
+            .find('input')
+            .type('20192.75');
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_decimal"]')
+            .find('input')
+            .type('20192019201920192019201920192019.75757575757575757575751');
+        contentEditor.save();
+
+        cy.get('input[name="qant:allFields_long"]').should('have.value', '123456789');
+        cy.get('input[name="qant:allFields_double"]').should('have.value', '20192.75');
+        cy.get('input[name="qant:allFields_decimal"]').should('have.value', '20192019201920192019201920192019.75757575757575757575751');
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_long"]')
+            .find('input')
+            .clear()
+            .type('123456789.abcd123456789', {force: true});
+        cy.get('input[name="qant:allFields_long"]').should('have.value', '123456789123456789');
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_double"]')
+            .find('input')
+            .clear()
+            .type('20192.75.abcd', {force: true});
+        cy.get('input[name="qant:allFields_double"]').should('have.value', '20192.75');
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_decimal"]')
+            .find('input')
+            .clear()
+            .type('20192019201920192019201920192019.75757575757575757575751.abcd', {force: true});
+        cy.get('input[name="qant:allFields_decimal"]').should('have.value', '20192019201920192019201920192019.75757575757575757575751');
+    });
+
+    it('should add values to text area', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsSimple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_textarea"]')
+            .find('textarea')
+            .type('This is my text');
+        contentEditor.save();
+        cy.get('textarea[name="qant:allFields_textarea"]').should('have.value', 'This is my text');
+
+        contentEditor.getLanguageSwitcherAdvancedMode().selectLangByValue('fr');
+        cy.get('[data-sel-content-editor-field="qant:allFields_textarea"]')
+            .find('textarea')
+            .type('Voici mon texte');
+        cy.get('textarea[name="qant:allFields_textarea"]').should('have.value', 'Voici mon texte');
+        contentEditor.getLanguageSwitcherAdvancedMode().selectLangByValue('en');
+        cy.get('textarea[name="qant:allFields_textarea"]').should('have.value', 'This is my text');
+    });
+
+    it('check read only fields on protectedFields', () => {
+        const contentEditor = jcontent.editComponentByRowName('protectedFields');
+        contentEditor.switchToAdvancedMode();
+
+        const protectedFields = [
+            'qant:protectedFields_protectedText',
+            'qant:protectedFields_protectedTextarea',
+            'qant:protectedFields_protectedLong',
+            'qant:protectedFields_protectedBoolean',
+            'qant:protectedFields_protectedDate',
+            'qant:protectedFields_protectedChoicelist',
+            'qant:protectedFields_protectedDynamicChoicelist',
+        ];
+
+        protectedFields.forEach(fieldName => {
+            cy.get(`[data-sel-content-editor-field="${fieldName}"]`)
+                .should('have.attr', 'data-sel-content-editor-field-readonly', 'true');
+        });
+        contentEditor.cancel();
+    });
+
+    it('check ckeditor field', () => {
+        const contentEditor = jcontent.editComponentByRowName('allFieldsSimple');
+        contentEditor.switchToAdvancedMode();
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_sharedBigtext"]')
+            .find('.cke_button__bold')
+            .should('exist')
+            .and('have.attr', 'title', 'Bold (Ctrl+B)');
+
+        cy.get('[data-sel-content-editor-field="qant:allFields_offersMinTsAndCsFinanceStandard"]')
+            .find('.cke_button__language')
+            .should('exist')
+            .and('have.attr', 'title', 'Set language');
+        contentEditor.cancel();
+    });
+});

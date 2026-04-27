d1723f9dddb4dca7b898835ce2f151a190306000
Alex Karmanov <alexandrk_@hotmail.com>
2026-04-27 09:06:45 -0400

    fix: Update permission scripts to ensure permissions added to right nodes (#2291)
    * fix: Update permission scripts to ensure permissions added to right nodes
    
    * Change jcontent version bump from patch to minor
    
    Update permission scripts to ensure jContentAccordions is added to right nodes (#2291)

--- src/main/resources/META-INF/patches/groovy/01-permissions.started.groovy
+++ src/main/resources/META-INF/patches/groovy/01-permissions.started.groovy
@@ -5,27 +5,39 @@ import org.jahia.services.content.JCRTemplate
 import javax.jcr.RepositoryException
 
 def updateAccordionPermissions() {
-    JCRTemplate.getInstance().doExecuteWithSystemSession (new JCRCallback<Void>(){
+    JCRTemplate.getInstance().doExecuteWithSystemSession(new JCRCallback<Void>() {
 
         @Override
         Void doInJCR(JCRSessionWrapper jcrSessionWrapper) throws RepositoryException {
-            log.info("Updating permissions for jContentAccordions...");
-            def roleNames = List.of("editor", "reviewer", "translator");
-            roleNames.stream().forEach(roleName -> {
+            log.info("Updating permissions for jContentAccordions...")
+            def roleNames = List.of("editor", "reviewer", "translator")
+            roleNames.forEach(roleName -> {
                 try {
-                    def role = jcrSessionWrapper.getNode("/roles/" + roleName);
-                    if (role != null) {
-                        log.info("Adding jContentAccordions permission to role: " + roleName);
-                        role.getProperty("j:permissionNames").addValue("jContentAccordions");
+                    def rolePath = "/roles/" + roleName
+                    if (!jcrSessionWrapper.nodeExists(rolePath)) {
+                        log.warn("Role node not found: " + rolePath)
+                        return
+                    }
+                    def role = jcrSessionWrapper.getNode(rolePath)
+                    if (!role.hasNode("currentSite-access")) {
+                        log.warn("currentSite-access child not found under role: " + roleName)
+                        return
+                    }
+                    def siteAccess = role.getNode("currentSite-access")
+                    log.info("Adding jContentAccordions permission to role: " + roleName + " (currentSite-access)")
+                    if (siteAccess.hasProperty("j:permissionNames")) {
+                        siteAccess.getProperty("j:permissionNames").addValue("jContentAccordions")
+                    } else {
+                        siteAccess.setProperty("j:permissionNames", ["jContentAccordions"] as String[])
                     }
                 } catch (RepositoryException e) {
-                    log.error("Error updating permissions for role: " + roleName, e);
+                    log.error("Error updating permissions for role: " + roleName, e)
                 }
-            });
-            jcrSessionWrapper.save();
-            return null;
+            })
+            jcrSessionWrapper.save()
+            return null
         }
-    });
+    })
 }
 
-updateAccordionPermissions();
+updateAccordionPermissions()

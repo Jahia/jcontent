96ad099122c0100e34ddcdf574f0601633667792
akarmanov <alexandrk_@hotmail.com>
2026-05-04 11:50:16 -0400

    fix: Update test

--- tests/cypress/e2e/pageBuilder/areaRenderingTest.cy.ts
+++ tests/cypress/e2e/pageBuilder/areaRenderingTest.cy.ts
@@ -1,4 +1,4 @@
-import {JContent} from '../../page-object';
+import {JContent, JContentPageBuilder} from '../../page-object';
 import {addNode, createSite, deleteSite} from '@jahia/cypress';
 
 describe('Area rendering test', () => {
@@ -28,8 +28,30 @@ describe('Area rendering test', () => {
 
     it('Allows to edit and create content in an absolute area with site and home page as parent', () => {
         cy.login();
-        const jcontentPageBuilder = JContent.visit(siteKey, 'en', 'pages/home')
-            .switchToPageBuilder();
-        cy.wait(10000);
+
+        // Sometimes pagebuilder js is running a little behind and requires refreshing to see boxes, navigating to another page solves the issue
+        let pageBuilder: JContentPageBuilder = JContent.visit(siteKey, 'en', 'pages/home').switchToPageBuilder();
+        pageBuilder = JContent.visit(siteKey, 'en', 'pages/home/testPage').switchToPageBuilder();
+        let module = pageBuilder.getModule(`/sites/${siteKey}/aaSiteRootLinks`);
+        module.getBox().getHeader().assertHeaderText('aaSiteRootLinks');
+        module.click({force: true});
+        module.getCreateButtons().should('have.length', 2);
+
+        module = pageBuilder.getModule(`/sites/${siteKey}/home/aaHomeRootLinks`);
+        module.getBox().getHeader().assertHeaderText('aaHomeRootLinks');
+        module.click({force: true});
+        module.getCreateButtons().should('have.length', 2);
+
+        // Same areas should be visible and editable on the home page
+        pageBuilder = JContent.visit(siteKey, 'en', 'pages/home').switchToPageBuilder();
+        module = pageBuilder.getModule(`/sites/${siteKey}/aaSiteRootLinks`);
+        module.getBox().getHeader().assertHeaderText('aaSiteRootLinks');
+        module.click({force: true});
+        module.getCreateButtons().should('have.length', 2);
+
+        module = pageBuilder.getModule(`/sites/${siteKey}/home/aaHomeRootLinks`);
+        module.getBox().getHeader().assertHeaderText('aaHomeRootLinks');
+        module.click({force: true});
+        module.getCreateButtons().should('have.length', 2);
     });
 });

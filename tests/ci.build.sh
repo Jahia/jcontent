f3e2ec93d13fc606636230fb7ccee049cd84bc65
akarmanov <alexandrk_@hotmail.com>
2026-05-05 07:42:23 -0400

    fix: Intermediate updates

--- tests/ci.build.sh
+++ tests/ci.build.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
 version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
 echo Using @jahia/cypress@$version...
+
+echo "Copy .tgz files"
+
+if [[ -f .env ]]; then
+  source .env
+  export $(cat .env | sed 's/=.*//g'| xargs)
+else
+  source .env.example
+  export $(cat .env.example | sed 's/=.*//g'| xargs)
+fi
+
+if [ ! -d ./artifacts ]; then
+  mkdir -p ./artifacts
+fi
+
+if [ -d ./jahia-module ]; then
+  cd jahia-module
+  echo "Contents of jahia-module"
+  ls -al
+
+  if [ -e "pom.xml" ]; then
+    mvn clean install
+    echo "Looking up snapshot tgz"
+    find . -type f -name "*-SNAPSHOT.tgz"
+    find . -type f -name "*-SNAPSHOT.tgz" -exec cp {} ../artifacts/ \;
+  fi
+  cd ..
+fi
+
 npx --yes --package @jahia/cypress@$version ci.build

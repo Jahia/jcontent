dcc60009eaaee57d561a7dc7287fb04752c2d5d0
akarmanov <alexandrk_@hotmail.com>
2026-05-05 07:07:59 -0400

    fix: Try to update build to move tgz files

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
 echo Using @jahia/cypress@$version...
+
+npx --yes --package @jahia/cypress@$version set-env
+
+if [ ! -d ./artifacts ]; then
+  mkdir -p ./artifacts
+fi
+
+if [ -d ./jahia-module ]; then
+  cd jahia-module
+  if [ -e "pom.xml" ]; then
+    mvn clean install
+    find . -type f -name "*-SNAPSHOT.tgz" -exec cp {} ../artifacts/ \;
+  fi
+  cd ..
+fi
+
 npx --yes --package @jahia/cypress@$version env.run

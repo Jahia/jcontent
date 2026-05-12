f5319832a06aa0f8517d2fec2d802753862975dc
david.griffon <dgriffon@jahia.com>
2026-05-12 16:00:39 +0200

    add cypress test + test config for one test only

---
+++ tests/jahia-module/jcontent-test-module/src/main/java/org/jahia/modules/choicelistinitializer/MyLinkTypeInitializer.java
@@ -0,0 +1,52 @@
+package org.jahia.modules.choicelistinitializer;
+
+import org.apache.jackrabbit.value.ValueFactoryImpl;
+import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
+import org.jahia.services.content.nodetypes.initializers.ChoiceListValue;
+import org.jahia.services.content.nodetypes.initializers.ModuleChoiceListInitializer;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import javax.jcr.Value;
+import java.util.*;
+
+@Component(service = {ModuleChoiceListInitializer.class})
+public class MyLinkTypeInitializer implements ModuleChoiceListInitializer {
+    private String key;
+    @Activate
+    public void activate(Map<String, ?> props) {
+
+    }
+
+    @Override
+    public String getKey() {
+        return "myLinkTypeInitializer";
+    }
+
+    @Override
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+
+    @Override
+    public List<ChoiceListValue> getChoiceListValues(ExtendedPropertyDefinition extendedPropertyDefinition, String s, List<ChoiceListValue> list, Locale locale, Map<String, Object> map) {
+       ChoiceListValue internalValue = new ChoiceListValue();
+       internalValue.setValue(ValueFactoryImpl.getInstance().createValue("internalLink"));
+       internalValue.setDisplayName("internalLink");
+       Map<String, Object> properties = new HashMap<>();
+       properties.put("addMixin", "cemix:internalLink");
+       internalValue.setProperties(properties);
+
+        ChoiceListValue externalValue = new ChoiceListValue();
+        Value externalLink = ValueFactoryImpl.getInstance().createValue("externalLink");
+        externalValue.setDisplayName("externalLink");
+        externalValue.setValue(externalLink);
+        Map<String, Object> properties1 = new HashMap<>();
+        properties1.put("addMixin", "cemix:externalLink");
+        externalValue.setProperties(properties1);
+
+
+       return Arrays.asList(internalValue, externalValue);
+    }
+}

76eeb90b385959c022d7b9f129570696a976ea32
Sonia Dusolle <42536778+sdusolle@users.noreply.github.com>
2026-05-22 14:44:16 +0200

    test: move tags tests to Cypress (#2274)
    * test: move tags tests to Cypress
    
    * test: add timeouts
    
    * test: reduce delay from 500 to 100
    
    * test: rework should not add duplicate tags test
    
    * test: increase timeout
    
    * test: modify should add a tag
    
    * test: fix auto-completion
    
    * test: fixed should have auto-completion test + created helpers in TagField

--- tests/cypress/page-object/fields/tagField.ts
+++ tests/cypress/page-object/fields/tagField.ts
@@ -1,8 +1,24 @@
 import {Field} from './field';
 
 export class TagField extends Field {
+    type(text: string, options = {delay: 100, force: true}) {
+        return this.get().find('input[type="text"]').type(text, options);
+    }
+
+    clear() {
+        return this.get().find('input[type="text"]').clear();
+    }
+
     addNewValue(text: string): void {
-        this.get().find(`#${this.fieldName}`).type(`${text}{enter}`, {delay: 500});
-        this.get().find(`#${this.fieldName} [role="button"]`).contains(text).should('be.visible');
+        this.type(`${text}{enter}`);
+        this.getTags().contains(text).should('be.visible');
+    }
+
+    getTags() {
+        return this.get().find('[role="button"]');
+    }
+
+    assertTagText(text: string, index: number): void {
+        this.getTags().eq(index).should('have.text', text);
     }
 }

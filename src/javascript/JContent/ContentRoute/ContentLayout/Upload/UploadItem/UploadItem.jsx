Jahia/jcontent

commit 3ede5420eed6aa983023d65347f0ccd06eba47dc

fix: Update file upload naming (#2058)
* fix: temp commit

* fix: Update mutation

* fix: Update system name sanitizer

* fix: Update system name sanitizer to produce lowercase names

* fix: Update test

src/javascript/JContent/ContentRoute/ContentLayout/Upload/UploadItem/UploadItem.jsx
@@ -10,7 +10,7 @@ import {registry} from '@jahia/ui-extender';
 import JContentConstants from '~/JContent/JContent.constants';
 import styles from './UploadItem.scss';
 import {useApolloClient} from '@apollo/client';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {
     NUMBER_OF_SIMULTANEOUS_UPLOADS,
     uploadStatuses
@@ -62,6 +62,7 @@ export const UploadItem = ({upload, index}) => {
     const {t} = useTranslation();
     const {file, entry} = upload;
     const client = useApolloClient();
+    const {lang} = useSelector(state => ({lang: state.language}));
 
     const getFileName = useCallback(() => userChosenName ? userChosenName : (file ? file.name : entry.name).normalize('NFC'), [entry, file, userChosenName]);
 
@@ -95,8 +96,8 @@ export const UploadItem = ({upload, index}) => {
         }
 
         const filename = getFileName();
-        return registry.get('fileUpload', 'default').handleUpload({path: normalizedPath, file, filename, client});
-    }, [client, getFileName, upload]);
+        return registry.get('fileUpload', 'default').handleUpload({path: normalizedPath, file, filename, client, lang});
+    }, [client, getFileName, upload, lang]);
 
     const doUploadAndStatusUpdate = useCallback((type = upload.type) => {
         const newUpload = {

src/javascript/JContent/ContentRoute/ContentLayout/Upload/Upload.redux.js
@@ -3,6 +3,7 @@ import {createActions, handleActions} from 'redux-actions';
 import {importContent, updateFileContent, uploadFile} from './UploadItem/UploadItem.gql-mutations';
 import JContentConstants from '~/JContent/JContent.constants';
 import {getUploadedFileMimeType} from '../ContentLayout.utils';
+import {sanitizeName} from '~/JContent/ContentRoute/ContentLayout/Upload/Upload.utils';
 
 export const {fileuploadSetStatus, fileuploadSetUploads, fileuploadAddUploads, fileuploadUpdateUpload, fileuploadRemoveUpload, fileuploadTakeFromQueue} =
     createActions('FILEUPLOAD_SET_STATUS', 'FILEUPLOAD_SET_UPLOADS', 'FILEUPLOAD_ADD_UPLOADS', 'FILEUPLOAD_UPDATE_UPLOAD', 'FILEUPLOAD_REMOVE_UPLOAD', 'FILEUPLOAD_TAKE_FROM_QUEUE');
@@ -105,7 +106,7 @@ export const fileuploadRedux = registry => {
     });
 
     registry.add('fileUpload', 'default', {
-        handleUpload: ({path, file, filename, client}) => {
+        handleUpload: ({path, file, filename, client, lang}) => {
             if (filename.length > contextJsParameters.config.maxNameSize) {
                 throw new Error(uploadErrors.FILE_NAME_SIZE);
             }
@@ -118,7 +119,9 @@ export const fileuploadRedux = registry => {
                 mutation: uploadFile,
                 variables: {
                     fileHandle: file,
-                    nameInJCR: filename,
+                    nameInJCR: sanitizeName(filename),
+                    title: filename,
+                    lang: lang,
                     path: path,
                     mimeType: getUploadedFileMimeType(file)
                 }

src/javascript/JContent/ContentRoute/ContentLayout/Upload/Upload.utils.js
@@ -114,3 +114,22 @@ export const createMissingFolders = async (client, directories) => {
         created, exists, cannotCreate
     };
 };
+
+export const sanitizeName = name => {
+    name = name.trim();
+    // Normalize Unicode and remove diacritics
+    name = name.normalize('NFKD').replace(/[\u0300-\u036F]/g, '');
+
+    // Split extension if present
+    const parts = name.split('.');
+    const ext = parts.length > 1 ? parts.pop().toLowerCase() : '';
+    let base = parts.join('.');
+
+    // Replace invalid characters (but keep Unicode letters/numbers)
+    base = base.toLowerCase();
+    base = base.replace(/[^\p{L}\p{N}._-]+/gu, '-');
+    base = base.replace(/-+/g, '-');
+    base = base.replace(/^[-_.]+|[-_.]+$/g, '');
+
+    return ext ? `${base}.${ext}` : base;
+};

src/javascript/JContent/ContentRoute/ContentLayout/Upload/UploadItem/UploadItem.gql-mutations.js
@@ -1,8 +1,11 @@
 import gql from 'graphql-tag';
 
-export const uploadFile = gql`mutation uploadFile($nameInJCR: String!, $path: String!, $mimeType: String!, $fileHandle: String!) {
+export const uploadFile = gql`mutation uploadFile($nameInJCR: String!, $path: String!, $mimeType: String!, $fileHandle: String!, $title: String!, $lang: String!) {
     jcr {
         addNode(name: $nameInJCR, parentPathOrId: $path, primaryNodeType: "jnt:file") {
+            mutateProperty(name: "jcr:title") {
+                setValue(language: $lang, value: $title)
+            }
             addChild(name: "jcr:content", primaryNodeType: "jnt:resource") {
                 content: mutateProperty(name: "jcr:data") {
                     setValue(type: BINARY, value: $fileHandle)

src/javascript/JContent/JContent.constants.js
@@ -1,5 +1,5 @@
 const JContentConstants = {
-    namingInvalidCharactersRegexp: /[\\/:*?"<>|%]/g,
+    namingInvalidCharactersRegexp: /[\\/:*?"'<>|%]/g,
     maxCreateContentOfTypeDirectItems: 5,
     availablePublicationStatuses: {
         PUBLISHED: 'PUBLISHED',

tests/cypress/e2e/contentEditor/createContentI18N.cy.ts
@@ -104,22 +104,22 @@ describe('Create content tests in I18N site', () => {
                 // eslint-disable-next-line cypress/no-unnecessary-waiting
                 cy.wait(2000);
                 cy.apollo({mutationFile: 'contentEditor/createContent/addJcrTitleWithLang.graphql', variables: {
-                    path: `/sites/${sitekey}/files/${fileName}`,
+                    path: `/sites/${sitekey}/files/${fileName.toLowerCase()}`,
                     value: 'No lang'
                 }});
 
                 cy.apollo({mutationFile: 'contentEditor/createContent/addJcrTitleWithLang.graphql', variables: {
-                    path: `/sites/${sitekey}/files/${fileName}`,
+                    path: `/sites/${sitekey}/files/${fileName.toLowerCase()}`,
                     value: 'With lang',
                     lang: 'en'
                 }});
 
                 jcontent.switchToListMode();
-                let contentEditor = jcontent.editComponentByText(fileName);
+                let contentEditor = jcontent.editComponentByText(fileName.toLowerCase());
                 contentEditor.getSmallTextField(fieldName).checkValue('With lang');
                 contentEditor.getSmallTextField(fieldName).addNewValue('New value', true);
                 contentEditor.save();
-                contentEditor = jcontent.editComponentByText(fileName);
+                contentEditor = jcontent.editComponentByText(fileName.toLowerCase());
                 contentEditor.getSmallTextField(fieldName).checkValue('New value');
             });
     });

tests/cypress/e2e/selectorTypes/addMixinChoicelistInitializers.cy.ts
@@ -19,7 +19,7 @@ describe('Add Mixin by using choice list initializers (Image Reference)', () => 
                 action: 'drag-drop',
                 waitForAnimations: true
             });
-        cy.get('[data-sel-role-card="snowbearHome.jpeg"]').should('be.visible');
+        cy.get('[data-sel-role-card="snowbearhome.jpeg"]').should('be.visible');
     });
 
     after(function () {
@@ -98,19 +98,19 @@ describe('Add Mixin by using choice list initializers (Image Reference)', () => 
         cy.get('[data-sel-content-editor-field="jnt\\:imageReferenceLink_j\\:node"]')
             .should('be.visible')
             .click();
-        cy.get('[data-sel-role-card="snowbearHome.jpeg"]').should('be.visible').contains('snowbearHome.jpeg').click();
+        cy.get('[data-sel-role-card="snowbearhome.jpeg"]').should('be.visible').contains('snowbearhome.jpeg').click();
         cy.get('button[data-sel-picker-dialog-action="done"]').click();
         contentEditor.create();
         pageComposer
             .refresh()
             .componentShouldBeVisible(
-                `a[href*="/sites/${sitekey}/home/search-results.html"] > img[src*="/sites/${sitekey}/files/snowbearHome.jpeg"]`
+                `a[href*="/sites/${sitekey}/home/search-results.html"] > img[src*="/sites/${sitekey}/files/snowbearhome.jpeg"]`
             );
     });
 
     it('Can edit a document manager image reference link', () => {
         const contentEditor = pageComposer.editComponent(
-            `a[href*="/sites/${sitekey}/home/search-results.html"] > img[src*="/sites/${sitekey}/files/snowbearHome.jpeg"]`
+            `a[href*="/sites/${sitekey}/home/search-results.html"] > img[src*="/sites/${sitekey}/files/snowbearhome.jpeg"]`
         );
         cy.get('#contenteditor-dialog-title')
             .should('be.visible')

tests/jahia-module/jcontent-test-module/src/main/java/org/jahia/modules/textfieldinitializer/UploadErrorValidator.java
@@ -34,7 +34,7 @@ import javax.validation.constraints.AssertTrue;
 public class UploadErrorValidator implements JCRNodeValidator {
 
     private final JCRNodeWrapper node;
-    private static final String TEST_NODE_NAME_PREFIX = "uploadConstraintValidation";
+    private static final String TEST_NODE_NAME_PREFIX = "uploadconstraintvalidation";
 
     public UploadErrorValidator(JCRNodeWrapper node) {
         this.node = node;

commit 7e8e16d6bcb35ab30785556b5ed8d0bae2975896

fix: sanitize filename in replace path to match stored node name
When replacing a file, the path was built using the raw filename,
while the initial upload stores the node via sanitizeName() which
lowercases the base name. Any filename with uppercase letters caused
a case-sensitive JCR path mismatch, resulting in a blank page.

Regression introduced by 3ede5420.

src/javascript/JContent/ContentRoute/ContentLayout/Upload/UploadItem/UploadItem.jsx
@@ -21,7 +21,7 @@ import {
     fileuploadTakeFromQueue,
     fileuploadUpdateUpload
 } from '~/JContent/ContentRoute/ContentLayout/Upload/Upload.redux';
-import {createMissingFolders, onFilesSelected} from '~/JContent/ContentRoute/ContentLayout/Upload/Upload.utils';
+import {createMissingFolders, onFilesSelected, sanitizeName} from '~/JContent/ContentRoute/ContentLayout/Upload/Upload.utils';
 import {uploadErrors} from '../Upload.constants';
 import clsx from 'clsx';
 
@@ -87,7 +87,7 @@ export const UploadItem = ({upload, index}) => {
         }
 
         if (type === 'replace') {
-            let newPath = `${normalizedPath}/${getFileName()}`;
+            let newPath = `${normalizedPath}/${sanitizeName(getFileName())}`;
             return registry.get('fileUpload', 'replace').handleUpload({path: newPath, file, client});
         }
 
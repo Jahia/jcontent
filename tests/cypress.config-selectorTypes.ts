5302cd873627c09d443b8acd1e4aa85c35b1e27f
david.griffon <dgriffon@jahia.com>
2026-06-08 18:35:00 +0200

    restore skipped tests

--- tests/cypress.config-selectorTypes.ts
+++ tests/cypress.config-selectorTypes.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'cypress';
 import {baseConfig} from './cypress.config.common';
 
-baseConfig.e2e.specPattern = ['cypress/e2e/selectorTypes/**/addMixinChoicelistInitializers.cy.ts'];
+baseConfig.e2e.specPattern = ['cypress/e2e/selectorTypes/**/*.cy.ts'];
 
 export default defineConfig(baseConfig);

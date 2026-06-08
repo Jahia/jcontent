d0886bf6831ef19a151ad1c23380d686a521720c
Alex Karmanov <alexandrk_@hotmail.com>
2026-06-08 12:04:32 -0400

    test: Fix description tests, add skill (#2379)

--- src/main/java/org/jahia/modules/visibility/graphql/type/VisibilityConditionEvaluationResult.java
+++ src/main/java/org/jahia/modules/visibility/graphql/type/VisibilityConditionEvaluationResult.java
@@ -1,9 +1,11 @@
 package org.jahia.modules.visibility.graphql.type;
 
+import graphql.annotations.annotationTypes.GraphQLDescription;
 import graphql.annotations.annotationTypes.GraphQLField;
 import graphql.annotations.annotationTypes.GraphQLName;
 import org.jahia.modules.graphql.provider.dxm.node.GqlJcrNode;
 
+@GraphQLDescription("Result of evaluating a single visibility condition on a node")
 public class VisibilityConditionEvaluationResult {
 
     private final GqlJcrNode conditionNode;
@@ -16,12 +18,14 @@ public class VisibilityConditionEvaluationResult {
 
     @GraphQLField
     @GraphQLName("conditionNode")
+    @GraphQLDescription("The JCR node representing the visibility condition")
     public GqlJcrNode getConditionNode() {
         return conditionNode;
     }
 
     @GraphQLField
     @GraphQLName("matches")
+    @GraphQLDescription("True if the visibility condition matches for the current context")
     public boolean isMatches() {
         return matches;
     }

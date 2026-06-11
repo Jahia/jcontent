bcee4caafd73c2bd396f1641b359611650235907
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-06-11 11:03:50 +0200

    naze

--- src/javascript/ContentEditor/actions/jcontent/editContent/editContentAction.jsx
+++ src/javascript/ContentEditor/actions/jcontent/editContent/editContentAction.jsx
@@ -16,24 +16,29 @@ export const EditContent = ({
     useTranslation('jcontent');
     const api = useContentEditorApiContext();
     const language = useSelector(state => state.language);
-    const res = useNodeChecks(
-        {path: path, language: language},
-        {...otherProps}
-    );
+    const res = useNodeChecks({path, language}, otherProps);
+
+    // For side-by-side, pick an active lang that is not the current one for source content
+    const languages = res.node?.site?.languages?.filter(l => l.activeInEdit) || [];
+    const sourceLang = languages.find(l => l.language !== language) || languages[0];
+
+    console.log('lng', language, 'sourceLang', sourceLang);
 
     if (Loading && res.loading) {
         return <Loading {...otherProps}/>;
     }
 
     return (
-        <Render {...otherProps}
-                isVisible={res.checksResult}
-                onClick={() => api.edit({
+        <Render
+            {...otherProps}
+            isVisible={res.checksResult}
+            onClick={() =>
+                api.edit({
                     uuid: res.node.uuid,
                     lang: language,
                     isFullscreen,
                     editCallback,
-                    sideBySideContext: {lang: language},
+                    sideBySideContext: {lang: sourceLang.language},
                     ...otherProps.editConfig
                 })}
         />

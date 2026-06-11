fb38f3017036c214f8f9f6ba3246050a04d7bbcb
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-06-11 11:34:43 +0200

    nope, couldn't make it work

--- src/javascript/ContentEditor/actions/jcontent/editContent/editContentAction.jsx
+++ src/javascript/ContentEditor/actions/jcontent/editContent/editContentAction.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {useNodeChecks} from '@jahia/data-helper';
 import * as PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
-import {useTranslation} from 'react-i18next';
 import {useContentEditorApiContext} from '~/ContentEditor/contexts/ContentEditorApi/ContentEditorApi.context';
 
 export const EditContent = ({
@@ -13,20 +12,17 @@ export const EditContent = ({
     loading: Loading,
     ...otherProps
 }) => {
-    useTranslation('jcontent');
     const api = useContentEditorApiContext();
     const language = useSelector(state => state.language);
     const res = useNodeChecks({path, language}, otherProps);
 
-    // For side-by-side, pick an active lang that is not the current one for source content
-    const languages = res.node?.site?.languages?.filter(l => l.activeInEdit) || [];
-    const sourceLang = languages.find(l => l.language !== language) || languages[0];
-
-    console.log('lng', language, 'sourceLang', sourceLang);
-
     if (Loading && res.loading) {
         return <Loading {...otherProps}/>;
     }
+
+    // For side-by-side, pick a source language that is active and different from the current language, if any
+    const languages = res.node?.site?.languages?.filter(l => l.activeInEdit) || [];
+    const sourceLang = languages.find(l => l.language !== language) || languages[0];
 
     return (
         <Render

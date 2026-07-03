46ec03416306f78bdcba7d7d9082c11fafa32e02
Geofrey Flores <gflores@jahia.com>
2026-07-03 19:10:46 -0400

    Refactor MainActionBar

--- src/javascript/JContent/ContentRoute/MainActionBar/MainActionBar.jsx
+++ src/javascript/JContent/ContentRoute/MainActionBar/MainActionBar.jsx
@@ -5,48 +5,42 @@ import {DisplayAction} from '@jahia/ui-extender';
 import {useNodeInfo} from '@jahia/data-helper';
 import {shallowEqual, useSelector} from 'react-redux';
 import styles from './MainActionBar.scss';
-import {booleanValue} from '~/JContent/JContent.utils';
 
 const ButtonRenderer = getButtonRenderer({defaultButtonProps: {size: 'big'}});
 const ButtonRendererNoLabel = getButtonRenderer({labelStyle: 'none', defaultButtonProps: {size: 'big'}});
 const ButtonRendererShortLabel = getButtonRenderer({labelStyle: 'short', defaultButtonProps: {size: 'big'}});
 
 export const MainActionBar = () => {
-    const showPageBuilder = booleanValue(contextJsParameters.config.jcontent?.showPageBuilder);
-
-    const {path, language, selection} = useSelector(state => ({path: state.jcontent.path, language: state.language, selection: state.jcontent.selection}), shallowEqual);
-
-    const {node, loading} = useNodeInfo({path, language}, {getIsNodeTypes: ['jnt:page', 'jnt:contentFolder', 'jnt:folder']});
+    const {path, language, selection} = useSelector(state => ({
+        path: state.jcontent.path,
+        language: state.language,
+        selection: state.jcontent.selection
+    }), shallowEqual);
+    const {node, loading} = useNodeInfo(
+        {path, language},
+        {getIsNodeTypes: ['jnt:page', 'jnt:contentFolder', 'jnt:folder']}
+    );
 
     if (loading || !node) {
-        return false;
+        return null;
     }
 
     const isFolder = node['jnt:folder'] || node['jnt:contentFolder'];
     const publishAction = isFolder ? 'publishAll' : 'publish';
-    const isDisabled = selection && selection.length > 0;
+    const isDisabled = selection?.length > 0;
 
     return (
         <div className={styles.root}>
             <DisplayAction actionKey="search" path={path} isDisabled={isDisabled} render={ButtonRenderer} buttonProps={{variant: 'ghost', size: 'big', 'data-sel-role': 'open-search-dialog'}}/>
-            <Separator variant="vertical" invisible="firstOrLastChild" className={styles.showSeparator}/>
+            <Separator variant="vertical" invisible="firstOrLastChild"/>
 
-            {showPageBuilder ? (
-                <>
-                    { !isFolder && (
-                        <ButtonGroup size="big" variant="outlined" color="default" className={styles.item}>
-                            <DisplayAction isMediumLabel actionKey="openInPreview" path={path} isDisabled={isDisabled} render={ButtonRendererShortLabel} buttonProps={{className: styles.item}}/>
-                            <DisplayAction menuUseElementAnchor actionKey="openInPreviewMenu" path={path} isDisabled={isDisabled} render={ButtonRendererNoLabel} buttonProps={{icon: <ChevronDown/>}}/>
-                        </ButtonGroup>)}
-                    <DisplayAction actionKey="openInLive" path={path} isDisabled={isDisabled} render={ButtonRenderer} buttonProps={{variant: 'outlined', size: 'big', color: 'accent', className: styles.item}}/>
-                </>
-            ) : (
-                <>
-                    <DisplayAction actionKey="pageComposer" path={path} isDisabled={isDisabled} render={ButtonRenderer} buttonProps={{variant: 'ghost', size: 'big', color: 'accent', className: styles.item}}/>
-                    <DisplayAction actionKey={node['jnt:page'] ? 'editPage' : 'edit'} path={path} isDisabled={isDisabled} render={ButtonRenderer} buttonProps={{variant: 'outlined', size: 'big', className: styles.item}}/>
-                </>
-            )}
+            { !isFolder && (
+                <ButtonGroup size="big" variant="outlined" color="default" className={styles.item}>
+                    <DisplayAction isMediumLabel actionKey="openInPreview" path={path} isDisabled={isDisabled} render={ButtonRendererShortLabel} buttonProps={{className: styles.item}}/>
+                    <DisplayAction menuUseElementAnchor actionKey="openInPreviewMenu" path={path} isDisabled={isDisabled} render={ButtonRendererNoLabel} buttonProps={{icon: <ChevronDown/>}}/>
+                </ButtonGroup>)}
 
+            <DisplayAction actionKey="openInLive" path={path} isDisabled={isDisabled} render={ButtonRenderer} buttonProps={{variant: 'outlined', size: 'big', color: 'accent', className: styles.item}}/>
             <ButtonGroup size="big" variant="default" color="accent" className={styles.item}>
                 <DisplayAction isMediumLabel actionKey={publishAction} path={path} isDisabled={isDisabled} render={ButtonRendererShortLabel} buttonProps={{variant: 'default', size: 'big', color: 'accent'}}/>
                 <DisplayAction menuUseElementAnchor actionKey="publishMenu" path={path} isDisabled={isDisabled} render={ButtonRendererNoLabel} buttonProps={{variant: 'default', size: 'big', color: 'accent', icon: <ChevronDown/>}}/>

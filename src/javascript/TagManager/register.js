8483abd7ff5f09ddf6ff56b268b7bcc22557dc74
Geofrey Flores <gflores@jahia.com>
2026-07-09 16:11:43 -0400

    fix: Add (optional) minimum graphql version check to activate new tag manager

--- src/javascript/TagManager/register.js
+++ src/javascript/TagManager/register.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {Tag} from '@jahia/moonstone';
+import {satisfies} from 'compare-versions';
 import {TagManager} from './TagManager';
+
+const normalize = v => v?.match(/\d+/g)?.join('.');
+const REQUIRED_GQL_DXM_VERSION = '>=3.8';
 
 const TagManagerRoute = () => {
     const siteKey = useSelector(state => state.site);
@@ -9,12 +13,15 @@ const TagManagerRoute = () => {
 };
 
 export const register = registry => {
-    registry.add('adminRoute', 'jctagsmanager', {
-        targets: ['jcontent'],
-        label: 'jcontent:label.contentManager.navigation.manage.tags.title',
-        icon: <Tag/>,
-        isSelectable: true,
-        requiredPermission: 'tagManager',
-        render: () => <TagManagerRoute/>
-    });
+    const version = window.contextJsParameters?.config?.graphqlDxmProviderVersion;
+    if (version && satisfies(normalize(version), REQUIRED_GQL_DXM_VERSION)) {
+        registry.add('adminRoute', 'jctagsmanager', {
+            targets: ['jcontent'],
+            label: 'jcontent:label.contentManager.navigation.manage.tags.title',
+            icon: <Tag/>,
+            isSelectable: true,
+            requiredPermission: 'tagManager',
+            render: () => <TagManagerRoute/>
+        });
+    }
 };

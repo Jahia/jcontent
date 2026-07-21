04c9e9182b7d9e2e5df19d60c183b16aa4140d74
Driss <delallioui@jahia.com>
2026-07-21 17:30:35 +0200

    refactor: extract editForm sections into a reusable GraphQL fragment (#2574)
    Co-authored-by: Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>

---
+++ .chachalog/bquPA_Ju.md
@@ -0,0 +1,6 @@
+---
+# Allowed version bumps: patch, minor, major
+"@jahia/jcontent": patch
+---
+
+Exposed a new `ContentEditorFragment` GraphQL fragment for UI extensions to display a Content Editor form. (#2574)
--- src/javascript/shared/index.js
+++ src/javascript/shared/index.js
@@ -34,6 +34,7 @@ export * from '~/ContentEditor/ContentEditor/EditPanel/EditPanelLanguageSwitcher
 export {useEditFormDefinition} from '~/ContentEditor/ContentEditor/useEditFormDefinition';
 export {useCreateFormDefinition} from '~/ContentEditor/ContentEditor/useCreateFormDefinition';
 export {EditPanelContent} from '~/ContentEditor/editorTabs/EditPanelContent/EditPanelContent';
+export {ContentEditorFragment} from '~/ContentEditor/ContentEditor/fragments';
 
 // New date-formatter package to improve consistency
 export * as dateFormatter from 'date-formatter';

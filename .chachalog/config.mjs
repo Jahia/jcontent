3b7016c51045df338163b14b679a36cbb90f5bf3
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-08-06 13:33:42 +0200

    step 2: chachalog config

--- .chachalog/config.mjs
+++ .chachalog/config.mjs
@@ -1,11 +1,21 @@
 /// @ts-check
 /// <reference types="@chachalog/types" />
+import fs from "node:fs";
 import { defineConfig } from "chachalog";
 import github from "chachalog/github";
-import yarn from "chachalog/yarn";
 
 export default defineConfig(() => ({
   allowedBumps: ["patch", "minor", "major"],
-  platform: github({ base: "main" }),
-  managers: yarn(),
+  platform: github(),
+  managers: {
+    packages: {
+      name: "jcontent",
+      path: process.cwd(),
+      version: fs.readFileSync(".chachalog/.version", "utf-8").trim(),
+    },
+    setVersion(_pkg, version) {
+      fs.writeFileSync(".chachalog/.version", version);
+      return true;
+    },
+  },
 }));

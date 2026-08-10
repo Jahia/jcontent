8f2a10a0bf45bed7c2c3e529100f0d2a243954c8
Geofrey Flores <75278792+gflores-jahia@users.noreply.github.com>
2026-08-10 10:16:06 -0400

    fix: Update CheckboxItem import package path (#2654)

--- src/javascript/JContent/actions/deleteActions/Delete/Delete.jsx
+++ src/javascript/JContent/actions/deleteActions/Delete/Delete.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {useTranslation} from 'react-i18next';
 import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@material-ui/core';
-import {Button, Loader} from '@jahia/moonstone';
+import {Button, Information, Loader} from '@jahia/moonstone';
 import styles from './Delete.scss';
 import {useApolloClient, useMutation, useQuery} from '@apollo/client';
 import {shallowEqual, useDispatch, useSelector} from 'react-redux';
@@ -14,7 +14,6 @@ import {
     UndeleteMutation
 } from '~/JContent/actions/deleteActions/Delete/delete.gql-mutation';
 import InfoTable from './InfoTable';
-import SvgInformation from '@jahia/moonstone/dist/icons/components/Information';
 import {Info} from '~/JContent/actions/deleteActions/Delete/Info';
 import {isPathChildOfAnotherPath, JahiaRenderedModulesUtil} from '../../../JContent.utils';
 import {renderLabel} from './renderLabel';
@@ -43,7 +42,7 @@ const DeleteContent = ({data, onClose, isLoading, isMutationLoading, dialogType,
             <DialogTitle>
                 {t(`jcontent:label.contentManager.deleteAction.${dialogType}.title`)}
                 <Button className={styles.button}
-                        icon={<SvgInformation/>}
+                        icon={<Information/>}
                         variant="ghost"
                         onClick={() => {
                             setInfoOpen(true);
